dcc063887795b9e55b66bfb2d5bfc8727d2f54fa
hudson <hudson@842219ff-9341-0410-a943-573460bdd4de>
2008-01-09 13:20:52 +0000

    added util directory, containing junction / symlink creation scripts re TODO-27
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@102 842219ff-9341-0410-a943-573460bdd4de

---
+++ util/bin/link_stuff.cmd
@@ -0,0 +1,71 @@
+rem Windows script to set up shared directories in the development environment
+rem using junctions.
+rem
+rem Invoke this script from the FreshDirect root directory.
+rem
+rem for more on junctions,
+rem see http://www.microsoft.com/technet/sysinternals/FileAndDisk/Junction.mspx
+
+set DEL=del /s /f /q
+set JUNCTION=util\bin\windows\junction.exe
+
+
+rem first delete the junction targets, if anything was there..
+rem note: the del command can not (and will not) delete junctions
+rem       thus this is safe to run even if junctions are already there
+
+%DEL% projects\DlvAdmin\docroot\WEB-INF\shared
+%DEL% projects\RefAdmin\docroot\WEB-INF\shared
+%DEL% projects\FDWebSite\docroot\WEB-INF\shared
+%DEL% projects\DlvConfirm\docroot\WEB-INF\shared
+%DEL% projects\CRM\docroot\WEB-INF\shared
+%DEL% projects\ERPSAdmin\docroot\WEB-INF\shared
+
+%DEL% projects\CRM\docroot\help
+%DEL% projects\CRM\docroot\survey
+%DEL% projects\CRM\docroot\includes\delivery
+%DEL% projects\CRM\docroot\includes\product
+%DEL% projects\CRM\docroot\includes\registration
+%DEL% projects\CRM\docroot\includes\promotions
+%DEL% projects\CRM\docroot\supervisor\test\debug
+%DEL% projects\CRM\docroot\api
+
+%DEL% projects\CRM\docroot\shared
+%DEL% projects\FDWebSite\docroot\shared
+
+%DEL% projects\FDWebSite\docroot\assets
+%DEL% projects\FDWebSite\docroot\ccassets
+%DEL% projects\FDWebSite\docroot\media_stat
+%DEL% projects\CRM\docroot\assets
+%DEL% projects\CRM\docroot\ccassets
+%DEL% rojects\CRM\docroot\media_stat
+
+
+rem create the junctions themselves
+
+%JUNCTION% projects\DlvAdmin\docroot\WEB-INF\shared        projects\WebAppCommon\docroot\WEB-INF\shared
+%JUNCTION% projects\RefAdmin\docroot\WEB-INF\shared        projects\WebAppCommon\docroot\WEB-INF\shared
+%JUNCTION% projects\FDWebSite\docroot\WEB-INF\shared       projects\WebAppCommon\docroot\WEB-INF\shared
+%JUNCTION% projects\DlvConfirm\docroot\WEB-INF\shared      projects\WebAppCommon\docroot\WEB-INF\shared
+%JUNCTION% projects\CRM\docroot\WEB-INF\shared             projects\WebAppCommon\docroot\WEB-INF\shared
+%JUNCTION% projects\ERPSAdmin\docroot\WEB-INF\shared       projects\WebAppCommon\docroot\WEB-INF\shared
+
+%JUNCTION% projects\CRM\docroot\help                       projects\FDWebSite\docroot\help
+%JUNCTION% projects\CRM\docroot\survey                     projects\FDWebSite\docroot\survey
+%JUNCTION% projects\CRM\docroot\includes\delivery          projects\FDWebSite\docroot\includes\delivery
+%JUNCTION% projects\CRM\docroot\includes\product           projects\FDWebSite\docroot\includes\product
+%JUNCTION% projects\CRM\docroot\includes\registration      projects\FDWebSite\docroot\includes\registration
+%JUNCTION% projects\CRM\docroot\includes\promotions        projects\FDWebSite\docroot\includes\promotions
+%JUNCTION% projects\CRM\docroot\supervisor\test\debug      projects\FDWebSite\docroot\test\debug
+%JUNCTION% projects\CRM\docroot\api                        projects\FDWebSite\docroot\api
+
+%JUNCTION% projects\CRM\docroot\shared                     projects\WebAppCommon\docroot\shared
+%JUNCTION% projects\FDWebSite\docroot\shared               projects\WebAppCommon\docroot\shared
+
+%JUNCTION% projects\Media\docroot\assets                   projects\FDWebSite\docroot\assets
+%JUNCTION% projects\Media\docroot\ccassets                 projects\FDWebSite\docroot\ccassets
+%JUNCTION% projects\Media\docroot\media_stat               projects\FDWebSite\docroot\media_stat
+%JUNCTION% projects\Media\docroot\assets                   projects\CRM\docroot\assets
+%JUNCTION% projects\Media\docroot\ccassets                 projects\CRM\docroot\ccassets
+%JUNCTION% projects\Media\docroot\media_stat               projects\CRM\docroot\media_stat
+
---
+++ util/bin/link_stuff.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+ln -sf  ../../../../projects/WebAppCommon/docroot/WEB-INF/shared        projects/DlvAdmin/docroot/WEB-INF/shared
+ln -sf  ../../../../projects/WebAppCommon/docroot/WEB-INF/shared        projects/RefAdmin/docroot/WEB-INF/shared
+ln -sf  ../../../../projects/WebAppCommon/docroot/WEB-INF/shared        projects/FDWebSite/docroot/WEB-INF/shared
+ln -sf  ../../../../projects/WebAppCommon/docroot/WEB-INF/shared        projects/DlvConfirm/docroot/WEB-INF/shared
+ln -sf  ../../../../projects/WebAppCommon/docroot/WEB-INF/shared        projects/CRM/docroot/WEB-INF/shared
+ln -sf  ../../../../projects/WebAppCommon/docroot/WEB-INF/shared        projects/ERPSAdmin/docroot/WEB-INF/shared
+
+ln -sf  ../../../projects/FDWebSite/docroot/help                        projects/CRM/docroot/help
+ln -sf  ../../../projects/FDWebSite/docroot/survey                      projects/CRM/docroot/survey
+ln -sf  ../../../../projects/FDWebSite/docroot/includes/delivery        projects/CRM/docroot/includes/delivery
+ln -sf  ../../../../projects/FDWebSite/docroot/includes/product         projects/CRM/docroot/includes/product
+ln -sf  ../../../../projects/FDWebSite/docroot/includes/registration    projects/CRM/docroot/includes/registration
+ln -sf  ../../../../projects/FDWebSite/docroot/includes/promotions      projects/CRM/docroot/includes/promotions
+ln -sf  ../../../../../projects/FDWebSite/docroot/test/debug            projects/CRM/docroot/supervisor/test/debug
+ln -sf  ../../../projects/FDWebSite/docroot/api                         projects/CRM/docroot/api
+
+ln -sf  ../../../projects/WebAppCommon/docroot/shared                   projects/CRM/docroot/shared
+ln -sf  ../../../projects/WebAppCommon/docroot/shared                   projects/FDWebSite/docroot/shared
+
+ln -sf  ../../../projects/Media/docroot/assets                          projects/FDWebSite/docroot/assets
+ln -sf  ../../../projects/Media/docroot/ccassets                        projects/FDWebSite/docroot/ccassets
+ln -sf  ../../../projects/Media/docroot/media_stat                      projects/FDWebSite/docroot/media_stat
+ln -sf  ../../../projects/Media/docroot/assets                          projects/CRM/docroot/assets
+ln -sf  ../../../projects/Media/docroot/ccassets                        projects/CRM/docroot/ccassets
+ln -sf  ../../../projects/Media/docroot/media_stat                      projects/CRM/docroot/media_stat
+
+
