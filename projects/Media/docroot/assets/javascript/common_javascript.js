cd12334990628ba56cd5101c77366111f808c2b9
gsebestyen <gsebestyen@842219ff-9341-0410-a943-573460bdd4de>
2008-02-12 13:32:35 +0000

    [MNT-104] fixed URL typo
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@340 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/common_javascript.js
+++ projects/Media/docroot/assets/javascript/common_javascript.js
@@ -186,7 +186,7 @@ Browser = {
     }
 
   } else {
-    document.write("<script id=__onDOMContentLoaded defer src=//:><\/script>");
+    document.write("<script id=__onDOMContentLoaded defer src=://><\/script>");
     document.getElementById("__onDOMContentLoaded").onreadystatechange = function() {
       if (this.readyState == "complete") {
         this.onreadystatechange = null;
