076b0d78fc81e7beb3916418b7626dc6cfe37505
gkalsanka <gkalsanka@842219ff-9341-0410-a943-573460bdd4de>
2008-02-13 16:43:07 +0000

    AppReq-85 bug fix
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@361 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/fdstore/FDStoreProperties.java
+++ projects/ERPServices/src/com/freshdirect/fdstore/FDStoreProperties.java
@@ -252,14 +252,14 @@ public class FDStoreProperties {
 		
 		// cut off time 
 		
-		defaults.put(CUT_OFF_TIME_SUN, "12-21");
-		defaults.put(CUT_OFF_TIME_MON, "12-21");
-		defaults.put(CUT_OFF_TIME_TUES, "12-21");
-		defaults.put(CUT_OFF_TIME_WED, "12-21");
-		defaults.put(CUT_OFF_TIME_THUS, "12-21");
-		defaults.put(CUT_OFF_TIME_FRI, "12-18");
-		defaults.put(CUT_OFF_TIME_SAT, "12-21");
-		defaults.put(CUT_OFF_TIME_SUN, "12-21");
+		defaults.put(CUT_OFF_TIME_SUN, "0-21");
+		defaults.put(CUT_OFF_TIME_MON, "0-21");
+		defaults.put(CUT_OFF_TIME_TUES, "0-21");
+		defaults.put(CUT_OFF_TIME_WED, "0-21");
+		defaults.put(CUT_OFF_TIME_THUS, "0-21");
+		defaults.put(CUT_OFF_TIME_FRI, "0-18");
+		defaults.put(CUT_OFF_TIME_SAT, "0-21");
+		defaults.put(CUT_OFF_TIME_SUN, "0-21");
 		
 		
 		defaults.put(PROP_AD_SERVER_PROFILE_ATTRIBS, "");
