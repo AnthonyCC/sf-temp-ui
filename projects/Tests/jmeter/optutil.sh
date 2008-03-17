5ad05d9473bb59a57bb2e335208e1d3c74cccc8b
skrishnasamy <skrishnasamy@842219ff-9341-0410-a943-573460bdd4de>
2008-03-17 18:28:11 +0000

    [TODO-72]Debugging Login.sh
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@482 842219ff-9341-0410-a943-573460bdd4de

--- projects/Tests/jmeter/optutil.sh
+++ projects/Tests/jmeter/optutil.sh
@@ -164,6 +164,7 @@ function runprogs() {
    PROGS="$1";
    checkvars "s d w j";
    for program in $PROGS; do
+      echo $program
       sh -e $program -d "$OUTDIR" -s "$SERVER" -w "$WORKDIR" -j "$JMETERJAR";
    done
 }
