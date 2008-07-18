8215e27a7bf8ed516c1e34dfbc03788e68626eaf
ihernadvolgyi <ihernadvolgyi@842219ff-9341-0410-a943-573460bdd4de>
2008-07-18 17:31:50 +0000

    [MNT-289] Added configuration to the DYF product descriptions.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@1135 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/util/ConfigurationUtil.java
@@ -0,0 +1,39 @@
+package com.freshdirect.webapp.util;
+
+import com.freshdirect.fdstore.FDResourceException;
+import com.freshdirect.fdstore.content.ProductModel;
+import com.freshdirect.fdstore.customer.FDInvalidConfigurationException;
+import com.freshdirect.fdstore.customer.FDProductSelection;
+import com.freshdirect.fdstore.customer.FDProductSelectionI;
+import com.freshdirect.fdstore.customer.OrderLineUtil;
+
+/**
+ * Configuration utilities.
+ * @author istvan
+ *
+ */
+public class ConfigurationUtil {
+		
+	/**
+	 * Generate a product description for a a transactional (thus configured) product impression.
+	 * 
+	 * @param impression
+	 * @return description or null
+	 */
+	public static String getConfigurationDescription(TransactionalProductImpression impression) {
+		FDProductSelectionI selection = 
+			new FDProductSelection(
+				impression.getFDProduct(),
+				((ProductModel)impression.getSku().getParentNode()).getProductRef(),
+				impression.getConfiguration());
+		try {
+			OrderLineUtil.describe(selection);
+		} catch (FDResourceException e) {
+			return null;
+		} catch (FDInvalidConfigurationException e) {
+			return null;
+		}
+		String description = selection.getConfigurationDesc();
+		return description == null || "".equals(description) ? null : description;
+	}
+}
