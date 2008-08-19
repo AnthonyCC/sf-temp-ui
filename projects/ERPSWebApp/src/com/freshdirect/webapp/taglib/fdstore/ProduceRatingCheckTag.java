dc88761a5a79929467cc8a662fcc7864659499f4
gkalsanka <gkalsanka@842219ff-9341-0410-a943-573460bdd4de>
2008-08-19 14:55:42 +0000

    Produce Rating changes
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@1250 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/ProduceRatingCheckTag.java
@@ -0,0 +1,42 @@
+/*
+ * $Workfile$
+ *
+ * $Date$
+ *
+ * Copyright (c) 2001 FreshDirect, Inc.
+ *
+ */
+package com.freshdirect.webapp.taglib.fdstore;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.JspException;
+
+import com.freshdirect.fdstore.customer.FDUserI;
+
+/**
+ * Tag that renders its body if CCL functionality should be displayed for this user.
+ * 
+ * @version $Revision$
+ * @author $Author$
+ */
+public class ProduceRatingCheckTag extends com.freshdirect.framework.webapp.TagSupport {
+
+	public int doStartTag() throws JspException {
+		HttpSession session = pageContext.getSession();
+		FDSessionUser user = (FDSessionUser) session
+				.getAttribute(SessionName.USER);
+		
+		System.out.println("Inside Produce Rating Check Tag");
+				
+		if (user == null ||  !user.isProduceRatingEnabled()) {
+			return SKIP_BODY;
+		}
+
+		return EVAL_BODY_INCLUDE;
+	}
+
+	public int doEndTag() throws JspException {
+		return EVAL_PAGE;
+	}
+
+}
