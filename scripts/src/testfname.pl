baf9d65c02cdd68c3f4c2e2de842523fd2ccd413
abhatia <abhatia@842219ff-9341-0410-a943-573460bdd4de>
2008-10-17 20:26:31 +0000

    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@1796 842219ff-9341-0410-a943-573460bdd4de

--- scripts/src/testfname.pl
+++ scripts/src/testfname.pl
@@ -1,10 +1,35 @@
+my $os = lc $^O;
 
+my $configfile;
+my $linux;
+if($os =~ m/mswin/){
+    $configfile = '..\perlscripts.windows.cfg';
+    $islinux=0;
+}
+else{
+     $configfile = '../perlscripts.linux.cfg';
+    $islinux=1;
+}
+
+print "debug: configfile= ", $configfile, "\n"; 
+
+if($islinux){
+    $ENV{'ORACLE_HOME'}="/opt/fdbin/oracle/instantclient_11_1";
+    BEGIN {
+            push @INC,"../lib";
+           }
+    }
+else{
+        BEGIN {
+            push @INC,"..\lib";
+        }
+}
 use Common;
 my $cmn = new Common;
 
-my $sfx = $cmn->get_highest_suffix("c:\\junk\\foo.txt");
+my $sfx = $cmn->get_highest_suffix("C:\\FreshDirect\\scripts\\data\\peakproduce_inductees.dat");
 
-my $file = $cmn->make_suffix_file("c:\\junk\\foo.txt", $sfx);
+my $file = $cmn->make_suffix_file("C:\\FreshDirect\\scripts\\data\\peakproduce_inductees.dat", $sfx);
 
 print "file= ", $file, "\n";
 
