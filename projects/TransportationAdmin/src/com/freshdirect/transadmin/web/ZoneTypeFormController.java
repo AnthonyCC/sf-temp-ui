60151871ed1bdd73719fd6fbd54ff0b1ce8d1c69
mshetty <mshetty@842219ff-9341-0410-a943-573460bdd4de>
2008-12-01 22:25:46 +0000

    [APPREQ-216]ZoneType adjustments validations added.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@2220 842219ff-9341-0410-a943-573460bdd4de

--- projects/TransportationAdmin/src/com/freshdirect/transadmin/web/ZoneTypeFormController.java
+++ projects/TransportationAdmin/src/com/freshdirect/transadmin/web/ZoneTypeFormController.java
@@ -1,13 +1,10 @@
 package com.freshdirect.transadmin.web;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.dao.DataAccessException;
 
 import com.freshdirect.transadmin.model.TrnZoneType;
 import com.freshdirect.transadmin.util.TransStringUtil;
@@ -15,11 +12,7 @@ import com.freshdirect.transadmin.util.TransStringUtil;
 public class ZoneTypeFormController extends AbstractDomainFormController {
 		
 	protected Map referenceData(HttpServletRequest request) throws ServletException {
-		Map refData = new HashMap();
-		refData.put("roles", getDomainManagerService().getEmployeeRoleTypes());
-		//refData.put("supervisors", getDomainManagerService().getSupervisors());	
-		//refData.put("zonetypes", getDomainManagerService().getZoneTypes());	
-		//refData.put("areas", getDomainManagerService().getAreas());	
+		Map refData = new HashMap();						
 		return refData;
 	}
 	
@@ -33,7 +26,7 @@ public class ZoneTypeFormController extends AbstractDomainFormController {
 	
 	public boolean isNew(Object command) {
 		TrnZoneType modelIn = (TrnZoneType)command;
-		return (modelIn.getId() == null);
+		return (modelIn.getZoneTypeId() == null);
 	}
 	
 	public String getDomainObjectName() {
@@ -42,8 +35,8 @@ public class ZoneTypeFormController extends AbstractDomainFormController {
 	
 	protected void preProcessDomainObject(Object domainObject) {
 		TrnZoneType modelIn = (TrnZoneType)domainObject;
-		if(TransStringUtil.isEmpty(modelIn.getId()) ) {
-			modelIn.setId(modelIn.getId());
+		if(TransStringUtil.isEmpty(modelIn.getZoneTypeId()) ) {
+			modelIn.setZoneTypeId(modelIn.getZoneTypeId());
 		}
 	}
 
