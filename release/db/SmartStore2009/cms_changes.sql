f6723d92d0bddf673b32659f3269c46050166158
zgegesy <zgegesy@842219ff-9341-0410-a943-573460bdd4de>
2009-01-26 16:09:13 +0000

    APPREQ-304 : db changes
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@2851 842219ff-9341-0410-a943-573460bdd4de

--- release/db/SmartStore2009/cms_changes.sql
+++ release/db/SmartStore2009/cms_changes.sql
@@ -9,3 +9,15 @@ INSERT INTO cms.relationshipdestination (RELATIONSHIPDEFINITION_ID,CONTENTTYPE_I
 // Add MANUAL_SELECTION_SLOTS to Category
 
 INSERT INTO cms.attributedefinition (NAME,ID,CONTENTTYPE_ID,ATTRIBUTETYPE_CODE,INHERITABLE,REQUIRED,LABEL,CARDINALITY_CODE,LOOKUP_CODE) VALUES ('MANUAL_SELECTION_SLOTS', 'Category.MANUAL_SELECTION_SLOTS', 'Category','I','F','F','Manual Selection Slots','One',NULL);
+
+// Add FavoriteList
+
+INSERT INTO cms.contenttype (ID,NAME,DESCRIPTION,GENERATE_ID) VALUES ('FavoriteList','FavoriteList','Definition of type FavoriteList','F');
+
+INSERT INTO cms.attributedefinition (NAME,ID,CONTENTTYPE_ID,ATTRIBUTETYPE_CODE,INHERITABLE,REQUIRED,LABEL,CARDINALITY_CODE,LOOKUP_CODE) VALUES ('full_name','FavoriteList.full_name','FavoriteList','S','F','F','','One',NULL);
+INSERT INTO cms.relationshipdefinition (NAME,ID,CONTENTTYPE_ID,INHERITABLE,REQUIRED,NAVIGABLE,LABEL,CARDINALITY_CODE) VALUES ('items','FavoriteList.items','FavoriteList','F','F','F','','Many');
+
+INSERT INTO cms.relationshipdestination (RELATIONSHIPDEFINITION_ID,CONTENTTYPE_ID,LABEL,ID) VALUES ('FavoriteList.items','Product',NULL,'FavoriteList.items.Product');
+INSERT INTO cms.relationshipdestination (RELATIONSHIPDEFINITION_ID,CONTENTTYPE_ID,LABEL,ID) VALUES ('FavoriteList.items','ConfiguredProduct',NULL,'FavoriteList.items.ConfiguredProduct');
+
+INSERT INTO cms.relationshipdestination (RELATIONSHIPDEFINITION_ID,CONTENTTYPE_ID,LABEL,ID) VALUES ('FDFolder.children','FavoriteList',NULL,'FDFolder.children.FavoriteList');
