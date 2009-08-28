93aad61377f3c4e671c05a36c762583dcd5116b0
zgegesy <zgegesy@842219ff-9341-0410-a943-573460bdd4de>
2009-08-28 11:56:31 +0000

    APPREQ-363 : add delete button to the one-to-one field
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@5261 842219ff-9341-0410-a943-573460bdd4de

--- projects/cms-gwt/src/com/freshdirect/cms/ui/client/fields/OneToOneRelationField.java
+++ projects/cms-gwt/src/com/freshdirect/cms/ui/client/fields/OneToOneRelationField.java
@@ -9,77 +9,93 @@ import com.extjs.gxt.ui.client.widget.button.IconButton;
 import com.extjs.gxt.ui.client.widget.form.AdapterField;
 import com.extjs.gxt.ui.client.widget.form.LabelField;
 import com.extjs.gxt.ui.client.widget.form.MultiField;
+import com.extjs.gxt.ui.client.widget.tips.ToolTipConfig;
 import com.freshdirect.cms.ui.client.nodetree.ContentNodeModel;
 import com.freshdirect.cms.ui.client.nodetree.ContentTreePopUp;
 
 public class OneToOneRelationField extends MultiField<ContentNodeModel> {
 
-	private IconButton relationButton;
-	private LabelField valueField;
+    private IconButton relationButton;
+    private LabelField valueField;
+
+    private ContentNodeModel value;
+    private HashSet<String> allowedTypes;
+    private IconButton deleteButton;
+
+    public OneToOneRelationField(HashSet<String> aTypes) {
+        super();
+        allowedTypes = aTypes;
+        initialize();
+    }
+
+    @Override
+    public ContentNodeModel getValue() {
+        return value;
+    }
 	
-	private ContentNodeModel value;
-	private HashSet<String> allowedTypes;
+    @Override
+    public void setValue(ContentNodeModel model) {
+        this.value = model;
+
+        if (value != null) {
+            valueField.setValue(model.renderLink(true));
+            valueField.setToolTip(model.getPreviewToolTip());
+        } else {
+            valueField.setValue("");
+        }
+    }
 	
-	public OneToOneRelationField(HashSet<String> aTypes) {
-		super();
-		allowedTypes = aTypes;
-		initialize();
-	}
+    @Override
+    public void setReadOnly(boolean readOnly) {
+        super.setReadOnly(readOnly);
+        relationButton.setEnabled(!readOnly);
+    }
 	
-	@Override
-	public ContentNodeModel getValue() {		
-		return value;	
-	}
-	
-	@Override
-	public void setValue( ContentNodeModel model ) {
-		this.value = model;
-		
-		if ( value != null ) {
-			valueField.setValue( model.renderLink( true ) );
-			valueField.setToolTip( model.getPreviewToolTip() );
-		}
-	}
-	
-	@Override
-	public void setReadOnly( boolean readOnly ) {
-		super.setReadOnly( readOnly );
-		relationButton.setEnabled( !readOnly );		
-	}
-	
-	private void initialize() { 
-		
-		relationButton = new IconButton("rel-button");
-		relationButton.setToolTip( "Change relationship" );
-		relationButton.addListener(Events.OnClick, new Listener<BaseEvent>() {
-			public void handleEvent(BaseEvent be) {
-				final ContentTreePopUp popup = ContentTreePopUp.getInstance( allowedTypes );
-				popup.setHeading(getFieldLabel());
-				popup.addListener(Events.Select, new Listener<BaseEvent>() {
-					public void handleEvent(BaseEvent be) {
-						setValue(popup.getSelected());
-						fireEvent(AttributeChangeEvent.TYPE, new AttributeChangeEvent(OneToOneRelationField.this));
-					}				
-				});
-				popup.show();
-			}			
-		});
-		add(new AdapterField(relationButton));
-		
-		valueField = new LabelField();		
-		add(valueField);		
-	}
+    private void initialize() {
+        relationButton = new IconButton("rel-button");
+        relationButton.setToolTip("Change relationship");
+        relationButton.addListener(Events.OnClick, new Listener<BaseEvent>() {
+            public void handleEvent(BaseEvent be) {
+                final ContentTreePopUp popup = ContentTreePopUp.getInstance(allowedTypes);
+                popup.setHeading(getFieldLabel());
+                popup.addListener(Events.Select, new Listener<BaseEvent>() {
+                    public void handleEvent(BaseEvent be) {
+                        setValue(popup.getSelected());
+                        fireEvent(AttributeChangeEvent.TYPE, new AttributeChangeEvent(OneToOneRelationField.this));
+                    }
+                });
+                popup.show();
+            }
+        });
+        add(new AdapterField(relationButton));
+
+        valueField = new LabelField();
+        add(valueField);
+
+        deleteButton = new IconButton("delete-button");
+        deleteButton.setToolTip(new ToolTipConfig("DELETE", "Delete relation."));
+        deleteButton.addListener(Events.OnClick, new Listener<BaseEvent>() {
+            @Override
+            public void handleEvent(BaseEvent be) {
+                OneToOneRelationField.this.setValue(null);
+            }
+        });
+        add(new AdapterField(deleteButton));
+    }
 	
     @Override
     public void disable() {
-    	super.disable();
-    	if ( relationButton != null )
-    		relationButton.disable();
+        super.disable();
+        if (relationButton != null) {
+            relationButton.disable();
+        }
     }
+
     @Override
     public void enable() {
-    	super.enable();
-    	if ( relationButton != null )
-    		relationButton.enable();
+        super.enable();
+        if (relationButton != null) {
+            relationButton.enable();
+        }
     }
 }
