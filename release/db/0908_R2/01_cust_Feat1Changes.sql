1210816cc0db8c1b483c99826af6fa9a35b9e5e1
gsebestyen <gsebestyen@842219ff-9341-0410-a943-573460bdd4de>
2009-08-28 13:13:57 +0000

    [APPREQ-579] remove debug conde
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@5266 842219ff-9341-0410-a943-573460bdd4de

--- release/db/0908_R2/01_cust_Feat1Changes.sql
+++ release/db/0908_R2/01_cust_Feat1Changes.sql
@@ -7,35 +7,3 @@ alter table cust.complaint_code
   add subject_code varchar2(8)
   add priority integer;
 
-
---
--- Run the following comments if you want to assing random priorities and subjects to complaint reasons
---
-
--- assign random case subjects
---
-update cust.complaint_code set subject_code = (
-  select b.code as subject_code from
-  (
-    select code, name, rownum as priority
-    from complaint_code
-  ) a
-  join
-  (
-    select code, rownum as id
-    from (
-      SELECT code FROM
-      (
-        SELECT code, rownum as ident FROM case_subject
-        ORDER BY dbms_random.value
-      )
-      WHERE rownum <= (select count(*) from complaint_code)
-    ) order by id
-  ) b on(a.priority=b.id)
-  where a.code=complaint_code.code
-);
-
--- assign mock priorities
---
-update cust.complaint_code set priority=rownum;
-
