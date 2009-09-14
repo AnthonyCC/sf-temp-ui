9c562562753d3b596d54e62c8dd257dd0306f19f
ztreer <ztreer@842219ff-9341-0410-a943-573460bdd4de>
2009-09-14 13:06:41 +0000

    [APPREQ-363] minor fix : progress bar possibly not disappearing on error
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@5583 842219ff-9341-0410-a943-573460bdd4de

--- projects/cms-gwt/src/com/freshdirect/cms/ui/service/BaseCallback.java
+++ projects/cms-gwt/src/com/freshdirect/cms/ui/service/BaseCallback.java
@@ -1,6 +1,7 @@
 package com.freshdirect.cms.ui.service;
 
 import com.extjs.gxt.ui.client.widget.MessageBox;
+import com.freshdirect.cms.ui.client.MainLayout;
 import com.google.gwt.user.client.Window.Location;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.InvocationException;
@@ -9,6 +10,7 @@ public abstract class BaseCallback<X> implements AsyncCallback<X> {
 
     @Override
     public final void onFailure(Throwable error) {
+    	MainLayout.stopProgress();
         if (error instanceof InvocationException && error.getMessage().contains("<!--SESSION EXPIRATION-->")) {
             Location.reload();
             return;
