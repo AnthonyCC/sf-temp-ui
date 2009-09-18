e67c20e62c36f81c03aaa62e2010480375954ac7
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2009-09-18 18:51:48 +0000

    Modified ContextDynamic Routing Phase 1 - Modified Context to access routing services
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@5721 842219ff-9341-0410-a943-573460bdd4de

--- projects/DataLoader/src/com/freshdirect/dataloader/reservation/BaseReservationCronRunner.java
+++ projects/DataLoader/src/com/freshdirect/dataloader/reservation/BaseReservationCronRunner.java
@@ -9,6 +9,7 @@ import javax.naming.NamingException;
 import com.freshdirect.ErpServicesProperties;
 import com.freshdirect.delivery.ejb.DlvManagerSB;
 import com.freshdirect.delivery.model.DlvReservationModel;
+import com.freshdirect.fdstore.FDStoreProperties;
 import com.freshdirect.fdstore.customer.FDIdentity;
 import com.freshdirect.fdstore.customer.ejb.FDCustomerManagerSB;
 
@@ -18,8 +19,8 @@ public abstract class BaseReservationCronRunner {
 	   public Context getInitialContext() throws NamingException {
 			Hashtable h = new Hashtable();
 			h.put(Context.INITIAL_CONTEXT_FACTORY, "weblogic.jndi.WLInitialContextFactory");
-			System.out.println("ErpServicesProperties.getProviderURL() :"+ErpServicesProperties.getProviderURL());
-			h.put(Context.PROVIDER_URL, ErpServicesProperties.getProviderURL());
+			System.out.println("FDStoreProperties.getRoutingProviderURL() :"+FDStoreProperties.getRoutingProviderURL());
+			h.put(Context.PROVIDER_URL, FDStoreProperties.getRoutingProviderURL());
 			return new InitialContext(h);
 		}
 		
