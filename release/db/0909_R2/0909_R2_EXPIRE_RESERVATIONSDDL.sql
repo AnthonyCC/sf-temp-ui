df6c6977f223d621ba323ab37d15709c2d9ee081
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2009-09-24 18:58:05 +0000

    0909_R2 Release scripts.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@5889 842219ff-9341-0410-a943-573460bdd4de

---
+++ release/db/0909_R2/0909_R2_EXPIRE_RESERVATIONSDDL.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE PROCEDURE DLV.EXPIRE_RESERVATIONS AS
+
+BEGIN
+
+
+update DLV.reservation set status_code = 20  where expiration_datetime <= sysdate and status_code = 5 and UNASSIGNED_ACTION is not null;
+
+update DLV.reservation set status_code = 20, UNASSIGNED_DATETIME = sysdate,UNASSIGNED_ACTION = 'CANCEL_TIMESLOT', MODIFIED_DTTM = sysdate where expiration_datetime <= sysdate and status_code = 5 and UNASSIGNED_ACTION is null and IN_UPS = 'X';
+
+
+commit;
+
+END;
