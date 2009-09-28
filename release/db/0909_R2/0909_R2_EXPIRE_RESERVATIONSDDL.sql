079c931c15431aba5568695fd57fb03f276378b4
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2009-09-28 00:31:05 +0000

    Fixed Reservation cleaner error
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@5959 842219ff-9341-0410-a943-573460bdd4de

--- release/db/0909_R2/0909_R2_EXPIRE_RESERVATIONSDDL.sql
+++ release/db/0909_R2/0909_R2_EXPIRE_RESERVATIONSDDL.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE PROCEDURE DLV.EXPIRE_RESERVATIONS AS
 
 BEGIN
 
+update DLV.reservation set status_code = 20  where expiration_datetime <= sysdate and status_code = 5 and IN_UPS is null;
 
 update DLV.reservation set status_code = 20  where expiration_datetime <= sysdate and status_code = 5 and UNASSIGNED_ACTION is not null;
 
