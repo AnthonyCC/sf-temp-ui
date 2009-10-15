c4af3c4a2ea87657004c268ad61ebc4d981713f8
cssomogyi <cssomogyi@842219ff-9341-0410-a943-573460bdd4de>
2009-10-15 09:36:09 +0000

    APPREQ-681 created alias fix, rollback scripts, extended FI variants with brand uniqueness versions
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@6298 842219ff-9341-0410-a943-573460bdd4de

---
+++ release/db/0910_R3/RB_01_cms_word_stemming_exception.sql
@@ -0,0 +1,5 @@
+
+DELETE FROM cms.relationshipdestination WHERE ID = 'FDFolder.children.WordStemmingException';
+DELETE FROM cms.attributedefinition WHERE ID = 'WordStemmingException.word';
+DELETE FROM cms.contenttype WHERE ID = 'WordStemmingException';
+
---
+++ release/db/0910_R3/RB_04_cust_scripted_variants.sql
@@ -0,0 +1,28 @@
+DELETE FROM "CUST"."SS_VARIANT_PARAMS" WHERE ID = 'featured-cms-s-bu';
+DELETE FROM "CUST"."SS_VARIANT_PARAMS" WHERE ID = 'featured-popular-s-bu';
+DELETE FROM "CUST"."SS_VARIANT_PARAMS" WHERE ID = 'featured-pop2-s-bu';
+
+DELETE FROM "CUST"."SS_VARIANTS" WHERE ID = 'featured-cms-s-bu';
+DELETE FROM "CUST"."SS_VARIANTS" WHERE ID = 'featured-popular-s-bu';
+DELETE FROM "CUST"."SS_VARIANTS" WHERE ID = 'featured-pop2-s-bu';
+
+DELETE FROM "CUST"."SS_VARIANT_PARAMS" WHERE ID = 'featured-cms-s';
+DELETE FROM "CUST"."SS_VARIANT_PARAMS" WHERE ID = 'featured-popular-s';
+DELETE FROM "CUST"."SS_VARIANT_PARAMS" WHERE ID = 'featured-pop2-s';
+
+DELETE FROM "CUST"."SS_VARIANTS" WHERE ID = 'featured-cms-s';
+DELETE FROM "CUST"."SS_VARIANTS" WHERE ID = 'featured-popular-s';
+DELETE FROM "CUST"."SS_VARIANTS" WHERE ID = 'featured-pop2-s';
+
+DELETE FROM "CUST"."SS_VARIANT_PARAMS" WHERE ID = 'featured-cms-bu';
+DELETE FROM "CUST"."SS_VARIANT_PARAMS" WHERE ID = 'featured-popular-bu';
+DELETE FROM "CUST"."SS_VARIANT_PARAMS" WHERE ID = 'featured-pop2-bu';
+
+DELETE FROM "CUST"."SS_VARIANTS" WHERE ID = 'featured-cms-bu';
+DELETE FROM "CUST"."SS_VARIANTS" WHERE ID = 'featured-popular-bu';
+DELETE FROM "CUST"."SS_VARIANTS" WHERE ID = 'featured-pop2-bu';
+
+DELETE FROM "CUST"."SS_VARIANT_PARAMS" WHERE ID = 'featured-candidate-s';
+
+DELETE FROM "CUST"."SS_VARIANTS" WHERE ID = 'featured-candidate-s';
+
