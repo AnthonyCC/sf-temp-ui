361a0be407551613a8abb03bc24544a91139431c
asexton <asexton@842219ff-9341-0410-a943-573460bdd4de>
2009-12-14 15:27:40 +0000

    Email Capture fix
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@7549 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/mail/service/MailMessageListener.java
+++ projects/ERPServices/src/com/freshdirect/mail/service/MailMessageListener.java
@@ -21,6 +21,8 @@ import javax.xml.transform.TransformerException;
 
 import org.apache.log4j.Category;
 
+import weblogic.auddi.util.Logger;
+
 import com.freshdirect.fdstore.FDStoreProperties;
 import com.freshdirect.framework.content.BaseTemplateContext;
 import com.freshdirect.framework.content.TemplateRenderer;
@@ -53,7 +55,7 @@ public class MailMessageListener extends MessageDrivenBeanSupport implements Mai
 		String mailBodyXml, mailFrom, mailTo, mailCc, mailBcc, mailTitle, xslPath, personalLabel;
 		boolean isHtml;
 		String mailBody = null;
-		
+
 		if(msg instanceof TextMessage){
 		try {
 
@@ -86,7 +88,7 @@ public class MailMessageListener extends MessageDrivenBeanSupport implements Mai
 
 		//
 		// Do transformation
-		//		
+		//
 		try {
 			mailBody = new XSLTransformer().transform(mailBodyXml, xslPath);
 		} catch (TransformerException ex) {
@@ -105,54 +107,71 @@ public class MailMessageListener extends MessageDrivenBeanSupport implements Mai
 
 		LOGGER.debug("message sent");
 		}else if(msg instanceof ObjectMessage){
+			boolean gc = false;
+			boolean iphone = false;
 			try {
 				ObjectMessage objMsg = (ObjectMessage)msg;
 				FDFtlEmail fdFtlEmail = (FDFtlEmail)objMsg.getObject();
 				Map parameters = fdFtlEmail.getParameters();
+				if(null != (String)parameters.get(MailName.GC_FTL_PATH)) {
+					gc = true;
+				}
+				if(null != (String)parameters.get(MailName.IPHONE_FTL_PATH) ) {
+					iphone = true;
+				}
 				mailFrom = objMsg.getStringProperty(MailName.FROM_ADDRESS);
-//				mailTo = objMsg.getStringProperty(MailName.TO_ADDRESS);
-				mailTo = (String)parameters.get(MailName.GC_RECIPIENT_EMAIL);
+				mailTo = objMsg.getStringProperty(MailName.TO_ADDRESS);
+				if(gc) {
+					mailTo = (String)parameters.get(MailName.GC_RECIPIENT_EMAIL);
+				}
 				mailCc = objMsg.getStringProperty(MailName.CC_ADDRESS);
 				mailTitle = objMsg.getStringProperty(MailName.TITLE);
 				xslPath = objMsg.getStringProperty(MailName.XSL);
 				personalLabel = objMsg.getStringProperty(MailName.FROM_ADDRESS_NAME);
 				isHtml = objMsg.getBooleanProperty(MailName.IS_HTML);
-				mailBody = processFtl(parameters);
+				mailBody = processFtl(parameters, gc, iphone);
 				ErpMailSender mailer = new ErpMailSender();
-				mailer.sendMail(mailFrom, mailTo, mailCc, mailTitle, mailBody, isHtml, personalLabel); 
-
+				mailer.sendMail(mailFrom, mailTo, mailCc, mailTitle, mailBody, isHtml, personalLabel);
 				LOGGER.debug("message sent");
 			} catch (JMSException e) {
-				
+				LOGGER.error("JMSException trying to send email", e);
 			} catch(TemplateException te){
-				
+				LOGGER.error("TemplateException trying to send email", te);
 			} catch(IOException ioe){
-				
+				LOGGER.error("IOException trying to send email", ioe);
+				throw new RuntimeException("Unable to send message: " + ioe.getMessage());
 			} catch (MessagingException ex) {
 				LOGGER.error("Unable to send message, throwing RuntimeException", ex);
 				throw new RuntimeException("Unable to send message: " + ex.getMessage());
 			}
-			
+
 		}
 
 	}
-	
+
 	/**
 	 * [APPREQ-621] - Kasi Sriram.
 	 * For parsing the ftl template with the received parameters.
 	 * @param parameters
+	 * @param gc
+	 * @param iphone
 	 * @return
 	 * @throws IOException
 	 * @throws TemplateException
 	 */
-	private String processFtl(Map parameters)
-	throws IOException, TemplateException {
+	private String processFtl(Map parameters, boolean gc, boolean iphone) throws IOException, TemplateException {
 		String mailBody;
 		BaseTemplateContext context = new BaseTemplateContext(parameters);
 		StringWriter writer = new StringWriter();
-		URL url = TemplateRenderer.resolve(FDStoreProperties.getMediaPath(), (String)parameters.get(MailName.GC_FTL_PATH));//"media/editorial/giftcards/_TEMPLATE/email_template.ftl");
+		URL url = null;
+		if(gc) {
+			url = TemplateRenderer.resolve(FDStoreProperties.getMediaPath(), (String)parameters.get(MailName.GC_FTL_PATH));//"media/editorial/giftcards/_TEMPLATE/email_template.ftl");
+		} else if(iphone) {
+			url = new URL(FDStoreProperties.getMediaPath().trim() + FDStoreProperties.getMediaIPhoneTemplatePath().trim() + (String)parameters.get(MailName.IPHONE_FTL_PATH));
+			LOGGER.info("Email Capture url = " + url.toString());
+		}
 		TemplateRenderer.getInstance().render(url, writer, context, true);
-		mailBody = writer.toString();		
+		mailBody = writer.toString();
 		return mailBody;
-}
+	}
 }
