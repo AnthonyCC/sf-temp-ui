c6ae89b22b704de12ed34e0bcba4da218e3a7084
rsung <rsung@842219ff-9341-0410-a943-573460bdd4de>
2009-12-15 14:17:40 +0000

    https://info.schematic.com/jira/browse/FDIPONE-1146
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@7596 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDIntegrationServices/src/com/freshdirect/mobileapi/controller/HelpController.java
+++ projects/FDIntegrationServices/src/com/freshdirect/mobileapi/controller/HelpController.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Category;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.freshdirect.fdstore.FDException;
@@ -24,6 +21,10 @@ import com.freshdirect.mobileapi.util.ProductUtil;
 public class HelpController extends BaseController {
     private static Category LOGGER = LoggerFactory.getInstance(HelpController.class);
 
+    protected boolean validateUser() {
+        return false;
+    }
+    
     public static final String HELP_ACTION = "help";
 
     public static final String CONTACT_US_ACTION = "contactUs";
