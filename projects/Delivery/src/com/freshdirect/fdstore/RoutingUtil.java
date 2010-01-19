8c74f73e161353772e37dde0801b15ec7a4e3e2f
mshetty <mshetty@842219ff-9341-0410-a943-573460bdd4de>
2010-01-19 22:24:10 +0000

    1001_R2 changes for dynamic routing.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@8053 842219ff-9341-0410-a943-573460bdd4de

--- projects/Delivery/src/com/freshdirect/fdstore/RoutingUtil.java
+++ projects/Delivery/src/com/freshdirect/fdstore/RoutingUtil.java
@@ -63,7 +63,7 @@ public class RoutingUtil {
 		try {
 			this.routingProvider = FDStoreProperties.getRoutingInitialContext();
 		} catch (NamingException e) {
-			setUseAltProvider(true);
+			useAltProvider=true;
 			e.printStackTrace();
 			//throw new RuntimeException("Unable to get InitialContext "+e.getMessage());
 		}
