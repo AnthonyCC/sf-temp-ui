d4ac5b10f032886f2e30df3335d0b643afed788f
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2010-04-08 12:19:05 +0000

    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@9094 842219ff-9341-0410-a943-573460bdd4de

--- release/db/2010_Mar_R2/04_cust_faq.sql
+++ release/db/2010_Mar_R2/04_cust_faq.sql
@@ -1,2 +1,11 @@
 -- Create new table in CUST schema
 create table cust.TOP_FAQS(CMSNODE_ID varchar2(255) not null, TIME_STAMP timestamp);
+
+grant insert,select,delete,update on cust.TOP_FAQS to fdstore_prda;
+
+grant insert,select,delete,update on cust.TOP_FAQS to fdstore_prdb;
+
+grant insert,select,delete,update on cust.TOP_FAQS to fdstore_ststg01;
+
+grant select on cust.TOP_FAQS to appdev;
+
