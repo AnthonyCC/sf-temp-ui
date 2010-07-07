911d6e0eb08df1da98a6c2c516cafe22019e5b63
cssomogyi <cssomogyi@842219ff-9341-0410-a943-573460bdd4de>
2010-07-07 09:22:33 +0000

    APPDEV-585 adding forgotten class
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@10074 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/FDStore/src/com/freshdirect/fdstore/util/IgnoreCaseString.java
@@ -0,0 +1,43 @@
+package com.freshdirect.fdstore.util;
+
+public class IgnoreCaseString implements Comparable<IgnoreCaseString> {
+	final String string;
+	
+	public IgnoreCaseString(String string) {
+		super();
+		if (string == null)
+			throw new IllegalArgumentException();
+		this.string = string;
+	}
+
+	@Override
+	public int hashCode() {
+		return string.toLowerCase().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (obj instanceof String)
+			return equals(new IgnoreCaseString((String) obj));
+		if (getClass() != obj.getClass())
+			return false;
+		IgnoreCaseString other = (IgnoreCaseString) obj;
+		if (!string.equalsIgnoreCase(other.string))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return string;
+	}
+
+	@Override
+	public int compareTo(IgnoreCaseString o) {
+		return string.compareToIgnoreCase(o.string);
+	}
+}
