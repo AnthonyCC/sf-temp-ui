763a7eead01677acf4a4edb7a83b01709e342303
kkanuganti <kkanuganti@842219ff-9341-0410-a943-573460bdd4de>
2010-07-15 20:18:54 +0000

    APPDEV-761:Data Collection Changes.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@10174 842219ff-9341-0410-a943-573460bdd4de

--- projects/TransportationAdmin/src/com/freshdirect/transadmin/web/LocationController.java
+++ projects/TransportationAdmin/src/com/freshdirect/transadmin/web/LocationController.java
@@ -21,8 +21,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import lcc.japi.trans;
-
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.servlet.ModelAndView;
 
