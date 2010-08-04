ff4b78f877d2167fc82f3f8f0f522c230d35ad20
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2010-08-04 02:13:40 +0000

    APPDEV-761 Fixed ZoneType Mapping
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@10614 842219ff-9341-0410-a943-573460bdd4de

--- projects/RoutingServices/src/com/freshdirect/routing/manager/EstimationManager.java
+++ projects/RoutingServices/src/com/freshdirect/routing/manager/EstimationManager.java
@@ -1,15 +1,14 @@
 package com.freshdirect.routing.manager;
 
+import java.util.Map;
+
 import com.freshdirect.routing.constants.EnumProcessInfoType;
 import com.freshdirect.routing.constants.EnumProcessType;
-import com.freshdirect.routing.model.IDeliveryModel;
-import com.freshdirect.routing.model.ILocationModel;
-import com.freshdirect.routing.model.IOrderModel;
 import com.freshdirect.routing.model.IServiceTimeScenarioModel;
+import com.freshdirect.routing.model.IServiceTimeTypeModel;
 import com.freshdirect.routing.service.exception.RoutingProcessException;
 import com.freshdirect.routing.service.exception.RoutingServiceException;
 import com.freshdirect.routing.service.proxy.DeliveryServiceProxy;
-import com.freshdirect.routing.util.RoutingServicesProperties;
 
 
 public class EstimationManager extends BaseProcessManager {
@@ -21,8 +20,34 @@ public class EstimationManager extends BaseProcessManager {
 								
 		double serviceTime = 0.0;
 		try {
+			Map<String, IServiceTimeTypeModel> serviceTimeTypeMapping = (Map<String, IServiceTimeTypeModel>)request.getServiceTimeTypeCache();
+			
+			if(serviceTimeTypeMapping != null && request.getOrderInfo().getDeliveryInfo() != null) {
+				if(request.getOrderInfo().getDeliveryInfo().getDeliveryZone() != null
+						&& request.getOrderInfo().getDeliveryInfo().getDeliveryZone().getServiceTimeType().getCode() != null) {
+					request.getOrderInfo().getDeliveryInfo()
+							.getDeliveryZone().setServiceTimeType(serviceTimeTypeMapping.get(request.getOrderInfo().getDeliveryInfo()
+																			.getDeliveryZone().getServiceTimeType().getCode()));
+				} else {
+					request.getOrderInfo().getDeliveryInfo().getDeliveryZone().setServiceTimeType(null);
+				}
+				if(request.getOrderInfo().getDeliveryInfo().getDeliveryLocation().getServiceTimeType() != null) {
+					request.getOrderInfo().getDeliveryInfo().getDeliveryLocation().setServiceTimeType(serviceTimeTypeMapping
+																.get(request.getOrderInfo().getDeliveryInfo().getDeliveryLocation()
+																				.getServiceTimeType().getCode()));
+				} else {
+					request.getOrderInfo().getDeliveryInfo().getDeliveryLocation().setServiceTimeType(null);
+				}
+				if(request.getOrderInfo().getDeliveryInfo().getDeliveryLocation().getBuilding() != null 
+						&& request.getOrderInfo().getDeliveryInfo().getDeliveryLocation().getBuilding().getServiceTimeType() != null) {
+					request.getOrderInfo().getDeliveryInfo().getDeliveryLocation().getBuilding().setServiceTimeType(serviceTimeTypeMapping
+							.get(request.getOrderInfo().getDeliveryInfo().getDeliveryLocation().getBuilding().getServiceTimeType().getCode()));
+				} else {
+					request.getOrderInfo().getDeliveryInfo().getDeliveryLocation().getBuilding().setServiceTimeType(null);
+				}
+			}
 			serviceTime = proxy.getServiceTime(request.getOrderInfo(), scenario);
-		} catch(RoutingServiceException e) {
+		} catch(Exception e) {
 			ProcessInfo  processInfo = new ProcessInfo();
 			processInfo.setProcessType(EnumProcessType.LOAD_SERVICETIME);
 			processInfo.setProcessInfoType(EnumProcessInfoType.WARNING);
