4e13f3dbf513345dd2830ca86a593fd630f42899
ztreer <ztreer@842219ff-9341-0410-a943-573460bdd4de>
2010-08-25 14:56:34 +0000

    [APPDEV-672] added missing sql script
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@10907 842219ff-9341-0410-a943-573460bdd4de

---
+++ release/db/APPDEV-672_cms_4mm_layouts_DBSTG_DBSTO.sql
@@ -0,0 +1,3 @@
+INSERT INTO cms.lookup (LOOKUPTYPE_CODE,CODE,LABEL,DESCRIPTION,ORDINAL) VALUES ('Product.LAYOUT','40','4minute-meals landing page','4minute-meals landing page',29);
+INSERT INTO cms.lookup (LOOKUPTYPE_CODE,CODE,LABEL,DESCRIPTION,ORDINAL) VALUES ('Product.LAYOUT','41','4minute-meals restaurant page','4minute-meals restaurant page',30);
+
