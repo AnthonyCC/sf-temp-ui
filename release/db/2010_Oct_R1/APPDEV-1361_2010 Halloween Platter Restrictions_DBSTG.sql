bcc17e11beebf0af9270544dc4d3dae123d01275
kkanuganti <kkanuganti@842219ff-9341-0410-a943-573460bdd4de>
2010-10-01 16:05:28 +0000

    APPDEV-1361: 2010 Halloween Platter Restrictions DB scripts.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@11251 842219ff-9341-0410-a943-573460bdd4de

---
+++ release/db/2010_Oct_R1/APPDEV-1361_2010 Halloween Platter Restrictions_DBSTG.sql
@@ -0,0 +1,119 @@
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720056', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720054', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720100', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720176', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720177', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720197', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720044', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300800046', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720055', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300800047', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720198', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720025', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720209', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720222', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720223', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720014', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720073', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720155', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720163', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720050', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720140', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720041', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720164', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720165', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720020', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720200', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720091', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720092', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720076', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720023', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720106', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000400550039', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720040', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720026', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720169', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720207', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300720206', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300800110', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+insert into ERPS.ATTRIBUTES ( ID, ROOT_ID, CHILD1_ID, CHILD2_ID, ATR_TYPE, ATR_NAME, ATR_VALUE, DATE_MODIFIED )
+    VALUES (erps.system_seq.nextval, '000000000300800109', NULL, NULL, 'S', 'restrictions', 'TMPA',  sysdate);
+
+
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720056';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720054';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720100';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720176';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720177';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720197';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720044';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300800046';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300800047';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720055';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720198';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720025';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720209';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720222';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720223';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720014';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720073';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720155';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720163';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720050';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720140';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720041';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720164';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720165';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720020';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720200';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720091';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720092';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720076';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720023';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720106';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000400550039';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720040';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720026';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720169';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720207';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300720206';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300800110';
+update ERPS.ATTRIBUTES set date_modified=sysdate where ROOT_ID='000000000300800109';
