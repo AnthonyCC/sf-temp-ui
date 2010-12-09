1aac83ef00b04967478b36e37ef34dc14b040e5d
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2010-12-09 22:56:32 +0000

    (APPDEV-1428) Release DB folders Initial Update.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@11878 842219ff-9341-0410-a943-573460bdd4de

---
+++ release/db/2010_Nov_R1/APPDEV-1428_WAVEDROPREENG_DBSTG_DBSTO.sql
@@ -0,0 +1,26 @@
+alter table TRANSP.SCRIB add  (CUTOFF_DATETIME DATE);
+alter table TRANSP.PLAN add  (LAST_DLV_TIME DATE , CUTOFF_DATETIME DATE DEFAULT to_date('1970/01/01:06:00:00PM', 'yyyy/mm/dd:hh:mi:ssam'));
+alter table TRANSP.ZONE add (LOADING_PRIORITY INTEGER);
+alter table TRANSP.HANDOFF_BATCHROUTE add (DISPATCHTIME   DATE, DISPATCHSEQUENCE INTEGER);
+
+CREATE TABLE TRANSP.HANDOFF_BATCHDISPATCH
+(
+  DELIVERY_DATE   DATE             NOT NULL,
+  DISPATCHTIME   DATE             NOT NULL,
+  PLANNED_RESOURCES INTEGER,
+  ACTUAL_RESOURCES INTEGER,
+  STATUS VARCHAR2(10 BYTE)
+);
+
+ALTER TABLE TRANSP.HANDOFF_BATCHDISPATCH ADD (CONSTRAINT PK_HANDOFF_BATCHDISPATCH  PRIMARY KEY (DELIVERY_DATE, DISPATCHTIME));
+
+update transp.scrib s set S.CUTOFF_DATETIME = (select t.CUTOFF_TIME from dlv.timeslot t, dlv.zone z where t.BASE_DATE = S.SCRIB_DATE and t.ZONE_ID = z.ID and Z.ZONE_CODE = S.ZONE_ID 
+        and t.START_TIME = S.FIRST_WINDOW_TIME );
+        
+update transp.plan s set S.CUTOFF_DATETIME = (select t.CUTOFF_TIME from dlv.timeslot t, dlv.zone z where t.BASE_DATE = S.PLAN_DATE and t.ZONE_ID = z.ID and Z.ZONE_CODE = S.ZONE 
+        and t.START_TIME = S.FIRST_DLV_TIME );
+        
+update transp.plan s set S.LAST_DLV_TIME = (select t.END_WINDOW_TIME from transp.scrib t where t.SCRIB_DATE = S.PLAN_DATE and t.ZONE_ID = s.ZONE and t.START_TIME = S.START_TIME 
+        and t.FIRST_WINDOW_TIME = S.FIRST_DLV_TIME );         
+
+
