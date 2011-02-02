e88517c9ac347c1a4e4467f12b113504584b9701
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2011-02-02 20:06:18 +0000

    [APPDEV-661]- Bug Fixes - CRM Security Upgrade.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@12255 842219ff-9341-0410-a943-573460bdd4de

--- projects/DataLoader/src/com/freshdirect/dataloader/payment/AutoCreditApprovalCron.java
+++ projects/DataLoader/src/com/freshdirect/dataloader/payment/AutoCreditApprovalCron.java
@@ -42,7 +42,7 @@ public class AutoCreditApprovalCron {
 				String complaintId = (String) i.next();
 				LOGGER.info("Auto approve STARTED for complaint ID : " + complaintId);
 				try {
-					sb.approveComplaint(complaintId, true, "SYSTEM", true,2000.0);
+					sb.approveComplaint(complaintId, true, "SYSTEM", true,ErpServicesProperties.getCreditAutoApproveAmount());
 					LOGGER.info("Auto approve FINISHED for comolaint ID : " + complaintId);
 				} catch (ErpComplaintException ex) {
 					LOGGER.warn("Auto approve FAILED for complaint ID : " + complaintId);
