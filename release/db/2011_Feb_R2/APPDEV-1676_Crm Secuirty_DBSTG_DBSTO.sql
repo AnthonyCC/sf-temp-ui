82ef4f676ba6945100e8bb3b3abb422f2a4d9176
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2011-03-14 21:02:29 +0000

    [APPDEV-1676]- CRM Security Upgrade.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@12801 842219ff-9341-0410-a943-573460bdd4de

--- release/db/2011_Feb_R2/APPDEV-1676_Crm Secuirty_DBSTG_DBSTO.sql
+++ release/db/2011_Feb_R2/APPDEV-1676_Crm Secuirty_DBSTG_DBSTO.sql
@@ -1,4 +1,4 @@
 
 Insert into CUST.CASE_OPERATION   (CASE_SUBJECT, START_CASE_STATE, END_CASE_STATE, ROLE, CASEACTION_TYPE) select 'DSQ-014',START_CASE_STATE, END_CASE_STATE, ROLE, CASEACTION_TYPE from CUST.CASE_OPERATION where CASE_SUBJECT='DSQ-002' and ROLE in('NCS','CSR','SCS');
 
-update cust.case c set C.CASE_STATE='OPEN' where c.case_state='REVW';
+update cust.case c set C.CASE_STATE='CLSD' where c.case_state='REVW';
