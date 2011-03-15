57288fcd68034b4cbde0d2e1b18c9e3975a315d4
mshetty <mshetty@842219ff-9341-0410-a943-573460bdd4de>
2011-03-15 21:59:16 +0000

    [APPDEV-1688] Fix for pricing zone id not sent to SAP during NSM order submission.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@12824 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/customer/ejb/ErpOrderLinePersistentBean.java
+++ projects/ERPServices/src/com/freshdirect/customer/ejb/ErpOrderLinePersistentBean.java
@@ -259,6 +259,8 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
         this.model.setVariantId(rs.getString("VARIANT_ID"));
         this.model.setSavingsId(rs.getString("SAVINGS_ID"));
         String pricingZoneId=rs.getString("PRICING_ZONE_ID");
+        this.model.setPricingZoneId(pricingZoneId);
+        
         String grpId = rs.getString("GROUP_ID");
         int version = rs.getInt("GROUP_VERSION");
         if(grpId!=null)
