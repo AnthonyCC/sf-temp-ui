5c8221ebefd12f18e77aa1b3c925d8375fd8d0a0
gsebestyen <gsebestyen@842219ff-9341-0410-a943-573460bdd4de>
2011-04-01 13:33:20 +0000

    [APPDEV-1674] distribution script
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@12997 842219ff-9341-0410-a943-573460bdd4de

---
+++ release/db/2011_Apr_R1/APPDEV-1674_scarab_rollout_to_more_cohorts.sql
@@ -0,0 +1,38 @@
+--
+-- Change cohort distributions according to requirements of the ticket APPDEV-1674
+--
+-- The desired configuration is (as per Viktor):
+--   sc_related2: C3, C4, C10, C19
+--   sc_related_merch: C6
+--   ymal_ss_c3: everything else (C1, C2, C5, C7, C8, C9, C11, C12, C13, C14, C15, C16, C17, C18, C20)
+--
+-- Changes:
+--   [C6] ymal_ss_c3 -> sc_related_merch
+--   [C10] ymal_ss_c3 -> sc_related2
+--
+
+-- Drop old assignments
+DELETE FROM CUST.SS_VARIANT_ASSIGNMENT WHERE variant_id in ('ymal_ss_c3', 'sc_related2', 'sc_related_merch');
+
+-- Make new distribution
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C1', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C2', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C3', sysdate, 'sc_related2');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C4', sysdate, 'sc_related2');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C5', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C6', sysdate, 'sc_related_merch');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C7', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C8', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C9', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C10', sysdate, 'sc_related2');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C11', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C12', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C13', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C14', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C15', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C16', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C17', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C18', sysdate, 'ymal_ss_c3');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C19', sysdate, 'sc_related2');
+INSERT INTO CUST.SS_VARIANT_ASSIGNMENT (cohort_id, "DATE", variant_id) VALUES ('C20', sysdate, 'ymal_ss_c3');
+
