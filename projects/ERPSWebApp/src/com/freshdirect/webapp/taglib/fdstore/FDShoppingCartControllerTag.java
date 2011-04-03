eb90ec27d83d024209661fe65cafe9d987fa24c0
batchley <batchley@842219ff-9341-0410-a943-573460bdd4de>
2011-04-03 19:58:27 +0000

    temp build fix
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@13008 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/FDShoppingCartControllerTag.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/FDShoppingCartControllerTag.java
@@ -1177,9 +1177,11 @@ public class FDShoppingCartControllerTag extends BodyTagSupport implements Sessi
 			pricingZoneId = user.getPricingZoneId();
 		}
 		
+		//temp revert for build fix
 		//FDGroup originalGrp = originalLine.getOriginalGroup();
-		
-		//FDCartLineI theCartLine = processSimple(suffix, prodNode, product, quantity, salesUnit, origCartLineId, variantId, pricingZoneId ,originalGrp);
+		FDGroup originalGrp = originalLine.getFDGroup();
+				
+		FDCartLineI theCartLine = processSimple(suffix, prodNode, product, quantity, salesUnit, origCartLineId, variantId, pricingZoneId ,originalGrp);
 
 		// recipe source tracking
 		String recipeId;
