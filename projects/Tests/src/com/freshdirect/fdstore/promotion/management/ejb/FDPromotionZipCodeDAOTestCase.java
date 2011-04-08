d322bcb11b9cb624cb49e2569344ef223d3bbfd2
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2011-04-08 15:36:51 +0000

    Fixes for Promo Engine Test cases.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@13097 842219ff-9341-0410-a943-573460bdd4de

--- projects/Tests/src/com/freshdirect/fdstore/promotion/management/ejb/FDPromotionZipCodeDAOTestCase.java
+++ projects/Tests/src/com/freshdirect/fdstore/promotion/management/ejb/FDPromotionZipCodeDAOTestCase.java
@@ -12,8 +12,8 @@ import java.util.TreeMap;
 import com.freshdirect.DbTestCaseSupport;
 import com.freshdirect.fdstore.promotion.AssignedCustomerParam;
 import com.freshdirect.fdstore.promotion.management.FDPromoZipRestriction;
-import com.freshdirect.fdstore.promotion.management.FDPromotionModel;
-import com.freshdirect.fdstore.promotion.management.ejb.FDPromotionManagerDAO;
+import com.freshdirect.fdstore.promotion.management.FDPromotionNewModel;
+import com.freshdirect.fdstore.promotion.management.ejb.FDPromotionManagerNewDAO;
 import com.freshdirect.framework.core.PrimaryKey;
 import com.freshdirect.framework.util.DateUtil;
 
@@ -40,10 +40,13 @@ public class FDPromotionZipCodeDAOTestCase extends DbTestCaseSupport {
 		return new String[] {
 			"CUST.CUSTOMER",
 			"CUST.CAMPAIGN",
-			"CUST.PROMOTION",
-			"CUST.PROMO_GEOGRAPHY",
-			"CUST.PROMO_GEOGRAPHY_DATA",
-			"CUST.PROMO_CUSTOMER"
+			
+					
+			"CUST.PROMOTION_NEW",
+			"CUST.PROMO_CUSTOMER",
+			"CUST.PROMO_GEOGRAPHY_NEW",
+			"CUST.PROMO_GEOGRAPHY_DATA_NEW"
+				
 			};
 	}
 	
@@ -51,42 +54,42 @@ public class FDPromotionZipCodeDAOTestCase extends DbTestCaseSupport {
 		//setup
 		this.setUpDataSet("FDPromotionManagerDAO-init.xml");
 		
-		FDPromotionModel p1 = new FDPromotionModel();
-		p1.setTmpAssignedCustomerUserIds("test@test.com");
-		p1.setCategoryName("");
+		FDPromotionNewModel p1 = new FDPromotionNewModel();
+//		p1.setTmpAssignedCustomerUserIds("test@test.com");
+		p1.setCategoryName("abc");
 		p1.setDescription("Promotion test description");
-		p1.setExcludeBrands("1000,1001,1002");
-		p1.setExcludeSkuPrefix("80*");
+//		p1.setExcludeBrands("1000,1001,1002");
+//		p1.setExcludeSkuPrefix("80*");
 		Date expDate = new Date();
 		p1.setExpirationDate(DateUtil.truncate(expDate));
 		p1.setMaxAmount("25");
 		p1.setPercentOff("");
-		p1.setIsOrderTypeCorporateAllowed(true);
-		p1.setIsOrderTypeDepotAllowed(true);
-		p1.setIsOrderTypeHomeAllowed(true);
-		p1.setIsOrderTypePickupAllowed(true);
+//		p1.setIsOrderTypeCorporateAllowed(true);
+//		p1.setIsOrderTypeDepotAllowed(true);
+//		p1.setIsOrderTypeHomeAllowed(true);
+//		p1.setIsOrderTypePickupAllowed(true);
 		p1.setMaxUsage("1");
 		p1.setMinSubtotal("40");
 		p1.setName("Test zipcode values1");
-		p1.setNeedBrands("XXX,YYY");
-		p1.setNeedItemsFrom("2000,2001");
-		p1.setNeedsDryGoods(true);
-		p1.setOrderCount("3");
+//		p1.setNeedBrands("XXX,YYY");
+//		p1.setNeedItemsFrom("2000,2001");
+//		p1.setNeedsDryGoods(true);
+//		p1.setOrderCount("3");
 		p1.setPromotionCode("Test zipcode1");
 		p1.setPromotionType("REDEMPTION");
-		p1.setRedemptionCode("XYZ");
-		p1.setRefProgCampaignCode("REF_PROG_1");
-		p1.setIsMaxUsagePerCustomer(true);
+		p1.setRedemptionCode("XYZ3");
+//		p1.setRefProgCampaignCode("REF_PROG_1");
+//		p1.setIsMaxUsagePerCustomer(true);
 		p1.setRollingExpirationDays(new Integer(3));
 		Date startDate = new Date();
 		p1.setStartDate(DateUtil.truncate(startDate));
-		p1.setUniqueUse(true);
-		p1.setActive(true);
-		p1.setNotes("Some useful test note");
+//		p1.setUniqueUse(true);
+//		p1.setActive(true);
+//		p1.setNotes("Some useful test note");
 		p1.setModifiedBy("yuva");
 		// execute
 
-		PrimaryKey pk = FDPromotionManagerDAO.createPromotion(conn, p1);
+		PrimaryKey pk = FDPromotionManagerNewDAO.createPromotion(conn, p1);
 		
 		p1.setId(pk.getId());
 		
@@ -94,37 +97,37 @@ public class FDPromotionZipCodeDAOTestCase extends DbTestCaseSupport {
 		assertNotNull(pk);
 		assertTrue("Primary key is blank", !"".equals(pk.getId()));
 		
-		FDPromotionModel p2 = FDPromotionManagerDAO.getPromotion(conn, p1.getPromotionCode());
+		FDPromotionNewModel p2 = FDPromotionManagerNewDAO.getPromotion(conn, p1.getPromotionCode());
 		//assertTrue("No Zip Code Restrictions", !p2.getZipRestrictions().isEmpty());
 
-		assertEquals(p1.getTmpAssignedCustomerUserIds(), p2.getAssignedCustomerUserIds());
+//		assertEquals(p1.getTmpAssignedCustomerUserIds(), p2.getAssignedCustomerUserIds());
 		assertEquals(p1.getCategoryName(), p2.getCategoryName());
 		assertEquals(p1.getDescription(), p2.getDescription());
-		assertEquals(p1.getExcludeBrands(), p2.getExcludeBrands());
-		assertEquals(p1.getExcludeSkuPrefix(), p2.getExcludeSkuPrefix());
+//		assertEquals(p1.getExcludeBrands(), p2.getExcludeBrands());
+//		assertEquals(p1.getExcludeSkuPrefix(), p2.getExcludeSkuPrefix());
 		assertEquals(p1.getExpirationDate(), DateUtil.truncate(p2.getExpirationDate()));
 		assertEquals(p1.getId(), p2.getId());
 		assertEquals(Double.valueOf(p1.getMaxAmount()), Double.valueOf(p2.getMaxAmount()));
-		assertEquals(p1.isOrderTypeCorporateAllowed(), p2.isOrderTypeCorporateAllowed());
-		assertEquals(p1.isOrderTypeDepotAllowed(), p2.isOrderTypeDepotAllowed());
-		assertEquals(p1.isOrderTypeHomeAllowed(), p2.isOrderTypeHomeAllowed());
-		assertEquals(p1.isOrderTypePickupAllowed(), p2.isOrderTypePickupAllowed());
+//		assertEquals(p1.isOrderTypeCorporateAllowed(), p2.isOrderTypeCorporateAllowed());
+//		assertEquals(p1.isOrderTypeDepotAllowed(), p2.isOrderTypeDepotAllowed());
+//		assertEquals(p1.isOrderTypeHomeAllowed(), p2.isOrderTypeHomeAllowed());
+//		assertEquals(p1.isOrderTypePickupAllowed(), p2.isOrderTypePickupAllowed());
 		assertEquals(Integer.valueOf(p1.getMaxUsage()), Integer.valueOf(p2.getMaxUsage()));
 		assertEquals(Double.valueOf(p1.getMinSubtotal()), Double.valueOf(p2.getMinSubtotal()));
 		assertEquals(p1.getName(), p2.getName());
-		assertEquals(p1.getNeedBrands(), p2.getNeedBrands());
-		assertEquals(p1.getNeedItemsFrom(), p2.getNeedItemsFrom());
-		assertEquals(p1.getNeedDryGoods(), p2.getNeedDryGoods());
-		assertEquals(Integer.valueOf(p1.getOrderCount()), Integer.valueOf(p2.getOrderCount()));
+//		assertEquals(p1.getNeedBrands(), p2.getNeedBrands());
+//		assertEquals(p1.getNeedItemsFrom(), p2.getNeedItemsFrom());
+//		assertEquals(p1.getNeedDryGoods(), p2.getNeedDryGoods());
+//		assertEquals(Integer.valueOf(p1.getOrderCount()), Integer.valueOf(p2.getOrderCount()));
 		assertEquals(p1.getPromotionCode(), p2.getPromotionCode());
 		assertEquals(p1.getPromotionType(), p2.getPromotionType());
 		assertEquals(p1.getRedemptionCode(), p2.getRedemptionCode());
 		assertEquals(p1.getStartDate(), DateUtil.truncate(p2.getStartDate()));
-		assertEquals(p1.getRefProgCampaignCode(), p2.getRefProgCampaignCode());
-		assertEquals(p1.isMaxUsagePerCustomer(),p2.isMaxUsagePerCustomer()); 
+//		assertEquals(p1.getRefProgCampaignCode(), p2.getRefProgCampaignCode());
+//		assertEquals(p1.isMaxUsagePerCustomer(),p2.isMaxUsagePerCustomer()); 
 		assertEquals(p1.getRollingExpirationDays().intValue(),p2.getRollingExpirationDays().intValue()); 
-		assertEquals(p1.isUniqueUse(), p2.isUniqueUse());
-		assertEquals(p1.getNotes(), p2.getNotes());
+//		assertEquals(p1.isUniqueUse(), p2.isUniqueUse());
+//		assertEquals(p1.getNotes(), p2.getNotes());
 		
 	}
 	
@@ -132,42 +135,42 @@ public class FDPromotionZipCodeDAOTestCase extends DbTestCaseSupport {
 //		 setup
 		this.setUpDataSet("FDPromotionManagerDAO-init.xml");
 		
-		FDPromotionModel p1 = new FDPromotionModel();
-		p1.setTmpAssignedCustomerUserIds("test@test.com");
-		p1.setCategoryName("");
+		FDPromotionNewModel p1 = new FDPromotionNewModel();
+//		p1.setTmpAssignedCustomerUserIds("test@test.com");
+		p1.setCategoryName("abc");
 		p1.setDescription("Promotion test description");
-		p1.setExcludeBrands("1000,1001,1002");
-		p1.setExcludeSkuPrefix("80*");
+//		p1.setExcludeBrands("1000,1001,1002");
+//		p1.setExcludeSkuPrefix("80*");
 		Date expDate = new Date();
 		p1.setExpirationDate(DateUtil.truncate(expDate));
 		p1.setMaxAmount("25");
 		p1.setPercentOff("");
-		p1.setIsOrderTypeCorporateAllowed(true);
-		p1.setIsOrderTypeDepotAllowed(true);
-		p1.setIsOrderTypeHomeAllowed(true);
-		p1.setIsOrderTypePickupAllowed(true);
+//		p1.setIsOrderTypeCorporateAllowed(true);
+//		p1.setIsOrderTypeDepotAllowed(true);
+//		p1.setIsOrderTypeHomeAllowed(true);
+//		p1.setIsOrderTypePickupAllowed(true);
 		p1.setMaxUsage("1");
 		p1.setMinSubtotal("40");
 		p1.setName("Test zipcode values1");
-		p1.setNeedBrands("XXX,YYY");
-		p1.setNeedItemsFrom("2000,2001");
-		p1.setNeedsDryGoods(true);
-		p1.setOrderCount("3");
+//		p1.setNeedBrands("XXX,YYY");
+//		p1.setNeedItemsFrom("2000,2001");
+//		p1.setNeedsDryGoods(true);
+//		p1.setOrderCount("3");
 		p1.setPromotionCode("Test zipcode1");
 		p1.setPromotionType("REDEMPTION");
-		p1.setRedemptionCode("XYZ");
-		p1.setRefProgCampaignCode("REF_PROG_1");
-		p1.setIsMaxUsagePerCustomer(true);
+		p1.setRedemptionCode("XYZ4");
+//		p1.setRefProgCampaignCode("REF_PROG_1");
+//		p1.setIsMaxUsagePerCustomer(true);
 		p1.setRollingExpirationDays(new Integer(3));
 		Date startDate = new Date();
 		p1.setStartDate(DateUtil.truncate(startDate));
-		p1.setUniqueUse(true);
-		p1.setActive(true);
-		p1.setNotes("Some useful test note");
+//		p1.setUniqueUse(true);
+//		p1.setActive(true);
+//		p1.setNotes("Some useful test note");
 		p1.setModifiedBy("yuva");
 		// execute
 
-		PrimaryKey pk = FDPromotionManagerDAO.createPromotion(conn, p1);
+		PrimaryKey pk = FDPromotionManagerNewDAO.createPromotion(conn, p1);
 		
 		p1.setId(pk.getId());
 		p1.setPK(pk);
@@ -188,37 +191,37 @@ public class FDPromotionZipCodeDAOTestCase extends DbTestCaseSupport {
 		zipRestrictions.put(DF.parse("2006-05-15"), zp1);
 		p1.setZipRestrictions(zipRestrictions);
 		
-		FDPromotionManagerDAO.storePromotion(conn, p1);
+		FDPromotionManagerNewDAO.storePromotion(conn, p1);
 		
-		FDPromotionModel p2 = FDPromotionManagerDAO.getPromotion(conn, p1.getPromotionCode());
-		assertEquals(p1.getTmpAssignedCustomerUserIds(), p2.getAssignedCustomerUserIds());
+		FDPromotionNewModel p2 = FDPromotionManagerNewDAO.getPromotion(conn, p1.getPromotionCode());
+//		assertEquals(p1.getTmpAssignedCustomerUserIds(), p2.getAssignedCustomerUserIds());
 		assertEquals(p1.getCategoryName(), p2.getCategoryName());
 		assertEquals(p1.getDescription(), p2.getDescription());
-		assertEquals(p1.getExcludeBrands(), p2.getExcludeBrands());
-		assertEquals(p1.getExcludeSkuPrefix(), p2.getExcludeSkuPrefix());
+//		assertEquals(p1.getExcludeBrands(), p2.getExcludeBrands());
+//		assertEquals(p1.getExcludeSkuPrefix(), p2.getExcludeSkuPrefix());
 		assertEquals(DateUtil.truncate(p1.getExpirationDate()), DateUtil.truncate(p2.getExpirationDate()));
 		assertEquals(p1.getId(), p2.getId());
 		assertEquals(Double.valueOf(p1.getMaxAmount()), Double.valueOf(p2.getMaxAmount()));
-		assertEquals(p1.isOrderTypeCorporateAllowed(), p2.isOrderTypeCorporateAllowed());
-		assertEquals(p1.isOrderTypeDepotAllowed(), p2.isOrderTypeDepotAllowed());
-		assertEquals(p1.isOrderTypeHomeAllowed(), p2.isOrderTypeHomeAllowed());
-		assertEquals(p1.isOrderTypePickupAllowed(), p2.isOrderTypePickupAllowed());
+//		assertEquals(p1.isOrderTypeCorporateAllowed(), p2.isOrderTypeCorporateAllowed());
+//		assertEquals(p1.isOrderTypeDepotAllowed(), p2.isOrderTypeDepotAllowed());
+//		assertEquals(p1.isOrderTypeHomeAllowed(), p2.isOrderTypeHomeAllowed());
+//		assertEquals(p1.isOrderTypePickupAllowed(), p2.isOrderTypePickupAllowed());
 		assertEquals(Integer.valueOf(p1.getMaxUsage()), Integer.valueOf(p2.getMaxUsage()));
 		assertEquals(Double.valueOf(p1.getMinSubtotal()), Double.valueOf(p2.getMinSubtotal()));
 		assertEquals(p1.getName(), p2.getName());
-		assertEquals(p1.getNeedBrands(), p2.getNeedBrands());
-		assertEquals(p1.getNeedItemsFrom(), p2.getNeedItemsFrom());
-		assertEquals(p1.getNeedDryGoods(), p2.getNeedDryGoods());
-		assertEquals(Integer.valueOf(p1.getOrderCount()), Integer.valueOf(p2.getOrderCount()));
+//		assertEquals(p1.getNeedBrands(), p2.getNeedBrands());
+//		assertEquals(p1.getNeedItemsFrom(), p2.getNeedItemsFrom());
+//		assertEquals(p1.getNeedDryGoods(), p2.getNeedDryGoods());
+//		assertEquals(Integer.valueOf(p1.getOrderCount()), Integer.valueOf(p2.getOrderCount()));
 		assertEquals(p1.getPromotionCode(), p2.getPromotionCode());
 		assertEquals(p1.getPromotionType(), p2.getPromotionType());
 		assertEquals(p1.getRedemptionCode(), p2.getRedemptionCode());
 		assertEquals(DateUtil.truncate(p1.getStartDate()), DateUtil.truncate(p2.getStartDate()));
-		assertEquals(p1.getRefProgCampaignCode(), p2.getRefProgCampaignCode());
-		assertEquals(p1.isMaxUsagePerCustomer(),p2.isMaxUsagePerCustomer()); 
+//		assertEquals(p1.getRefProgCampaignCode(), p2.getRefProgCampaignCode());
+//		assertEquals(p1.isMaxUsagePerCustomer(),p2.isMaxUsagePerCustomer()); 
 		assertEquals(p1.getRollingExpirationDays().intValue(),p2.getRollingExpirationDays().intValue()); 
-		assertEquals(p1.isUniqueUse(), p2.isUniqueUse());
-		assertEquals(p1.getNotes(), p2.getNotes());
+//		assertEquals(p1.isUniqueUse(), p2.isUniqueUse());
+//		assertEquals(p1.getNotes(), p2.getNotes());
 		assertNotSame(p1.getModifiedBy(), p2.getModifiedBy());
 		TreeMap p1ZipRestrictions = p1.getZipRestrictions();
 		TreeMap p2ZipRestrictions = p2.getZipRestrictions();
@@ -237,41 +240,41 @@ public class FDPromotionZipCodeDAOTestCase extends DbTestCaseSupport {
 		// setup
 		this.setUpDataSet("FDPromotionManagerDAO-init.xml");
 		
-		FDPromotionModel p1 = new FDPromotionModel();
-		p1.setTmpAssignedCustomerUserIds("test@test.com");
-		p1.setCategoryName("");
+		FDPromotionNewModel p1 = new FDPromotionNewModel();
+//		p1.setTmpAssignedCustomerUserIds("test@test.com");
+		p1.setCategoryName("abc");
 		p1.setDescription("Promotion test description");
-		p1.setExcludeBrands("1000,1001,1002");
-		p1.setExcludeSkuPrefix("80*");
+//		p1.setExcludeBrands("1000,1001,1002");
+//		p1.setExcludeSkuPrefix("80*");
 		Date expDate = new Date();
 		p1.setExpirationDate(DateUtil.truncate(expDate));
 		p1.setMaxAmount("25");
 		p1.setPercentOff("");
-		p1.setIsOrderTypeCorporateAllowed(true);
-		p1.setIsOrderTypeDepotAllowed(true);
-		p1.setIsOrderTypeHomeAllowed(true);
-		p1.setIsOrderTypePickupAllowed(true);
+//		p1.setIsOrderTypeCorporateAllowed(true);
+//		p1.setIsOrderTypeDepotAllowed(true);
+//		p1.setIsOrderTypeHomeAllowed(true);
+//		p1.setIsOrderTypePickupAllowed(true);
 		p1.setMaxUsage("1");
 		p1.setMinSubtotal("40");
 		p1.setName("Test zipcode values1");
-		p1.setNeedBrands("XXX,YYY");
-		p1.setNeedItemsFrom("2000,2001");
-		p1.setNeedsDryGoods(true);
-		p1.setOrderCount("3");
+//		p1.setNeedBrands("XXX,YYY");
+//		p1.setNeedItemsFrom("2000,2001");
+//		p1.setNeedsDryGoods(true);
+//		p1.setOrderCount("3");
 		p1.setPromotionCode("Test zipcode1");
 		p1.setPromotionType("REDEMPTION");
-		p1.setRedemptionCode("XYZ");
-		p1.setRefProgCampaignCode("REF_PROG_1");
-		p1.setIsMaxUsagePerCustomer(true);
+		p1.setRedemptionCode("XYZ5");
+//		p1.setRefProgCampaignCode("REF_PROG_1");
+//		p1.setIsMaxUsagePerCustomer(true);
 		p1.setRollingExpirationDays(new Integer(3));
 		Date startDate = new Date();
 		p1.setStartDate(DateUtil.truncate(startDate));
-		p1.setUniqueUse(true);
-		p1.setActive(true);
-		p1.setNotes("Some useful test note");
+//		p1.setUniqueUse(true);
+//		p1.setActive(true);
+//		p1.setNotes("Some useful test note");
 		p1.setModifiedBy("yuva");
 		// execute
-		PrimaryKey pk = FDPromotionManagerDAO.createPromotion(conn, p1);
+		PrimaryKey pk = FDPromotionManagerNewDAO.createPromotion(conn, p1);
 		
 		p1.setId(pk.getId());
 
@@ -292,7 +295,7 @@ public class FDPromotionZipCodeDAOTestCase extends DbTestCaseSupport {
 		zipRestrictions.put(DF.parse("2006-05-15"), zp1);
 		p1.setZipRestrictions(zipRestrictions);
 		
-		FDPromotionManagerDAO.storePromotion(conn, p1);
+		FDPromotionManagerNewDAO.storePromotion(conn, p1);
 		
 		TreeMap preZrMap = (TreeMap) p1.getZipRestrictions();
 		
@@ -307,37 +310,37 @@ public class FDPromotionZipCodeDAOTestCase extends DbTestCaseSupport {
 		
 		preZrMap.put(DF.parse("2006-05-30"), preZr2);
 		p1.setZipRestrictions(preZrMap);
-		FDPromotionManagerDAO.storePromotion(conn, p1);
+		FDPromotionManagerNewDAO.storePromotion(conn, p1);
 		
-		FDPromotionModel p2 = FDPromotionManagerDAO.getPromotion(conn, p1.getPromotionCode());
-		assertEquals(p1.getTmpAssignedCustomerUserIds(), p2.getAssignedCustomerUserIds());
+		FDPromotionNewModel p2 = FDPromotionManagerNewDAO.getPromotion(conn, p1.getPromotionCode());
+//		assertEquals(p1.getTmpAssignedCustomerUserIds(), p2.getAssignedCustomerUserIds());
 		assertEquals(p1.getCategoryName(), p2.getCategoryName());
 		assertEquals(p1.getDescription(), p2.getDescription());
-		assertEquals(p1.getExcludeBrands(), p2.getExcludeBrands());
-		assertEquals(p1.getExcludeSkuPrefix(), p2.getExcludeSkuPrefix());
+//		assertEquals(p1.getExcludeBrands(), p2.getExcludeBrands());
+//		assertEquals(p1.getExcludeSkuPrefix(), p2.getExcludeSkuPrefix());
 		assertEquals(DateUtil.truncate(p1.getExpirationDate()), DateUtil.truncate(p2.getExpirationDate()));
 		assertEquals(p1.getId(), p2.getId());
 		assertEquals(Double.valueOf(p1.getMaxAmount()), Double.valueOf(p2.getMaxAmount()));
-		assertEquals(p1.isOrderTypeCorporateAllowed(), p2.isOrderTypeCorporateAllowed());
-		assertEquals(p1.isOrderTypeDepotAllowed(), p2.isOrderTypeDepotAllowed());
-		assertEquals(p1.isOrderTypeHomeAllowed(), p2.isOrderTypeHomeAllowed());
-		assertEquals(p1.isOrderTypePickupAllowed(), p2.isOrderTypePickupAllowed());
+//		assertEquals(p1.isOrderTypeCorporateAllowed(), p2.isOrderTypeCorporateAllowed());
+//		assertEquals(p1.isOrderTypeDepotAllowed(), p2.isOrderTypeDepotAllowed());
+//		assertEquals(p1.isOrderTypeHomeAllowed(), p2.isOrderTypeHomeAllowed());
+//		assertEquals(p1.isOrderTypePickupAllowed(), p2.isOrderTypePickupAllowed());
 		assertEquals(Integer.valueOf(p1.getMaxUsage()), Integer.valueOf(p2.getMaxUsage()));
 		assertEquals(Double.valueOf(p1.getMinSubtotal()), Double.valueOf(p2.getMinSubtotal()));
 		assertEquals(p1.getName(), p2.getName());
-		assertEquals(p1.getNeedBrands(), p2.getNeedBrands());
-		assertEquals(p1.getNeedItemsFrom(), p2.getNeedItemsFrom());
-		assertEquals(p1.getNeedDryGoods(), p2.getNeedDryGoods());
-		assertEquals(Integer.valueOf(p1.getOrderCount()), Integer.valueOf(p2.getOrderCount()));
+//		assertEquals(p1.getNeedBrands(), p2.getNeedBrands());
+//		assertEquals(p1.getNeedItemsFrom(), p2.getNeedItemsFrom());
+//		assertEquals(p1.getNeedDryGoods(), p2.getNeedDryGoods());
+//		assertEquals(Integer.valueOf(p1.getOrderCount()), Integer.valueOf(p2.getOrderCount()));
 		assertEquals(p1.getPromotionCode(), p2.getPromotionCode());
 		assertEquals(p1.getPromotionType(), p2.getPromotionType());
 		assertEquals(p1.getRedemptionCode(), p2.getRedemptionCode());
 		assertEquals(DateUtil.truncate(p1.getStartDate()), DateUtil.truncate(p2.getStartDate()));
-		assertEquals(p1.getRefProgCampaignCode(), p2.getRefProgCampaignCode());
-		assertEquals(p1.isMaxUsagePerCustomer(),p2.isMaxUsagePerCustomer()); 
+//		assertEquals(p1.getRefProgCampaignCode(), p2.getRefProgCampaignCode());
+//		assertEquals(p1.isMaxUsagePerCustomer(),p2.isMaxUsagePerCustomer()); 
 		assertEquals(p1.getRollingExpirationDays().intValue(),p2.getRollingExpirationDays().intValue()); 
-		assertEquals(p1.isUniqueUse(), p2.isUniqueUse());
-		assertEquals(p1.getNotes(), p2.getNotes());
+//		assertEquals(p1.isUniqueUse(), p2.isUniqueUse());
+//		assertEquals(p1.getNotes(), p2.getNotes());
 		assertNotSame(p1.getModifiedBy(), p2.getModifiedBy());
 		TreeMap p2ZipRestrictions = p2.getZipRestrictions();
 		FDPromoZipRestriction postZr1 = (FDPromoZipRestriction) p2ZipRestrictions.get(DF.parse("2006-04-27"));
