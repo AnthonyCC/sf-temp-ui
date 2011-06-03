75f5fc96fbd9c02047d1d7e9aa035309731b2d42
ztreer <ztreer@842219ff-9341-0410-a943-573460bdd4de>
2011-06-03 10:32:35 +0000

    [APPDEV-1777] Extend brand-uniqueness sorter functionality to smart-categories
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@13652 842219ff-9341-0410-a943-573460bdd4de

---
+++ release/db/2011_May_R1/APPDEV-1777_brand_uniqeness_smart_categories.sql
@@ -0,0 +1,5 @@
+insert into cms.attributedefinition 
+(name,id,contenttype_id,attributetype_code,inheritable,required,label,cardinality_code) 
+values 
+('brand_uniq_sort','RecommenderStrategy.brand_uniq_sort','RecommenderStrategy','B','F','F','Brand Uniqueness Sorting','One');
+
