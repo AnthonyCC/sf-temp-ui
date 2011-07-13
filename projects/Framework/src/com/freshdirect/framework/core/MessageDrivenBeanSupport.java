2a90a19a77fcfb17c590f6f84641279311c3e95c
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2011-07-13 01:42:33 +0000

    [APPDEV-1732]:Timeslot Log Enhancement - Capacity Management Phase 1
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@14219 842219ff-9341-0410-a943-573460bdd4de

--- projects/Framework/src/com/freshdirect/framework/core/MessageDrivenBeanSupport.java
+++ projects/Framework/src/com/freshdirect/framework/core/MessageDrivenBeanSupport.java
@@ -8,8 +8,16 @@
  */
 package com.freshdirect.framework.core;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.jms.MessageListener;
 import javax.ejb.*;
+
+import org.apache.log4j.Logger;
+
+import com.freshdirect.framework.util.log.LoggerFactory;
 
 /**
  *
@@ -19,6 +27,8 @@ import javax.ejb.*;
  */
 public abstract class MessageDrivenBeanSupport implements MessageDrivenBean, MessageListener {
 	
+	final static Logger LOGGER = LoggerFactory.getInstance(MessageDrivenBeanSupport.class);
+	   
 	private MessageDrivenContext messageDrivenCtx;
 
 	public void ejbRemove() {
@@ -34,5 +44,45 @@ public abstract class MessageDrivenBeanSupport implements MessageDrivenBean, Mes
 	public MessageDrivenContext getMessageDrivenContext() {
 		return this.messageDrivenCtx;	
 	}
+	
+	   /**
+     * Retreives a database connection. Caches the underlying DataSource
+     * (using the key from template method <CODE>getResourceCacheKey()</CODE>).
+     *
+     * @see DataSourceLocator
+     *
+     * @throws SQLException if any problems were encountered while trying
+     * to find that DataSource that retreives connections from a pool
+     */
+    public Connection getConnection() throws SQLException {
+   		return DataSourceLocator.getConnection( this.getResourceCacheKey() );
+    }
+	/**
+	 * Template method that returns the cache key to use for caching resources.
+	 *
+	 * @return null if caching is not supported by this bean (default implementation).
+	 */
+    protected String getResourceCacheKey() {
+    	return null;
+    }
+    protected final void close(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException sqle) {
+            LOGGER.error("problem in closing connection : " + this.getClass().getName() + " err: " + sqle.getMessage(), sqle);
+        }
+    }
+    
+    protected final void close(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException sqle) {
+                LOGGER.error("problem in closing result set: " + this.getClass().getName() + " err: " + sqle.getMessage(), sqle);
+            }
+        }
+    }
 
 }
