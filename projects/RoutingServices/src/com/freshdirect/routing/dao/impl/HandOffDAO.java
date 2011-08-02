3e20f0cc9951ed8e6ca0083a76d98a40c5a874be
kkanuganti <kkanuganti@842219ff-9341-0410-a943-573460bdd4de>
2011-08-02 20:13:12 +0000

    APPDEV-804: Truck Assignment- TransApp
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@14512 842219ff-9341-0410-a943-573460bdd4de

--- projects/RoutingServices/src/com/freshdirect/routing/dao/impl/HandOffDAO.java
+++ projects/RoutingServices/src/com/freshdirect/routing/dao/impl/HandOffDAO.java
@@ -291,13 +291,17 @@ public class HandOffDAO extends BaseDAO implements IHandOffDAO   {
 	private static final String GET_HANDOFFBATCHDISPATCHSEQ_QRY = "select TRANSP.DISPATCHSEQ.nextval FROM DUAL";
 				
 	private static final String GET_HANDOFFBATCH_DISPATCHROUTES = "SELECT R.AREA, R.ROUTE_NO, R.STARTTIME, R.DISPATCHTIME, "+
-         "(SELECT min(S.WINDOW_STARTTIME) from TRANSP.HANDOFF_BATCHSTOP s where S.BATCH_ID = R.BATCH_ID and S.ROUTE_NO = R.ROUTE_NO ) FIRSTDLVTIME, "+                                                         
-         "decode(A.IS_DEPOT , 'X', (SELECT max(S.STOP_DEPARTUREDATETIME ) + 3/48  from TRANSP.HANDOFF_BATCHSTOP s where S.BATCH_ID = R.BATCH_ID and S.ROUTE_NO = R.ROUTE_NO), R.COMPLETETIME) COMPLETETIME "+ 
-         "from (select  BD.DISPATCHTIME as DT from TRANSP.HANDOFF_BATCHDISPATCHEX BD where BD.BATCH_ID = ? AND BD.STATUS = ? and exists(select 1 from TRANSP.HANDOFF_BATCHROUTE R "+
-         "where R.BATCH_ID = bd.batch_id  and bd.DISPATCHTIME=r.DISPATCHTIME)) T, transp.HANDOFF_BATCH b, TRANSP.HANDOFF_BATCHROUTE R, TRANSP.TRN_AREA A "+
-         "where B.DELIVERY_DATE = ? and B.BATCH_ID = R.BATCH_ID and R.AREA=A.CODE and B.BATCH_STATUS IN ('CPD/ADC','CPD','CPD/ADF') "+
-         "and R.DISPATCHTIME IN (select R.DISPATCHTIME from TRANSP.HANDOFF_BATCHROUTE R where R.BATCH_ID = ? ) "+
-         "and R.DISPATCHTIME = T.DT";
+       "(SELECT min(S.WINDOW_STARTTIME) from TRANSP.HANDOFF_BATCHSTOP s where S.BATCH_ID = R.BATCH_ID and S.ROUTE_NO = R.ROUTE_NO ) FIRSTDLVTIME, "+                                                          
+       "decode(A.IS_DEPOT , 'X', (SELECT max(S.STOP_DEPARTUREDATETIME ) + 3/48  from TRANSP.HANDOFF_BATCHSTOP s where S.BATCH_ID = R.BATCH_ID and S.ROUTE_NO = R.ROUTE_NO), R.COMPLETETIME) COMPLETETIME "+ 
+       "from (select dispatchtime as dt from ((select BD.DISPATCHTIME from  TRANSP.HANDOFF_BATCHDISPATCHEX BD where BD.BATCH_ID = ? AND BD.STATUS = ?) "+
+            "MINUS "+
+                "( select distinct BD.DISPATCHTIME  from TRANSP.HANDOFF_BATCHDISPATCHEX BD, transp.HANDOFF_BATCH b where "+ 
+                "b.batch_id=bd.batch_id and B.DELIVERY_DATE=(select delivery_date from TRANSP.HANDOFF_BATCH where batch_id = ?) "+
+                "and BD.BATCH_ID < to_number(?) AND BD.STATUS = ? and B.BATCH_STATUS IN ('CPD/ADC','CPD','CPD/ADF'))) " +
+                ") T, transp.HANDOFF_BATCH b, TRANSP.HANDOFF_BATCHROUTE R, TRANSP.TRN_AREA A " +
+         "where B.DELIVERY_DATE = ? "+ 
+         "and B.BATCH_ID = R.BATCH_ID and R.AREA=A.CODE and B.BATCH_STATUS IN ('CPD/ADC','CPD','CPD/ADF') "+
+         "and R.DISPATCHTIME = T.dt ";
 
 	private static final String GET_HANDOFFBATCH_PLANS = "SELECT * FROM TRANSP.PLAN P WHERE P.PLAN_DATE = ? "+
             "and P.START_TIME in (select B.DISPATCHTIME from TRANSP.HANDOFF_BATCHDISPATCHEX B where B.BATCH_ID = ? and B.STATUS = 'CPD' "+ 
@@ -1473,8 +1477,10 @@ public class HandOffDAO extends BaseDAO implements IHandOffDAO   {
 					connection.prepareStatement(GET_HANDOFFBATCH_DISPATCHROUTES);
 			    ps.setString(1, handoffBatchId);
 			    ps.setString(2, EnumHandOffDispatchStatus.COMPLETE.value());
-				ps.setDate(3, new java.sql.Date(deliveryDate.getTime()));
-				ps.setString(4, handoffBatchId);				
+				ps.setString(3, handoffBatchId);
+				ps.setString(4, handoffBatchId);
+				ps.setString(5, EnumHandOffDispatchStatus.COMPLETE.value());
+				ps.setDate(6, new java.sql.Date(deliveryDate.getTime()));
 				
 				return ps;
 			}  
