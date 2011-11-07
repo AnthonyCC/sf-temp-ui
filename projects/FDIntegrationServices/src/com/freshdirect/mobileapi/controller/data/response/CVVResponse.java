0739125d8232bdac67b68e12bac953ea8c3d0c50
skrishnasamy <skrishnasamy@842219ff-9341-0410-a943-573460bdd4de>
2011-11-07 05:43:33 +0000

    [APPDEV-1565]Adding the missing class
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@15586 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/FDIntegrationServices/src/com/freshdirect/mobileapi/controller/data/response/CVVResponse.java
@@ -0,0 +1,20 @@
+package com.freshdirect.mobileapi.controller.data.response;
+
+import com.freshdirect.common.customer.EnumServiceType;
+import com.freshdirect.delivery.EnumDeliveryStatus;
+import com.freshdirect.mobileapi.controller.data.Message;
+
+public class CVVResponse extends Message {
+	
+	private boolean enabled;
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	} 
+	
+
+}
