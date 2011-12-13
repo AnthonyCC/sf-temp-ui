9a028cd48828d4ad6721eb3e3277c40858a0254e
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2011-12-13 22:20:05 +0000

    APPBUG-577 expected orders in order rate needs changes
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@15942 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/analytics/BounceDAO.java
+++ projects/ERPServices/src/com/freshdirect/analytics/BounceDAO.java
@@ -27,7 +27,7 @@ public class BounceDAO {
 			"VALUES (?,?,?,?,?,?,?,?,?)";
 	private static final String CANCEL_BOUNCE = "update MIS.bounce_event set status = 'CANCELLED', lastupdate=sysdate where id in (SELECT be.id FROM cust.sale s, " +
 			"cust.salesaction sa, MIS.bounce_event be WHERE s.ID=sa.SALE_ID AND s.CUSTOMER_ID=sa.CUSTOMER_ID  and be.customer_id = s.customer_id" +
-			" and s.CROMOD_DATE=sa.ACTION_DATE AND sa.ACTION_TYPE IN ('CRO') AND sa.REQUESTED_DATE > TRUNC(SYSDATE)  and be.status = 'NEW' " +
+			" and s.CROMOD_DATE=sa.ACTION_DATE AND sa.ACTION_TYPE IN ('CRO', 'MOD') AND sa.REQUESTED_DATE > TRUNC(SYSDATE)  and be.status = 'NEW' " +
 			"and trunc(S.CROMOD_DATE) = trunc(be.createdate) AND s.type='REG' AND s.status <> 'CAN')";
 	
 	private static final String BOUNCE_SELECT = "select count(*) cnt, zone, cutoff, type, to_char(createdate, 'HH:MI AM') time from mis.bounce_event where status = 'NEW' and to_char(createdate, 'mm/dd/yyyy') = ? " +
