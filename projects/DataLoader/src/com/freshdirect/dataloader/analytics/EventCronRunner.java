85f93acca8ad7d3fe7782da02d02a7a47834952c
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2011-12-19 19:28:58 +0000

    APPBUG-577 expected orders in order rate needs changes
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@15956 842219ff-9341-0410-a943-573460bdd4de

--- projects/DataLoader/src/com/freshdirect/dataloader/analytics/EventCronRunner.java
+++ projects/DataLoader/src/com/freshdirect/dataloader/analytics/EventCronRunner.java
@@ -2,10 +2,12 @@ package com.freshdirect.dataloader.analytics;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Hashtable;
 
+import javax.mail.MessagingException;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -16,6 +18,7 @@ import com.freshdirect.ErpServicesProperties;
 import com.freshdirect.analytics.ejb.EventProcessorHome;
 import com.freshdirect.analytics.ejb.EventProcessorSB;
 import com.freshdirect.framework.util.log.LoggerFactory;
+import com.freshdirect.mail.ErpMailSender;
 
 
 public class EventCronRunner {
@@ -48,7 +51,7 @@ public class EventCronRunner {
 		e.printStackTrace(new PrintWriter(sw));			
 		LOGGER.info(new StringBuilder("EventCronRunner failed with Exception...").append(sw.toString()).toString());
 		LOGGER.error(sw.toString());
-	//	email(Calendar.getInstance().getTime(), sw.getBuffer().toString());		
+		email(Calendar.getInstance().getTime(), sw.getBuffer().toString());		
 	} finally {
 		try {
 			if (ctx != null) {
@@ -58,7 +61,7 @@ public class EventCronRunner {
 		} catch (NamingException ne) {
 			StringWriter sw = new StringWriter();
 			ne.printStackTrace(new PrintWriter(sw));	
-		//	email(Calendar.getInstance().getTime(), sw.getBuffer().toString());
+			email(Calendar.getInstance().getTime(), sw.getBuffer().toString());
 		}
 	}
 	}
@@ -71,4 +74,32 @@ public class EventCronRunner {
 		h.put(Context.PROVIDER_URL, ErpServicesProperties.getProviderURL());
 		return new InitialContext(h);
 	}
+	
+	private static void email(Date processDate, String exceptionMsg) {
+		// TODO Auto-generated method stub
+		try {
+			SimpleDateFormat dateFormatter = new SimpleDateFormat("EEE, MMM d, yyyy");
+			String subject="EventCronRunner:	"+ (processDate != null ? dateFormatter.format(processDate) : " date error");
+
+			StringBuffer buff = new StringBuffer();
+
+			buff.append("<html>").append("<body>");			
+			
+			if(exceptionMsg != null) {
+				buff.append("Exception is :").append("\n");
+				buff.append(exceptionMsg);
+			}
+			buff.append("</body>").append("</html>");
+
+			ErpMailSender mailer = new ErpMailSender();
+			mailer.sendMail(ErpServicesProperties.getCronFailureMailFrom(),
+					ErpServicesProperties.getCronFailureMailTo(),ErpServicesProperties.getCronFailureMailCC(),
+					subject, buff.toString(), true, "");
+			
+		}catch (MessagingException e) {
+			LOGGER.warn("Error Sending Event Cron report email: ", e);
+		}
+		
+	}
+
 }
--- projects/DataLoader/src/com/freshdirect/dataloader/analytics/OrderRateCronRunner.java
+++ projects/DataLoader/src/com/freshdirect/dataloader/analytics/OrderRateCronRunner.java
@@ -5,6 +5,7 @@ import java.io.StringWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Hashtable;
@@ -12,6 +13,8 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
+import javax.mail.MessagingException;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -24,6 +27,7 @@ import com.freshdirect.analytics.ejb.OrderRateHome;
 import com.freshdirect.analytics.ejb.OrderRateSB;
 import com.freshdirect.dataloader.payment.SaleCronRunner;
 import com.freshdirect.framework.util.log.LoggerFactory;
+import com.freshdirect.mail.ErpMailSender;
 
 public class OrderRateCronRunner {
 
@@ -53,7 +57,7 @@ public class OrderRateCronRunner {
 		e.printStackTrace(new PrintWriter(sw));			
 		LOGGER.info(new StringBuilder("OrderRateCronRunner failed with Exception...").append(sw.toString()).toString());
 		LOGGER.error(sw.toString());
-	//	email(Calendar.getInstance().getTime(), sw.getBuffer().toString());		
+		email(Calendar.getInstance().getTime(), sw.getBuffer().toString());		
 	} finally {
 		try {
 			if (ctx != null) {
@@ -63,12 +67,37 @@ public class OrderRateCronRunner {
 		} catch (NamingException ne) {
 			StringWriter sw = new StringWriter();
 			ne.printStackTrace(new PrintWriter(sw));	
-		//	email(Calendar.getInstance().getTime(), sw.getBuffer().toString());
+			email(Calendar.getInstance().getTime(), sw.getBuffer().toString());
 		}
 	}
 	}
 
-	
+	private static void email(Date processDate, String exceptionMsg) {
+		// TODO Auto-generated method stub
+		try {
+			SimpleDateFormat dateFormatter = new SimpleDateFormat("EEE, MMM d, yyyy");
+			String subject="OrderRateCronRunner:	"+ (processDate != null ? dateFormatter.format(processDate) : " date error");
+
+			StringBuffer buff = new StringBuffer();
+
+			buff.append("<html>").append("<body>");			
+			
+			if(exceptionMsg != null) {
+				buff.append("Exception is :").append("\n");
+				buff.append(exceptionMsg);
+			}
+			buff.append("</body>").append("</html>");
+
+			ErpMailSender mailer = new ErpMailSender();
+			mailer.sendMail(ErpServicesProperties.getCronFailureMailFrom(),
+					ErpServicesProperties.getCronFailureMailTo(),ErpServicesProperties.getCronFailureMailCC(),
+					subject, buff.toString(), true, "");
+			
+		}catch (MessagingException e) {
+			LOGGER.warn("Error Sending OrderRate Cron report email: ", e);
+		}
+		
+	}
 	
 	static public Context getInitialContext() throws NamingException {
 		Hashtable<String, String> h = new Hashtable<String, String>();
