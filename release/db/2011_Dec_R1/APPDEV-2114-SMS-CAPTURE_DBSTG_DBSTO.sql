d29958c2580d1f55e727b6a52db699b8723209e6
skanury <skanury@842219ff-9341-0410-a943-573460bdd4de>
2011-12-29 17:41:38 +0000

    [APPDEV-2114] SMS Capture
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@16106 842219ff-9341-0410-a943-573460bdd4de

---
+++ release/db/2011_Dec_R1/APPDEV-2114-SMS-CAPTURE_DBSTG_DBSTO.sql
@@ -0,0 +1,9 @@
+alter table CUST.CUSTOMERINFO add (
+mobile_number  varchar2(15),
+mobile_preference_flag varchar2(1),
+delivery_notification varchar2(1),
+offers_notification varchar2(1),
+go_green varchar2(1)
+)
+/
+
