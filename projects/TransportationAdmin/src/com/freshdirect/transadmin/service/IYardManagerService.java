1ae455266c569ee3b27ef4ff757d390265a33ac2
kkanuganti <kkanuganti@842219ff-9341-0410-a943-573460bdd4de>
2012-01-09 16:23:12 +0000

    APPDEV-2112:Yard Screen in TransApp
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@16204 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/TransportationAdmin/src/com/freshdirect/transadmin/service/IYardManagerService.java
@@ -0,0 +1,29 @@
+package com.freshdirect.transadmin.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.freshdirect.transadmin.constants.EnumAssetStatus;
+import com.freshdirect.transadmin.model.ParkingLocation;
+import com.freshdirect.transadmin.model.ParkingSlot;
+import com.freshdirect.transadmin.model.UPSRouteInfo;
+import com.freshdirect.transadmin.service.exception.TransAdminServiceException;
+
+public interface IYardManagerService {
+	
+	void addParkingLocation(Set<ParkingLocation> locations) throws TransAdminServiceException;
+	
+	void clearParkingLocation(Set<String> loactionLst) throws TransAdminServiceException;
+	
+	Map<String, ParkingLocation> getParkingLocation() throws TransAdminServiceException;
+	
+	void addParkingSlot(ParkingSlot slot) throws TransAdminServiceException;
+	
+	List<ParkingSlot> getParkingSlot(String parkingLocName)  throws TransAdminServiceException;
+	
+	Map<String, EnumAssetStatus> getAssets(List<String> assetTypes) throws TransAdminServiceException;
+	
+	UPSRouteInfo getUPSRouteInfo(final Date deliveryDate, final String routeNo) throws TransAdminServiceException;
+}
---
+++ projects/TransportationAdmin/src/com/freshdirect/transadmin/service/impl/YardManagerService.java
@@ -0,0 +1,88 @@
+package com.freshdirect.transadmin.service.impl;
+
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.dao.DataIntegrityViolationException;
+
+import com.freshdirect.transadmin.constants.EnumAssetStatus;
+import com.freshdirect.transadmin.dao.IYardManagerDAO;
+import com.freshdirect.transadmin.model.ParkingLocation;
+import com.freshdirect.transadmin.model.ParkingSlot;
+import com.freshdirect.transadmin.model.UPSRouteInfo;
+import com.freshdirect.transadmin.service.IYardManagerService;
+import com.freshdirect.transadmin.service.exception.IIssue;
+import com.freshdirect.transadmin.service.exception.TransAdminServiceException;
+
+public class YardManagerService implements IYardManagerService {
+	
+	private IYardManagerDAO yardMngDAOImpl;
+
+	public IYardManagerDAO getYardMngDAOImpl() {
+		return yardMngDAOImpl;
+	}
+
+	public void setYardMngDAOImpl(IYardManagerDAO yardMngDAOImpl) {
+		this.yardMngDAOImpl = yardMngDAOImpl;
+	}
+
+	public void addParkingLocation(Set<ParkingLocation> locations) throws TransAdminServiceException {
+		try{
+			yardMngDAOImpl.addParkingLocation(locations);
+		} catch (SQLException e) {
+			throw new TransAdminServiceException(e, IIssue.PROCESS_YARDMONITOR_ERROR);
+		}
+	}
+	
+	public void clearParkingLocation(Set<String> loactionLst) throws TransAdminServiceException {
+		try{
+			yardMngDAOImpl.clearParkingLocation(loactionLst);
+		} catch (SQLException e) {
+			throw new TransAdminServiceException(e, IIssue.PROCESS_YARDMONITOR_ERROR);
+		}
+	}
+	
+	public Map<String, ParkingLocation> getParkingLocation() throws TransAdminServiceException {
+		try{
+			return yardMngDAOImpl.getParkingLocation();
+		} catch (SQLException e) {
+			throw new TransAdminServiceException(e, IIssue.PROCESS_YARDMONITOR_ERROR);
+		}
+	}
+	
+	public void addParkingSlot(ParkingSlot slot) throws TransAdminServiceException {
+		try{
+			yardMngDAOImpl.addParkingSlot(slot);
+		} catch (DataIntegrityViolationException e) {
+			throw new TransAdminServiceException(e, "Parking slot already exists with the same slot number.");
+		} catch (SQLException e) {
+			throw new TransAdminServiceException(e, IIssue.PROCESS_YARDMONITOR_ERROR);
+		}
+	}
+	
+	public List<ParkingSlot> getParkingSlot(String parkingLocName) throws TransAdminServiceException {
+		try{
+			return yardMngDAOImpl.getParkingSlot(parkingLocName);	
+		} catch (SQLException e) {
+			throw new TransAdminServiceException(e, IIssue.PROCESS_YARDMONITOR_ERROR);
+		}
+	}
+	
+	public Map<String, EnumAssetStatus> getAssets(List<String> assetTypes) throws TransAdminServiceException {
+		try{
+			return yardMngDAOImpl.getAssets(assetTypes);	
+		} catch (SQLException e) {
+			throw new TransAdminServiceException(e, IIssue.PROCESS_YARDMONITOR_ERROR);
+		}
+	}
+	public UPSRouteInfo getUPSRouteInfo(final Date deliveryDate, final String routeNo) throws TransAdminServiceException {
+		try{
+			return yardMngDAOImpl.getUPSRouteInfo(deliveryDate, routeNo) ;	
+		} catch (SQLException e) {
+			throw new TransAdminServiceException(e, IIssue.PROCESS_YARDMONITOR_ERROR);
+		}
+	}
+}
