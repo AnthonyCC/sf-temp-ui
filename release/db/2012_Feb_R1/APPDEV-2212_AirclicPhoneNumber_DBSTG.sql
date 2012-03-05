9ebf74758e394681c2f00775cd6dbb2881e24174
kkanuganti <kkanuganti@842219ff-9341-0410-a943-573460bdd4de>
2012-03-05 20:55:35 +0000

    APPDEV-2212: AirclicPhoneNumber grants
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@16964 842219ff-9341-0410-a943-573460bdd4de

---
+++ release/db/2012_Feb_R1/APPDEV-2212_AirclicPhoneNumber_DBSTG.sql
@@ -0,0 +1,2 @@
+GRANT DELETE, INSERT, SELECT, UPDATE ON transp.dispatch_resource TO fdstore_ststg01;
+GRANT SELECT ON transp.assetstatus TO fdstore_ststg01;
---
+++ release/db/2012_Feb_R1/APPDEV-2212_AirclicPhoneNumber_DBSTO.sql
@@ -0,0 +1,2 @@
+GRANT DELETE, INSERT, SELECT, UPDATE ON transp.dispatch_resource TO fdstore_stprd01;
+GRANT SELECT ON transp.assetstatus TO fdstore_stprd01;
