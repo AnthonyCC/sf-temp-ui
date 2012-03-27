cdf2a110810fe120a39fd1072446ed3638c65c0f
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2012-03-27 13:38:32 +0000

    Updated Release DB scripts
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@17347 842219ff-9341-0410-a943-573460bdd4de

--- release/db/2012_Mar_R1/Refer-a-friend-APPDEV-1888.sql
+++ release/db/2012_Mar_R1/Refer-a-friend-APPDEV-1888.sql
@@ -30,47 +30,29 @@ add_by_date      DATE,
 change_by_date   DATE,
 add_by_user      VARCHAR2(50),
 change_by_user   VARCHAR2(50)
-)
-/
-
-GRANT SELECT, INSERT, DELETE, UPDATE on cust.referral_prgm to FDSTORE_STPRD01;
-
-GRANT SELECT on cust.referral_prgm to APPDEV;
---GRANT DELETE, INSERT, SELECT, UPDATE ON cust.referral_prgm TO FDSTORE_PRDA;
+);
 
 ALTER TABLE CUST.referral_prgm
 ADD CONSTRAINT fk_rp_promotion_id
   FOREIGN KEY (promotion_id)
-  REFERENCES CUST.promotion_new(ID)
-/
+  REFERENCES CUST.promotion_new(ID);
 
 create table cust.referral_customer_list (
 referal_prgm_id VARCHAR2(16 BYTE)    NOT NULL,
 erp_customer_id VARCHAR2(16)         NOT NULL
-)
-/
-
-GRANT SELECT, INSERT, DELETE, UPDATE on cust.referral_customer_list to FDSTORE_STPRD01;
-GRANT SELECT on cust.referral_customer_list to APPDEV;
---GRANT DELETE, INSERT, SELECT, UPDATE ON cust.referral_customer_list TO FDSTORE_PRDA;
+);
 
 ALTER TABLE CUST.referral_customer_list
 ADD CONSTRAINT fk_referal_prgm_id
   FOREIGN KEY (referal_prgm_id)
-  REFERENCES CUST.referral_prgm(ID)
-/
+  REFERENCES CUST.referral_prgm(ID);
 
 ALTER TABLE CUST.referral_customer_list
 ADD CONSTRAINT fk_rcl_customer_id
   FOREIGN KEY (erp_customer_id)
-  REFERENCES CUST.customer(ID)
-/
+  REFERENCES CUST.customer(ID);
 
-alter table cust.fdcustomer
-add (
-    referer_customer_id VARCHAR2(16)	
-)
-/
+alter table cust.fdcustomer add (referer_customer_id VARCHAR2(16));
 
 ALTER TABLE cust.fdcustomer
   ADD
@@ -83,18 +65,16 @@ ALTER TABLE cust.fdcustomer
   ADD
     CONSTRAINT customer_ref_prgm_idx
     FOREIGN KEY
-   ( referral_prgm_id )
+   ( referer_customer_id )
     REFERENCES cust.referral_prgm ( ID );
 	
 --alter prmotion table
-ALTER TABLE CUST.PROMOTION_NEW
-ADD REFERRAL_PROMO VARCHAR2(1) DEFAULT 'N';
+ALTER TABLE CUST.PROMOTION_NEW ADD REFERRAL_PROMO VARCHAR2(1) DEFAULT 'N';
 
 --create a new table to hold the referral link
 create table cust.referral_link (
 referral_link varchar2(150),
-customer_id   varchar2(16) )
-/
+customer_id   varchar2(16) );
 
 ALTER TABLE cust.referral_link
   ADD
@@ -104,13 +84,7 @@ ALTER TABLE cust.referral_link
 ALTER TABLE CUST.referral_link
 ADD CONSTRAINT fk_rl_customer_id
   FOREIGN KEY (customer_id)
-  REFERENCES CUST.customer(ID)
-/
-
-GRANT SELECT, INSERT, DELETE, UPDATE on cust.referral_link to FDSTORE_STPRD01;
-
-GRANT SELECT on cust.referral_link to APPDEV;
---GRANT DELETE, INSERT, SELECT, UPDATE ON cust.referral_link TO FDSTORE_PRDA;
+  REFERENCES CUST.customer(ID);
 
 
 --mail sent list
@@ -124,35 +98,11 @@ FRIENDS_CUSTOMER_ID  	VARCHAR2(16),
 CREDIT_SALE_ID			VARCHAR2(16),
 COMPLAINT_ID			VARCHAR2(16),
 REFERRAL_PRGM_ID 		VARCHAR2(16)
-)
-/
+);
 
-ALTER TABLE cust.CUSTOMER_INVITES
-  ADD
-    CONSTRAINT customer_invites_unique_idx
-    UNIQUE( REFERRAL_CUSTOMER_ID, upper(FRIENDS_EMAIL));
-
-GRANT SELECT, INSERT, DELETE, UPDATE on cust.CUSTOMER_INVITES to FDSTORE_STPRD01;
-
-GRANT SELECT on cust.CUSTOMER_INVITES to APPDEV;
---GRANT DELETE, INSERT, SELECT, UPDATE ON cust.CUSTOMER_INVITES TO FDSTORE_PRDA;
-
-	
---creating department and compalint code
-insert into CUST.DEPARTMENT
-select 'RAF', 'Referral', 'Refer A Friend', null from dual;
-
-insert into cust.complaint_dept
-select 'RAF', 'Referral' from dual;
-
-insert into  cust.complaint_code
-select 'RAFPRGM', 'Referral Program', 'RAF-001', 1, null from dual;
-
-insert into cust.complaint_dept_code
-select 'RAFPRGM', 'RAF', 10, null, '2197557754' from dual;
-
-insert into CUST.CASE_OPERATION 
-select 'RAF-001', 'NEW','OPEN', 'SYS','NOTE' from dual;
+ALTER TABLE CUST.CUSTOMER_INVITES ADD (
+  CONSTRAINT CUSTOMER_INVITES_UNIQUE_IDX
+ UNIQUE (REFERRAL_CUSTOMER_ID, FRIENDS_EMAIL));
 
 CREATE TABLE CUST.RAF_FAILED_ATTEMPTS (
 EMAIL             VARCHAR2(80),
@@ -163,10 +113,13 @@ ZIPCODE			  VARCHAR2(10),
 RAF_URL			  VARCHAR2(250),
 REASON			  VARCHAR2(50),
 ROW_ADDED_DATE    DATE
-)
-/
+);
 
-GRANT SELECT, INSERT, DELETE, UPDATE on cust.RAF_FAILED_ATTEMPTS to FDSTORE_STPRD01;
+--creating department and compalint code
+insert into CUST.DEPARTMENT select 'RAF', 'Referral', 'Refer A Friend', null from dual;
 
-GRANT SELECT on cust.RAF_FAILED_ATTEMPTS to APPDEV;
---GRANT DELETE, INSERT, SELECT, UPDATE ON cust.RAF_FAILED_ATTEMPTS TO FDSTORE_PRDA;
+insert into cust.complaint_dept select 'RAF', 'Referral' from dual;
+
+insert into CUST.COMPLAINT_CODE (CODE, NAME, SUBJECT_CODE, PRIORITY) Values ('RAFPRGM', 'Referral Program', 'RAF-001', 1);
+
+insert into cust.complaint_dept_code select 'RAFPRGM', 'RAF', 10, null, cust.system_seq.nextval from dual;
