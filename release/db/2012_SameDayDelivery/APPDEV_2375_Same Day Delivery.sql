0b90341a79251e1f678c2cf661823093301ad134
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2012-06-14 18:32:16 +0000

    same day db changes
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@18242 842219ff-9341-0410-a943-573460bdd4de

--- release/db/2012_SameDayDelivery/APPDEV_2375_Same Day Delivery.sql
+++ release/db/2012_SameDayDelivery/APPDEV_2375_Same Day Delivery.sql
@@ -5,3 +5,7 @@ alter table dlv.timeslot add ( PREMIUM_CUTOFF_TIME  DATE, PREMIUM_CAPACITY NUMBE
 alter table dlv.reservation add (CLASS CHAR(2 BYTE));
 
 alter table dlv.zone add (PREMIUM_CT_RELEASE_TIME NUMBER(4) DEFAULT 0 NOT NULL, PREMIUM_CT_ACTIVE VARCHAR2(1 BYTE));
+
+alter table mis.timeslot_event_hdr add sameday char(1 BYTE);
+
+alter table mis.session_event add sameday char(1 BYTE);
