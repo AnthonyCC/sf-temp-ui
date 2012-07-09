cd89837faf853a81a69223e4740ebdfdbfa5ee60
kkanuganti <kkanuganti@842219ff-9341-0410-a943-573460bdd4de>
2012-07-09 21:20:07 +0000

    APPDEV-2461:Gift card Donation module
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@18444 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/CMS/src/com/freshdirect/fdstore/content/DonationOrganization.java
@@ -0,0 +1,60 @@
+package com.freshdirect.fdstore.content;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.freshdirect.cms.ContentKey;
+import com.freshdirect.fdstore.attributes.FDAttributeFactory;
+
+/**
+ * @author kkanuganti
+ *
+ */
+public class DonationOrganization extends ContentNodeModelImpl {
+
+	private static final long serialVersionUID = 1L;
+	
+	private final List giftcardTypes = new ArrayList();
+
+	public DonationOrganization(ContentKey key) {
+		super(key);
+	}
+	
+	public String getOrganizationName(){
+		return getAttribute("ORGANIZATION_NAME", "");
+	}
+	
+	public String getEmail(){
+		return getAttribute("EMAIL", "");
+	}
+	
+	public String getContactInfo() {
+		return getAttribute("CONTACT_INFO", "");
+	}
+		
+	public List getGiftcardType() {
+		ContentNodeModelUtil.refreshModels(this, "GIFTCARD_TYPE", giftcardTypes, false);
+		return new ArrayList(giftcardTypes);		
+	}
+	
+	public Image getOrganizationLogo() {
+        return FDAttributeFactory.constructImage(this, "ORGANIZATION_LOGO");
+	}
+
+	public Image getOrganizationLogoSmall() {
+        return FDAttributeFactory.constructImage(this, "ORGANIZATION_LOGO_SMALL");
+	}
+	
+	public Html getEditorialMain() {
+		return FDAttributeFactory.constructHtml(this, "EDITORIAL_MAIN");
+	}
+	
+	public Html getEditorialDetail(){
+		return FDAttributeFactory.constructHtml(this, "EDITORIAL_DETAIL");
+	}
+	
+	public Html getEditorialHeaderMedia(){
+		return FDAttributeFactory.constructHtml(this, "EDITORIAL_HEADER_MEDIA");
+	}
+
+}
