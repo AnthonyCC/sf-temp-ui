71e5224a3339eb024151402ae1f3ed81fd885bff
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2012-08-24 12:46:09 +0000

    db script for APPDEV-2535
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@19133 842219ff-9341-0410-a943-573460bdd4de

--- release/db/2012_PA_Expansion/APPDEV-2535_PA_ROUTING_CHANGES.sql
+++ release/db/2012_PA_Expansion/APPDEV-2535_PA_ROUTING_CHANGES.sql
@@ -6,7 +6,7 @@ CREATE TABLE TRN_REGION
   NAME         VARCHAR2(32 BYTE)                NOT NULL,
   DESCRIPTION  VARCHAR2(256 BYTE),
   IS_DEPOT     VARCHAR2(1 BYTE)
-)
+);
 
 
 GRANT INSERT, SELECT, UPDATE, DELETE ON TRN_REGION TO FDTRN_PRDA, FDSTORE_PRDA;
@@ -34,10 +34,18 @@ Insert into TRANSP.TRN_REGION
 Insert into TRANSP.TRN_REGION
    (CODE, NAME)
  Values
-   ('PHL', 'PHI');
+   ('PHL', 'PHL');
 Insert into TRANSP.TRN_REGION
    (CODE, NAME, IS_DEPOT)
  Values
    ('PDP', 'PDP', 'X');
 
+alter table transp.trn_area add  REGION_CODE         VARCHAR2(8 BYTE);
+        
+update transp.trn_area set region_code = 'FD' where code in ('500','561','580','516','680','562','920','750','505','510','582','000','001','002','400',
+'690','450','511','800','585','017','019','105','922','560','700','581','515','200','455');
 
+update transp.trn_area set region_code = 'MDP' where code in ('004','005','013','014','023','030','040','050','052','060','062','066','070','072','080','520','521','522',
+'523','524','526','527','528','531','532','533','534','535','536','541','542','543')
+
+update transp.trn_area set region_code = 'PHL'   where code in ('475','480')
