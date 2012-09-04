e514c27cb248219acf413a339c4b8a8e1cfc39d2
batchley <batchley@842219ff-9341-0410-a943-573460bdd4de>
2012-09-04 22:22:40 +0000

    [APPDEV-2615] Show local Philadelphia phone number (on invoices, marketing emails, Storefront, etc.)
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@19219 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/SystemMessageList.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/SystemMessageList.java
@@ -85,7 +85,7 @@ public interface SystemMessageList {
 	public final static String MSG_UNIQUE_USERNAME				= "An account already exists with this email address.  Please enter a different one or <a href=\"/login/forget_password.jsp\">click here</a> if you have forgotten your password.";
 	public final static String MSG_UNIQUE_USERNAME_FOR_LSIGNUP	= "An account already exists with this email address.  Please enter a different one or <a href=\"#\" onclick=\"window.top.location='/login/forget_password.jsp';\">click here</a> if you have forgotten your password.";
 	public final static String MSG_INVALID_ADDRESS				= "Sorry, we're unable to recognize this address - please make sure it's entered correctly.";
-	public final static String MSG_OUTERSPACE_ADDRESS			= "We're sorry, but you must have a valid home address in the Tri-State area (New York, New Jersey and Connecticut) in order to register.";
+	public final static String MSG_OUTERSPACE_ADDRESS			= "We're sorry, but you must have a valid home address in one of our <a href=\"javascript:popup('/help/delivery_zones.jsp','large')\">delivery zones</a> in order to register a new dlivery location.";
 
 	// cart
 	public final static String MSG_QUANTITY_REQUIRED			= "Please select a quantity before adding items to your cart.";
