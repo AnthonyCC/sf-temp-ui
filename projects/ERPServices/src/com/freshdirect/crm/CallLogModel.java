983468727029362f9a2a4225f79cc53e42457abe
kkanuganti <kkanuganti@842219ff-9341-0410-a943-573460bdd4de>
2012-10-03 13:59:24 +0000

    Angel call log data services
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@19478 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/ERPServices/src/com/freshdirect/crm/CallLogModel.java
@@ -0,0 +1,82 @@
+package com.freshdirect.crm;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CallLogModel implements Serializable {
+	
+	private String callerId;
+	private String orderNumber;
+	private Date startTime;
+	private int duration;
+	private String callOutcome;
+	private String phoneNumber;
+	private String menuOption;
+	private int talkTime;	
+	
+	public CallLogModel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public CallLogModel(String callerId, String orderNumber, Date startTime,
+			int duration, String callOutcome, String phoneNumber,
+			String menuOption, int talkTime) {
+		super();
+		this.callerId = callerId;
+		this.orderNumber = orderNumber;
+		this.startTime = startTime;
+		this.duration = duration;
+		this.callOutcome = callOutcome;
+		this.phoneNumber = phoneNumber;
+		this.menuOption = menuOption;
+		this.talkTime = talkTime;
+	}
+	public String getCallerId() {
+		return callerId;
+	}
+	public void setCallerId(String callerId) {
+		this.callerId = callerId;
+	}
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public String getCallOutcome() {
+		return callOutcome;
+	}
+	public void setCallOutcome(String callOutcome) {
+		this.callOutcome = callOutcome;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getMenuOption() {
+		return menuOption;
+	}
+	public void setMenuOption(String menuOption) {
+		this.menuOption = menuOption;
+	}
+	public int getTalkTime() {
+		return talkTime;
+	}
+	public void setTalkTime(int talkTime) {
+		this.talkTime = talkTime;
+	}
+}
