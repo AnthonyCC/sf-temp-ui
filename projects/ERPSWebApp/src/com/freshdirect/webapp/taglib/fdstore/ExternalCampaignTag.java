b59499e54f4b32c5e1cbb1097af53d109889d7f1
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2012-12-24 20:19:16 +0000

    svn tree conflict
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@20377 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/ExternalCampaignTag.java
@@ -0,0 +1,48 @@
+package com.freshdirect.webapp.taglib.fdstore;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.JspException;
+
+import com.freshdirect.fdstore.FDResourceException;
+import com.freshdirect.fdstore.customer.FDCustomerManager;
+import com.freshdirect.fdstore.customer.FDUserI;
+import com.freshdirect.framework.webapp.ActionResult;
+import com.freshdirect.webapp.taglib.AbstractControllerTag;
+
+public class ExternalCampaignTag extends AbstractControllerTag {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6201818754616187135L;
+	
+	protected boolean performAction(HttpServletRequest request, ActionResult actionResult) throws JspException {
+		
+		try {
+	        HttpSession session = pageContext.getSession();
+	        if(request.getParameter("campaignId") == null)
+	        	return true;
+	        FDUserI user = (FDUserI)session.getAttribute(SessionName.USER);
+	        if (user == null || (user!=null && user.getLevel() != FDUserI.SIGNED_IN)) {
+	        	return true;
+	        }
+	        user.setExternalCampaign(request.getParameter("campaignId"));
+	        user.setExternalCampaignTC("on".equals(request.getParameter("terms")));
+	        user = FDCustomerManager.saveExternalCampaign(user);
+	        session.setAttribute(SessionName.USER, user);
+	       
+		} catch (FDResourceException e) {
+           /* if (e.getMessage().indexOf("unique constraint") > -1) {
+            	actionResult.addError(new ActionError("mainError", "Duplicate entry exists, record not saved"));
+            } */
+			// ignore the exception
+		}
+		return true;
+	}
+	
+	public static class TagEI extends AbstractControllerTag.TagEI {
+		// default impl
+	}
+	
+}
