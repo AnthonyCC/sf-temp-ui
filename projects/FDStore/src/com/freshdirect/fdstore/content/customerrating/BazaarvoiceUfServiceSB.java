b872e587bfee9bd4b60301495460e59eebd79d95
acsaszar <acsaszar@842219ff-9341-0410-a943-573460bdd4de>
2013-01-07 12:53:20 +0000

    [APPDEV-2657] - Bugfixes
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@20515 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDStore/src/com/freshdirect/fdstore/content/customerrating/BazaarvoiceUfServiceSB.java
+++ projects/FDStore/src/com/freshdirect/fdstore/content/customerrating/BazaarvoiceUfServiceSB.java
@@ -1,11 +1,17 @@
 package com.freshdirect.fdstore.content.customerrating;
 
 import java.rmi.RemoteException;
+import java.util.List;
 
 import javax.ejb.EJBObject;
+
+import com.freshdirect.fdstore.FDResourceException;
+import com.freshdirect.fdstore.content.customerrating.CustomerRatingsDTO;
 
 public interface BazaarvoiceUfServiceSB extends EJBObject{
 	
 	public BazaarvoiceFeedProcessResult processFile() throws RemoteException;
 	public BazaarvoiceFeedProcessResult processRatings() throws RemoteException;
+	public long getLastRefresh() throws FDResourceException, RemoteException;
+	public List<CustomerRatingsDTO> getCustomerRatings() throws FDResourceException, RemoteException;
 }
--- projects/FDStore/src/com/freshdirect/fdstore/content/customerrating/BazaarvoiceUfServiceSessionBean.java
+++ projects/FDStore/src/com/freshdirect/fdstore/content/customerrating/BazaarvoiceUfServiceSessionBean.java
@@ -1,5 +1,10 @@
 package com.freshdirect.fdstore.content.customerrating;
 
+import java.util.List;
+
+import com.freshdirect.fdstore.FDResourceException;
+import com.freshdirect.fdstore.content.customerrating.CustomerRatingsDAO;
+import com.freshdirect.fdstore.content.customerrating.CustomerRatingsDTO;
 import com.freshdirect.framework.core.SessionBeanSupport;
 
 public class BazaarvoiceUfServiceSessionBean extends SessionBeanSupport {
@@ -8,6 +13,8 @@ public class BazaarvoiceUfServiceSessionBean extends SessionBeanSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = -4735004684491213606L;
+	
+	private static CustomerRatingsDAO customerRatingsDAO = new CustomerRatingsDAO();
 	
 	public BazaarvoiceFeedProcessResult processFile() {
 		 return new UploadFeedProcessTask().process();
@@ -19,5 +26,13 @@ public class BazaarvoiceUfServiceSessionBean extends SessionBeanSupport {
 			result = new StoreFeedTask().process();
 		}
 		return result;
+	}
+	
+	public long getLastRefresh() throws FDResourceException{
+		return customerRatingsDAO.getTimestamp();
+	}
+	
+	public List<CustomerRatingsDTO> getCustomerRatings() throws FDResourceException{
+		return customerRatingsDAO.getCustomerRatings();
 	}
 }
