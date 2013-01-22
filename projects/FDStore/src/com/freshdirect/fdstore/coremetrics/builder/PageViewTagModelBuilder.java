5cdd94f3a5d8c6eb8f6336a8b17be5770f0c8802
tgelesz <tgelesz@842219ff-9341-0410-a943-573460bdd4de>
2013-01-22 17:08:58 +0000

    [APPDEV-2879]  Page view tag modification for department_cohort_match.jsp
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@20835 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDStore/src/com/freshdirect/fdstore/coremetrics/builder/PageViewTagModelBuilder.java
+++ projects/FDStore/src/com/freshdirect/fdstore/coremetrics/builder/PageViewTagModelBuilder.java
@@ -60,7 +60,7 @@ public class PageViewTagModelBuilder  {
 				tagModel.setCategoryId(CustomCategory.SEARCH.toString());
 				decoratePageIdWithCatId();
 			
-			} else if ("department".equals(fileName) || "category".equals(fileName) || "newsletter".equals(fileName) || "whatsgood".equals(fileName)){
+			} else if ("department".equals(fileName) || "department_cohort_match".equals(fileName) || "category".equals(fileName) || "newsletter".equals(fileName) || "whatsgood".equals(fileName)){
 				processDeptOrCat();
 			} else if ("product".equals(fileName)){
 				processProduct();
