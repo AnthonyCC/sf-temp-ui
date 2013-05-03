547246dc39e96dc3b1b11d9d3e8d7ae67572dc71
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2013-05-03 18:24:05 +0000

    (APPDEV-3034) Enhancement Storefront to do local inventory check when SAP blackhole is enabled
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@22242 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/sap/ejb/SapGatewaySessionBean.java
+++ projects/ERPServices/src/com/freshdirect/sap/ejb/SapGatewaySessionBean.java
@@ -8,6 +8,8 @@
  */
 package com.freshdirect.sap.ejb;
 
+import java.sql.Connection;
+
 import javax.ejb.EJBException;
 import javax.jms.JMSException;
 import javax.jms.ObjectMessage;
@@ -17,6 +19,7 @@ import org.apache.log4j.Category;
 import com.freshdirect.customer.EnumSaleType;
 import com.freshdirect.framework.core.GatewaySessionBeanSupport;
 import com.freshdirect.framework.util.log.LoggerFactory;
+import com.freshdirect.inventory.LocalInventoryDAO;
 import com.freshdirect.sap.SapCustomerI;
 import com.freshdirect.sap.SapOrderI;
 import com.freshdirect.sap.SapProperties;
@@ -41,7 +44,9 @@ public class SapGatewaySessionBean extends GatewaySessionBeanSupport {
 	public SapOrderI checkAvailability(SapOrderI order, long timeout) {
 		if (SapProperties.isBlackhole()) {
 			LOGGER.debug("Message blackholed.");
-			return order;
+			//This method will check the local inventory stored in ERPS.INVENTORY to do real time ATP check when SAP is down
+			//(APPDEV-3034) Enhancement Storefront to do local inventory check when SAP blackhole is enabled 
+			return checkLocalAvailability(order);
 		}
 
 		SapCheckAvailability command = new SapCheckAvailability(order, timeout);
@@ -55,9 +60,38 @@ public class SapGatewaySessionBean extends GatewaySessionBeanSupport {
 		return command.getOrder();
 	}
 
+	private SapOrderI checkLocalAvailability(SapOrderI order) {
+		Connection conn = null;
+		try {
+			conn = this.getConnection();
+			LocalInventoryDAO dao = new LocalInventoryDAO();
+			return dao.checkLocalAvailability(conn, order);
+		} catch (Exception e) {
+			LOGGER.warn("Error checking availability", e);
+		} finally {
+			close(conn);
+		}
+		return order;
+	}
+	
+	private void commitLocalInventory(SapOrderI order){
+		Connection conn = null;
+		try {
+			conn = this.getConnection();
+			LocalInventoryDAO dao = new LocalInventoryDAO();
+			dao.commitLocalInventory(conn, order);
+		} catch (Exception e) {
+			LOGGER.warn("Error commiting inventory", e);
+		} finally {
+			close(conn);
+		}
+	}
 	public void sendCreateSalesOrder(SapOrderI order,EnumSaleType saleType) {
 		LOGGER.info("Sending sales order create request " + (order != null ? order.getWebOrderNumber() : "NULL"));
 		if (SapProperties.isBlackhole()) {
+			//This method will check the local inventory stored in ERPS.INVENTORY to do real time ATP check when SAP is down
+			//(APPDEV-3034) Enhancement Storefront to do local inventory check when SAP blackhole is enabled 
+			commitLocalInventory(order);
 			LOGGER.debug("Message blackholed.");
 			return;
 		}
