6cc214cca148df1da6471f67f3de5f54ac5ff017
batchley <batchley@842219ff-9341-0410-a943-573460bdd4de>
2013-05-16 15:51:29 +0000

    [APPDEV-2368] eCoupons    fix coupon status updater
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@22384 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/fd/modules/search/statusupdate.js
+++ projects/Media/docroot/assets/javascript/fd/modules/search/statusupdate.js
@@ -1,36 +1,50 @@
 var FreshDirect = FreshDirect || {};
 
 (function(fd, $) {
-  var statusUpdater = function (node, msg) {
-    var $node,
-        $item;
+	var statusUpdater = function (node, msg) {
+		var $node, $item;
 
-    if (!node) {
-      return;
-    }
+		if (!node) {
+			return;
+		}
 
-    $node = $(node);
+		$node = $(node);
 
-    $node.find('.grid-item-status-added').html(msg);
-    $node.find('.grid-item-status-in-cart').html($node.find('.grid-item-status-added .in-cart').html());
-    $node.addClass('grid-item-status-visible');
+		$node.find('.grid-item-status-added').html(msg);
+		$node.find('.grid-item-status-in-cart').html($node.find('.grid-item-status-added .in-cart').html());
+		$node.addClass('grid-item-status-visible');
 
-    setTimeout(function () {
-      $node.removeClass('grid-item-status-visible');
-    }, 3000);
+		setTimeout(function () {
+			$node.removeClass('grid-item-status-visible');
+		}, 3000);
 
-    $item = $node.parents('.grid-item');
+		$item = $node.parents('.grid-item');
 
-    if ($item) {
-      $item.addClass('in-cart');
-      $item.addClass('grid-item-disabled');
-    }
+		if ($item) {
+			$item.addClass('in-cart');
+			$item.addClass('grid-item-disabled');
+		}
 
-  };
+	 };
+
+	var couponStatusUpdater = function (node, msg) {
+		var $node, $item;
+
+		if (!node) {
+			return;
+		}
+
+		$node = $(node);
+
+		$node.html(msg);
+
+	 };
 	
 	// register in fd namespace
 	fd.modules.common.utils.register("modules.search", "statusUpdater", statusUpdater, fd);
-  // backward compatibility...
+	fd.modules.common.utils.register("modules.search", "couponStatusUpdater", couponStatusUpdater, fd);
+	// backward compatibility...
 	fd.modules.common.utils.register("FDSearch", "statusUpdater", statusUpdater, window);
+	fd.modules.common.utils.register("FDSearch", "couponStatusUpdater", couponStatusUpdater, window);
 }(FreshDirect, jQuery));
 
