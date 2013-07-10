0435cc69d6aaa0cfe7398a79404d99710c7eb4ea
skanury <skanury@842219ff-9341-0410-a943-573460bdd4de>
2013-07-10 14:15:28 +0000

    [APPDEV-3019]Variable Minimum Orders
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@22981 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/FDStore/src/com/freshdirect/fdstore/rules/TimeslotCondition.java
@@ -0,0 +1,104 @@
+package com.freshdirect.fdstore.rules;
+
+import org.apache.log4j.Category;
+
+import com.freshdirect.framework.util.TimeOfDay;
+import com.freshdirect.framework.util.log.LoggerFactory;
+import com.freshdirect.rules.ConditionI;
+import com.freshdirect.rules.RuleRuntimeI;
+
+public class TimeslotCondition implements ConditionI {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+    private String startTime;
+    private String endTime;
+    private String day;
+    private TimeOfDay startTimeDay;
+    private TimeOfDay endTimeDay;
+    private String serviceType;
+    private Double orderMinimum;
+    
+    
+	public TimeslotCondition() {
+	}
+
+	public TimeslotCondition(String day) {
+		this.day = day;
+	}
+
+
+	public boolean evaluate(Object target, RuleRuntimeI ctx) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public boolean validate() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+	public TimeOfDay getStartTimeDay() 
+	{
+		if(startTimeDay==null&&startTime!=null)
+		{
+			this.startTimeDay=new TimeOfDay(startTime);
+		}
+		return startTimeDay;
+	}
+
+	public void setStartTimeDay(TimeOfDay startTimeDay) {
+		this.startTimeDay = startTimeDay;
+	}
+
+	public TimeOfDay getEndTimeDay() 
+	{
+		if(endTimeDay==null&&endTime!=null)
+		{
+			this.endTimeDay=new TimeOfDay(endTime);
+		}
+		return endTimeDay;
+	}
+
+	public void setEndTimeDay(TimeOfDay endTimeDay) {
+		this.endTimeDay = endTimeDay;
+	}
+
+	public String getDay() 
+	{		
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+	
+	public Double getOrderMinimum() {
+		return orderMinimum;
+	}
+
+	public void setOrderMinimum(Double orderMinimum) {
+		this.orderMinimum = orderMinimum;
+	}
+	
+
+}
