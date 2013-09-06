026746ddb6e97f024a68615c824fa4a3a8516061
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2013-09-06 17:56:17 +0000

    Try Object Leak Test.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@23710 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/fdstore/ecoupon/FDCouponActivityLogSessionBean.java
+++ projects/ERPServices/src/com/freshdirect/fdstore/ecoupon/FDCouponActivityLogSessionBean.java
@@ -1,38 +1,12 @@
 package com.freshdirect.fdstore.ecoupon;
 
-import java.rmi.RemoteException;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import javax.ejb.FinderException;
 import javax.jms.ObjectMessage;
 
 import org.apache.log4j.Logger;
 
-import com.freshdirect.FDCouponProperties;
-import com.freshdirect.analytics.TimeslotEventModel;
-import com.freshdirect.common.ERPSessionBeanSupport;
-import com.freshdirect.customer.ErpAbstractOrderModel;
-import com.freshdirect.customer.ErpCouponDiscountLineModel;
-import com.freshdirect.customer.ErpOrderLineModel;
-import com.freshdirect.customer.ejb.ErpSaleEB;
 import com.freshdirect.fdstore.FDResourceException;
-import com.freshdirect.fdstore.ecoupon.model.CouponCart;
-import com.freshdirect.fdstore.ecoupon.model.ErpCouponTransactionDetailModel;
-import com.freshdirect.fdstore.ecoupon.model.ErpCouponTransactionModel;
 import com.freshdirect.fdstore.ecoupon.model.FDCouponActivityLogModel;
-import com.freshdirect.fdstore.ecoupon.model.FDCouponCustomer;
-import com.freshdirect.fdstore.ecoupon.model.FDCouponEligibleInfo;
-import com.freshdirect.fdstore.ecoupon.model.FDCouponInfo;
-import com.freshdirect.fdstore.ecoupon.model.FDCustomerCouponWallet;
-import com.freshdirect.fdstore.ecoupon.service.CouponServiceException;
 import com.freshdirect.framework.core.GatewaySessionBeanSupport;
-import com.freshdirect.framework.core.PrimaryKey;
 import com.freshdirect.framework.util.log.LoggerFactory;
 
 public class FDCouponActivityLogSessionBean extends GatewaySessionBeanSupport {
@@ -43,21 +17,17 @@ public class FDCouponActivityLogSessionBean extends GatewaySessionBeanSupport {
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOGGER = LoggerFactory.getInstance(FDCouponActivityLogSessionBean.class);
 	
-	public void logCouponActivity(FDCouponActivityLogModel log) throws FDResourceException{
-		/*Connection conn = null;
-		try {
-			conn = getConnection();
-			FDCouponActivityLogDAO.logCouponActivity(log, conn);
-		} catch (SQLException e) {
-			throw new FDResourceException(e);
-		} */
+	private static final String LOG_COUPON_ACTIVITY = "LOG_COUPON_ACTIVITY";
+	private static final String MessageType = "MessageType";
+	
+	public void logCouponActivity(FDCouponActivityLogModel log) throws FDResourceException {		
 		enqueue(log);
 	}
 	
 	private void enqueue(FDCouponActivityLogModel log) {
 		try {
 			ObjectMessage logMsg = this.qsession.createObjectMessage();
-			logMsg.setStringProperty("MessageType","LOG_COUPON_ACTIVITY" );
+			logMsg.setStringProperty(MessageType,LOG_COUPON_ACTIVITY );
 			logMsg.setObject(log);
 			this.qsender.send(logMsg);
 		} catch (Exception ex) {
