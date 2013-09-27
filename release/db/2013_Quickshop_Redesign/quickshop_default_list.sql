278e8d7cf69d6d3db7116cc184529b11ef02bf3f
ztreer <ztreer@842219ff-9341-0410-a943-573460bdd4de>
2013-09-27 13:31:38 +0000

    [APPDEV-2869] Quickshop Redesign - final cleanup - db scripts
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@23940 842219ff-9341-0410-a943-573460bdd4de

--- release/db/2013_Quickshop_Redesign/quickshop_default_list.sql
+++
@@ -1,1 +0,0 @@
-alter table cust.fduser add default_list_id varchar2(16);
--- release/db/2013_Quickshop_Redesign/quickshop_recipe_list.sql
+++
@@ -1,2 +0,0 @@
-alter table cust.customerlist add recipe_id varchar2(128);
-alter table cust.customerlist add recipe_name varchar2(128);
