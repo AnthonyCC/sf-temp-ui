76a71165953572818cdee86c6212a7893e2b3759
RSzaloki <RSzaloki@842219ff-9341-0410-a943-573460bdd4de>
2013-10-04 15:32:03 +0000

    fixes [APPBUG-1556]
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@24009 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/fd/quickshop/common/breadcrumbs.js
+++ projects/Media/docroot/assets/javascript/fd/quickshop/common/breadcrumbs.js
@@ -6,24 +6,16 @@ var FreshDirect = FreshDirect || {};
 
 	var WIDGET = fd.modules.common.widget;
 	
+	var filteringId=	['TIME_FRAME_30' ,'TIME_FRAME_60' ,'TIME_FRAME_90' ,'TIME_FRAME_180','TIME_FRAME_LAST','ORDERS_BY_DATE','DEPT'    ,'GLUTEN_FREE'    ,'KOSHER'     ,'LOCAL'     ,'ORGANIC'     ,'ON_SALE'],
+		filteringValue=	['TIME_FRAME_ALL','TIME_FRAME_ALL','TIME_FRAME_ALL','TIME_FRAME_ALL','TIME_FRAME_ALL' ,'all_orders'    ,'all_dept','pref_glutenFree','pref_kosher','pref_local','pref_organic','pref_onSale'];
+
+	
 	var filteringUrlValue = {
-			DEPT: 'all_dept',
-			GLUTEN_FREE: 'pref_glutenFree',
-			KOSHER: 'pref_kosher',
-			LOCAL: 'pref_local',
-			ON_SALE: 'pref_onSale',
-			ORDERS_BY_DATE: 'all_orders',
-			ORGANIC: 'pref_organic',
-			TIME_FRAME_30: 'TIME_FRAME_ALL',
-			TIME_FRAME_60: 'TIME_FRAME_ALL',
-			TIME_FRAME_90: 'TIME_FRAME_ALL',
-			TIME_FRAME_180: 'TIME_FRAME_ALL',
-			TIME_FRAME_LAST:'TIME_FRAME_ALL' 
 	};
 	
 	function filterList(prev,current){
 		if(current.selected){
-			prev.push(current);
+			prev[current.filter]={name:current.name};
 		}
 		return prev;
 	}
@@ -40,7 +32,7 @@ var FreshDirect = FreshDirect || {};
 		},
 		callback:{
 			value:function(data){
-				var result=[];
+				var result={};
 				Object.keys(data).forEach(function(item){
 					var menuItem = data[item];
 					Object.keys(menuItem).forEach(function(filterName){
@@ -49,12 +41,11 @@ var FreshDirect = FreshDirect || {};
 				});
 			
 				
-				WIDGET.callback.call(this,{data:result.map(function(item){
-					if(item.filter in filteringUrlValue) {
-						return {
-							filteringUrlValue:filteringUrlValue[item.filter],
-							name:item.name
-						}
+				WIDGET.callback.call(this,{data:filteringId.map(function(item,ndx){
+					var ret = result[item];
+					if(ret) {
+						ret.filteringUrlValue=filteringValue[ndx];
+						return ret;
 					}
 					return null;
 				})});
