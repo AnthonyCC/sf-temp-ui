6c07c97320d1b41ee21aa604b1e4d6c2e9055e55
kkanuganti <kkanuganti@842219ff-9341-0410-a943-573460bdd4de>
2013-10-21 19:23:32 +0000

    APPDEV-2840: Staggered dispatch db scripts
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@24172 842219ff-9341-0410-a943-573460bdd4de

--- release/db/2013_Staggered_Dispatch/APPDEV-2840_Staggered_dispatch_DBSTO_dml.sql
+++ release/db/2013_Staggered_Dispatch/APPDEV-2840_Staggered_dispatch_DBSTO_dml.sql
@@ -1,17 +1,19 @@
 /* Rollout DML scripts */
 
+update transp.scrib set zone_code = zone_id;
+
+update transp.scrib_bid sb set sb.dispatch_grouptime = sb.time;
+
 update transp.scrib s set s.truck_dispatchtime = s.start_time, s.dispatch_grouptime = s.start_time,
-s.truck_endtime = (s.end_window_time  +  ((select decode(stem_from_time, NULL, decode(stem_to_time, NULL, 0, stem_to_time), stem_from_time)  from transp.zone z where z.zone_code=s.zone_code)  * (1/24/60)));
+s.truck_endtime = (s.end_window_time  + decode(s.zone_code, NULL, ((select decode(f.lead_from_time, NULL, decode(f.lead_to_time, NULL, 0, f.lead_to_time), f.lead_from_time)  from transp.trn_facility f where F.ID = s.DESTINATION_FACILITY)  * (1/24/60)),
+((select decode(stem_from_time, NULL, decode(stem_to_time, NULL, 0, stem_to_time), stem_from_time)  from transp.zone z where z.zone_code=s.zone_code)  * (1/24/60))));
 
 update transp.plan p set p.truck_dispatchtime = p.start_time, p.dispatch_grouptime = p.start_time,
-p.truck_endtime = (p.last_dlv_time  +  ((select decode(stem_from_time, NULL, decode(stem_to_time, NULL, 0, stem_to_time), stem_from_time)  from transp.zone z where z.zone_code=p.zone)  * (1/24/60)));
+p.truck_endtime = (p.last_dlv_time  + decode(p.zone, NULL,  ((select decode(f.lead_from_time, NULL, decode(f.lead_to_time, NULL, 0, f.lead_to_time), f.lead_from_time)  from transp.trn_facility f where F.ID = p.DESTINATION_FACILITY)  * (1/24/60)),
+((select decode(stem_from_time, NULL, decode(stem_to_time, NULL, 0, stem_to_time), stem_from_time)  from transp.zone z where z.zone_code=p.zone)  * (1/24/60))));
 
 update TRANSP.WAVE_INSTANCE w set w.truck_dispatchtime = w.dispatch_time,
 w.truck_endtime = (w.last_dlv_time +  ((select decode(stem_from_time, NULL, decode(stem_to_time, NULL, 0, stem_to_time), stem_from_time)  from transp.zone z where z.zone_code=w.area)  * (1/24/60))),
 w.max_time = (w.last_dlv_time +  ((select decode(stem_from_time, NULL, decode(stem_to_time, NULL, 0, stem_to_time), stem_from_time)  from transp.zone z where z.zone_code=w.area)  * (1/24/60)));
 
 update transp.dispatch d set d.truck_dispatchtime = d.start_time, d.dispatch_grouptime = d.start_time where d.dispatch_date >= trunc(sysdate);
-
-update transp.scrib set zone_code = zone_id;
-
-update transp.scrib_bid sb set sb.dispatch_grouptime = s.time;
--- release/db/2013_Staggered_Dispatch/APPDEV-2840_Staggered_dispatch_DBSTO_dml_rollback.sql
+++ release/db/2013_Staggered_Dispatch/APPDEV-2840_Staggered_dispatch_DBSTO_dml_rollback.sql
@@ -1,21 +1,21 @@
 /* Rollback DML scripts */
 
-update transp.scrib s set s.start_time = s.dispatch_grouptime
-s.first_dlv_time = (s.truck_dispatchtime  +  ((select decode(stem_to_time, NULL, 0, stem_to_time) from transp.zone z where z.zone_code=s.area)  * (1/24/60))),
-s.end_window_time = (s.truck_endtime  -  ((select decode(stem_from_time, NULL, decode(stem_to_time, NULL, 0, stem_to_time), stem_from_time)  from transp.zone z where z.zone_code=s.area)  * (1/24/60)));
+update transp.scrib set zone_id = zone_code;
 
-update transp.plan s set p.start_time = p.dispatch_grouptime
-p.first_dlv_time = (p.truck_dispatchtime  +  ((select decode(stem_to_time, NULL, 0, stem_to_time) from transp.zone z where z.zone_code=p.area)  * (1/24/60))),
-p.last_dlv_time = (p.truck_endtime  -  ((select decode(stem_from_time, NULL, decode(stem_to_time, NULL, 0, stem_to_time), stem_from_time)  from transp.zone z where z.zone_code=p.zone)  * (1/24/60)));
+update transp.scrib_bid sb set sb.time = sb.dispatch_grouptime where time is null;
+
+update transp.scrib s set s.start_time = s.dispatch_grouptime,
+s.first_window_time = (s.truck_dispatchtime  + decode(s.zone_code, NULL, ((select decode(lead_to_time, NULL, 0, lead_to_time) from transp.trn_facility f where F.ID = s.DESTINATION_FACILITY)  * (1/24/60)), ((select decode(stem_to_time, NULL, 0, stem_to_time) from transp.zone z where z.zone_code=s.zone_code)  * (1/24/60)))),
+s.end_window_time = (s.truck_endtime  -  decode(s.zone_code, NULL, ((select decode(f.lead_from_time, NULL, decode(f.lead_to_time, NULL, 0, f.lead_to_time), f.lead_from_time)  from transp.trn_facility f where F.ID = s.DESTINATION_FACILITY)  * (1/24/60)), ((select decode(stem_from_time, NULL, decode(stem_to_time, NULL, 0, stem_to_time), stem_from_time)  from transp.zone z where z.zone_code=s.zone_code)  * (1/24/60))));
+
+update transp.plan p set p.start_time = p.dispatch_grouptime,
+p.first_dlv_time = (p.truck_dispatchtime  + decode(p.zone, NULL, ((select decode(lead_to_time, NULL, 0, lead_to_time) from transp.trn_facility f where F.ID = p.DESTINATION_FACILITY)  * (1/24/60)), ((select decode(stem_to_time, NULL, 0, stem_to_time) from transp.zone z where z.zone_code=p.zone)  * (1/24/60)))),
+p.last_dlv_time = (p.truck_endtime  -  decode(p.zone, NULL, ((select decode(f.lead_from_time, NULL, decode(f.lead_to_time, NULL, 0, f.lead_to_time), f.lead_from_time)  from transp.trn_facility f where F.ID = p.DESTINATION_FACILITY)  * (1/24/60)), ((select decode(stem_from_time, NULL, decode(stem_to_time, NULL, 0, stem_to_time), stem_from_time)  from transp.zone z where z.zone_code=p.zone)  * (1/24/60))));
 
 update TRANSP.WAVE_INSTANCE w set w.dispatch_time = w.truck_dispatchtime, 
 w.first_dlv_time = (w.truck_dispatchtime  +  ((select decode(stem_to_time, NULL, 0, stem_to_time) from transp.zone z where z.zone_code=w.area)  * (1/24/60))),
 w.last_dlv_time = (w.truck_endtime  -  ((select decode(stem_from_time, NULL, decode(stem_to_time, NULL, 0, stem_to_time), stem_from_time)  from transp.zone z where z.zone_code=w.area)  * (1/24/60)));
 
-update transp.dispatch d set d.start_time = d.dispatch_grouptime 
+update transp.dispatch d set d.start_time = d.dispatch_grouptime, 
 d.first_dlv_time = (d.truck_dispatchtime  +  ((select decode(stem_to_time, NULL, 0, stem_to_time) from transp.zone z where z.zone_code=d.zone)  * (1/24/60)))
 where d.dispatch_date >= trunc(sysdate);
-
-update transp.scrib set zone_id = zone_code where zone_id is null;
-
-update transp.scrib_bid sb set sb.time = sb.dispatch_grouptime where time is null;
