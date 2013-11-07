9d8dc1c8d669c3f4be0d7c3b93826d45d5f203f4
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2013-11-07 21:42:18 +0000

    2013_Dec_R1 tickets-[APPDEV-3198]-Login API part for 'Product Feeds'.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@24378 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/login/LoginRequest.java
@@ -0,0 +1,25 @@
+package com.freshdirect.webapp.ajax.login;
+
+import java.io.Serializable;
+
+public class LoginRequest implements Serializable{
+
+	private static final long serialVersionUID = 8051699402519682381L;
+	
+	private String userId;
+	private String password;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
