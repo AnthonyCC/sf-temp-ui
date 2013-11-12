5d80a3b97817f1afbd99925a8b6d3bacaa5dd26f
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2013-11-12 21:51:50 +0000

    APPDEV-3151 Roadnet v 3.6.3 - Add Stop Service Time
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@24445 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/RoutingServices/src/com/freshdirect/routing/model/IServiceTime.java
@@ -0,0 +1,9 @@
+package com.freshdirect.routing.model;
+
+public interface IServiceTime {
+	
+	double getOrderServiceTime();
+	void setOrderServiceTime(double orderServiceTime);
+	double getStopServiceTime();
+	void setStopServiceTime(double stopServiceTime);
+}
