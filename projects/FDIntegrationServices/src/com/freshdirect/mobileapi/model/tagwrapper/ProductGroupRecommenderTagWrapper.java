5386ba16d25d356b2f465da657cfe5ac237c7628
kkanuganti <kkanuganti@842219ff-9341-0410-a943-573460bdd4de>
2014-01-03 22:38:50 +0000

    APPDEV-3395: Mobile API - Carousels on the department home screen
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@25157 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/FDIntegrationServices/src/com/freshdirect/mobileapi/model/tagwrapper/ProductGroupRecommenderTagWrapper.java
@@ -0,0 +1,43 @@
+package com.freshdirect.mobileapi.model.tagwrapper;
+
+import com.freshdirect.fdstore.FDException;
+import com.freshdirect.fdstore.content.ContentFactory;
+import com.freshdirect.mobileapi.model.SessionUser;
+import com.freshdirect.smartstore.fdstore.Recommendations;
+import com.freshdirect.webapp.taglib.smartstore.ProductGroupRecommenderTag;
+
+public class ProductGroupRecommenderTagWrapper extends GetterTagWrapper {
+    
+	String departmentId;
+	String siteFeature;
+
+	public ProductGroupRecommenderTagWrapper(String siteFeature, String departmentId,
+			SessionUser user) {
+		super(new ProductGroupRecommenderTag(), user);
+		this.departmentId = departmentId;
+		this.siteFeature = siteFeature;
+	}
+
+	public Recommendations getFeatureRecommendations() throws FDException {
+        ((ProductGroupRecommenderTag) wrapTarget).setCurrentNode(ContentFactory.getInstance().getContentNode(departmentId));
+        ((ProductGroupRecommenderTag) wrapTarget).setSiteFeature(siteFeature);
+        return (Recommendations) getResult();
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+	public String getSiteFeature() {
+		return siteFeature;
+	}
+
+	public void setSiteFeature(String siteFeature) {
+		this.siteFeature = siteFeature;
+	}    
+
+}
