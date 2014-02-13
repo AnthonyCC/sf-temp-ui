4b4e614a5f17d21aad66acd4981a2e31cc43bf2e
ghorak <ghorak@842219ff-9341-0410-a943-573460bdd4de>
2014-02-13 17:36:20 +0000

    #APPBUG-1738 subtotal number format fix
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@25591 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/fd/components/subtotal.js
+++ projects/Media/docroot/assets/javascript/fd/components/subtotal.js
@@ -2,121 +2,122 @@
 var FreshDirect = FreshDirect || {};
 
 (function (fd) {
-	"use strict";
+  "use strict";
 
-	var $ = fd.libs.$;
-	
-	function _update($subtotal){
-		var pricingInfo = $subtotal.attr('data-prices') || '[]',
-			salesunitInfo = $subtotal.attr('data-suratio') || '[]',
-	        gpricingInfo = $subtotal.attr('data-grpprices') || '[]',
-	        cvprices = $subtotal.attr('data-cvprices') || '[]',
-	        qInc = parseFloat($subtotal.attr('data-qinc')) || 1,
-			price = 0,
-			itemInfo = fd.modules.common.productSerialize($subtotal).pop(),
-			salesUnit = itemInfo.salesUnit,
-			qty = parseFloat(itemInfo.quantity),
-			origqty = qty,
-			pricingUnit = "",
+  var $ = fd.libs.$;
+  
+  function _update($subtotal){
+    var pricingInfo = $subtotal.attr('data-prices') || '[]',
+      salesunitInfo = $subtotal.attr('data-suratio') || '[]',
+          gpricingInfo = $subtotal.attr('data-grpprices') || '[]',
+          cvprices = $subtotal.attr('data-cvprices') || '[]',
+          qInc = parseFloat($subtotal.attr('data-qinc')) || 1,
+      price = 0,
+      itemInfo = fd.modules.common.productSerialize($subtotal).pop(),
+      salesUnit = itemInfo.salesUnit,
+      qty = parseFloat(itemInfo.quantity),
+      origqty = qty,
+      pricingUnit = "",
       template = $subtotal.attr('data-template') || '[]';
       template = FreshDirect.modules.common.utils.discover(template);
-		
-		
-		/* Convert JSON string to JSON object */
-	    try { pricingInfo = JSON.parse(pricingInfo); } catch (e) {} 
-	    try { salesunitInfo = JSON.parse(salesunitInfo); } catch (e) {}
-	    try { gpricingInfo = JSON.parse(gpricingInfo); } catch (e) {}
-	    try { cvprices = JSON.parse(cvprices); } catch (e) {}
-	    
-		/* grab the pricingUnit because we will need that - first one is just fine because all are the same */		
-	    try { pricingUnit = pricingInfo[0].pricingUnit; } catch (e) {}
-		
-		/* we may need to convert if actual salesUnit and pricingUnit is not the same - in that case multiply with the appropriate ratio */		
-		try {
-		    if ( salesUnit !== pricingUnit ) {
-			  salesunitInfo.forEach(function (si) {
-			    if ( salesUnit === si.unit ) {
-			      qty = qty * si.ratio;
-			    }
-			});
-		  }		
-		} catch (e) {
-		}
+    
+    
+    /* Convert JSON string to JSON object */
+      try { pricingInfo = JSON.parse(pricingInfo); } catch (e) {} 
+      try { salesunitInfo = JSON.parse(salesunitInfo); } catch (e) {}
+      try { gpricingInfo = JSON.parse(gpricingInfo); } catch (e) {}
+      try { cvprices = JSON.parse(cvprices); } catch (e) {}
+      
+    /* grab the pricingUnit because we will need that - first one is just fine because all are the same */    
+      try { pricingUnit = pricingInfo[0].pricingUnit; } catch (e) {}
+    
+    /* we may need to convert if actual salesUnit and pricingUnit is not the same - in that case multiply with the appropriate ratio */   
+    try {
+        if ( salesUnit !== pricingUnit ) {
+        salesunitInfo.forEach(function (si) {
+          if ( salesUnit === si.unit ) {
+            qty = qty * si.ratio;
+          }
+      });
+      }   
+    } catch (e) {
+    }
 
-	    try {
-		  pricingInfo.forEach(function (pi) {
-		    if (+qty >= +pi.lowerBound && +qty < +pi.upperBound) {
-		      price = Math.round(100 * +qty * +pi.price) / 100;
-		    }
-		  });
-		} catch (e) {
-		}
+      try {
+      pricingInfo.forEach(function (pi) {
+        if (+qty >= +pi.lowerBound && +qty < +pi.upperBound) {
+          price = Math.round(100 * +qty * +pi.price) / 100;
+        }
+      });
+    } catch (e) {
+    }
 
-	    try {
-	        gpricingInfo.forEach(function (pi) {
-	          if (+qty >= +pi.lowerBound && +qty < +pi.upperBound) {
-	            price = Math.round(100 * +qty * +pi.price) / 100;
-	          }
-	        });
-	      } catch (e) {
-	      }
+      try {
+          gpricingInfo.forEach(function (pi) {
+            if (+qty >= +pi.lowerBound && +qty < +pi.upperBound) {
+              price = Math.round(100 * +qty * +pi.price) / 100;
+            }
+          });
+        } catch (e) {
+        }
 
-	    try {
-	        if('configuration' in itemInfo){
-	        	cvprices.forEach(function(cvp){
-	        		if(itemInfo.configuration[cvp.name] == cvp.value) {
-	        			if( cvp.applyHow === '1' ) {
-		        			price += origqty * cvp.price;	        				
-	        			} else if( cvp.applyHow === '0' ) {
-	        				price += qty * cvp.price;
-	        			}
-	        		}
-	        	});
-	        }
-	      } catch (e) {
-	      }
+      try {
+          if('configuration' in itemInfo){
+            cvprices.forEach(function(cvp){
+              if(itemInfo.configuration[cvp.name] == cvp.value) {
+                if( cvp.applyHow === '1' ) {
+                  price += origqty * cvp.price;                 
+                } else if( cvp.applyHow === '0' ) {
+                  price += qty * cvp.price;
+                }
+              }
+            });
+          }
+        } catch (e) {
+        }
 
-	      price = Math.round( price * 100 ) /100;
+        price = Math.round( price * 100 ) /100;
 
-		if (price) {
+    if (price) {
+      price = price.toFixed(2);
       if( template && template.call ){
         $subtotal.html( template.call( null, { 'price' : price, 'quantity' : qty, 'pricingUnit' : pricingUnit } ) );
       }
       else{ // fallback
         $subtotal.html( Subtotal.innerTemplate.call(null, { 'price' : price }) );
       }
-		} else {
-		  $subtotal.html("");
-		}
-		
-	}
-	
-	
-	
-	
-	var Subtotal = {
-			update:function(product) {
-				var $subtotal = $(product).find('[data-component="subtotal"]');
-				if($subtotal.length) {
-					_update($subtotal);					
-				}
-			},
-			innerTemplate:common.subtotalInner
-	};
-	
-	function eventHandler(event){
-		var ct = $(event.currentTarget);
-		if( ct.data('component') !== 'product' ) {
-			ct = ct.closest('[data-component="product"]')[0];
-		}
-		Subtotal.update(ct);
-	}
-	
-	$(document).on('quantity-change','[data-component="product"]',eventHandler);
-	
-	$(document).on('salesunit-change','[data-component="product"]',eventHandler);
-	
-	$(document).on('change','[data-component="productDataConfiguration"]',eventHandler);
+    } else {
+      $subtotal.html("");
+    }
+    
+  }
+  
+  
+  
+  
+  var Subtotal = {
+      update:function(product) {
+        var $subtotal = $(product).find('[data-component="subtotal"]');
+        if($subtotal.length) {
+          _update($subtotal);         
+        }
+      },
+      innerTemplate:common.subtotalInner
+  };
+  
+  function eventHandler(event){
+    var ct = $(event.currentTarget);
+    if( ct.data('component') !== 'product' ) {
+      ct = ct.closest('[data-component="product"]')[0];
+    }
+    Subtotal.update(ct);
+  }
+  
+  $(document).on('quantity-change','[data-component="product"]',eventHandler);
+  
+  $(document).on('salesunit-change','[data-component="product"]',eventHandler);
+  
+  $(document).on('change','[data-component="productDataConfiguration"]',eventHandler);
 
-	fd.modules.common.utils.register("components", "Subtotal", Subtotal, fd);
+  fd.modules.common.utils.register("components", "Subtotal", Subtotal, fd);
 }(FreshDirect));
