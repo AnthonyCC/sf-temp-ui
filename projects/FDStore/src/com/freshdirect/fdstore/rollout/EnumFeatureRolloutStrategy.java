013626dedbd4669ae2938b69078fd958d87f6c35
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2014-02-15 16:10:29 +0000

    APPDEV-3457 Generic Partial RollOut Implementation
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@25637 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/FDStore/src/com/freshdirect/fdstore/rollout/EnumFeatureRolloutStrategy.java
@@ -0,0 +1,5 @@
+package com.freshdirect.fdstore.rollout;
+
+public enum EnumFeatureRolloutStrategy {
+	GLOBAL,PROFILE,COHORT
+}
---
+++ projects/FDStore/src/com/freshdirect/fdstore/rollout/EnumRolloutFeature.java
@@ -0,0 +1,5 @@
+package com.freshdirect.fdstore.rollout;
+
+public enum EnumRolloutFeature {
+	pdplayout2014, pplayout2014
+}
