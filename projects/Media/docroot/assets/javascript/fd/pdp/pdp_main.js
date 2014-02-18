0870abb7eb39da1d6c2c290d89102fcf5b44d390
ghorak <ghorak@842219ff-9341-0410-a943-573460bdd4de>
2014-02-18 11:35:34 +0000

    bazaarvoice related bugfix
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@25682 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/fd/pdp/pdp_main.js
+++ projects/Media/docroot/assets/javascript/fd/pdp/pdp_main.js
@@ -2,28 +2,25 @@
 var FreshDirect = FreshDirect || {};
 
 (function (fd) {
-	"use strict";
-
-	var $ = fd.libs.$;
-	
+  var $ = fd.libs.$;
+  
   fd.components.Subtotal.update($('[data-component="product"][data-cmeventsource="pdp_main"]'));
-	
-	var productId=$('#BVRRContainer').data('productid');
-	
-	if($BV){
-		$BV.ui('rr', 'show_reviews', { productId: productId });
-	} else {
-		//console.log("missing $BV");
-	}
-	
-	  
-	  window.pop=function(url,width,height){
-		  fd.components.ifrPopup.open({
-			  width:width,
-			  height:height,
-			  url:url
-		  });
-	  };
-	
-	fd.modules.common.utils.register("components", "pdp", {}, fd);
+  
+  var productId=$('#BVRRContainer').data('productid');
+  
+  if(window.$BV){
+    window.$BV.ui('rr', 'show_reviews', { productId: productId });
+  } else {
+    //console.log("missing $BV");
+  }
+  
+    window.pop=function(url,width,height){
+      fd.components.ifrPopup.open({
+        width:width,
+        height:height,
+        url:url
+      });
+    };
+  
+  fd.modules.common.utils.register("components", "pdp", {}, fd);
 }(FreshDirect));
