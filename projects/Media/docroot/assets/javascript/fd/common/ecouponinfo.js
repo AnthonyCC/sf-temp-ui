f154840fdbe42c0711a5684ca0e23f53a15abb3f
ghorak <ghorak@842219ff-9341-0410-a943-573460bdd4de>
2014-03-28 13:13:56 +0000

    #APPDEV-2910 coupon info display fix
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@26333 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/fd/common/ecouponinfo.js
+++ projects/Media/docroot/assets/javascript/fd/common/ecouponinfo.js
@@ -1,35 +1,41 @@
-/*global jQuery*/
+/*global jQuery, common*/
 var FreshDirect = FreshDirect || {};
 
 (function (fd) {
-	"use strict"
+  "use strict";
 
-	var $ = fd.libs.$;
-	var DISPATCHER = fd.common.dispatcher;
+  var $ = fd.libs.$;
+  var DISPATCHER = fd.common.dispatcher;
 
-	var ecouponInfo = Object.create(fd.common.signalTarget,{
-		signal:{
-			value:'couponStatus'
-		},
-		template:{
-			value:common.ecoupon.statusTextHtml
-		},
-		renderItem:{
-			value:function(couponId,message) {
-				var elements = $('[data-ecouponid="'+couponId+'"] [data-component="ecoupon-status"]');
-				elements.html(this.template({status:message}));
-			}
-		},
-		callback:{
-			value:function( statusTexts ) {
-				Object.keys(statusTexts).forEach(function(ecouponId){
-					this.renderItem(ecouponId,statusTexts[ecouponId]);
-				},this)
-			}
-		}
-	});
+  var ecouponInfo = Object.create(fd.common.signalTarget,{
+    signal:{
+      value:'couponStatus'
+    },
+    template:{
+      value:common.ecoupon.statusTextHtml
+    },
+    renderItem:{
+      value:function(couponId, message) {
+        var atcId = message.atcId,
+            elements = $('[data-productdata-name="atcItemId"][value="'+atcId+'"]').parents('[data-component="product"]').find('[data-component="ecoupon-status"]');
 
-	ecouponInfo.listen();
+        if (elements.size() === 0) {
+          elements = $('[data-ecouponid="'+couponId+'"] [data-component="ecoupon-status"]');
+        }
 
-	fd.modules.common.utils.register("components", "ecouponInfo", ecouponInfo, fd);
+        elements.html(this.template({status:message.message}));
+      }
+    },
+    callback:{
+      value:function( statusTexts ) {
+        Object.keys(statusTexts).forEach(function(ecouponId){
+          this.renderItem(ecouponId,statusTexts[ecouponId]);
+        },this);
+      }
+    }
+  });
+
+  ecouponInfo.listen();
+
+  fd.modules.common.utils.register("components", "ecouponInfo", ecouponInfo, fd);
 }(FreshDirect));
