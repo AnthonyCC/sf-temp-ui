5c9cc581f3fd73d8af1249e57caac4e58c5c2897
ghorak <ghorak@842219ff-9341-0410-a943-573460bdd4de>
2014-05-07 14:37:37 +0000

    #APPDEV-3597 new caroussel pager support
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@26993 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/fd/components/lightcarousel.js
+++ projects/Media/docroot/assets/javascript/fd/components/lightcarousel.js
@@ -6,7 +6,25 @@ var FreshDirect = FreshDirect || {};
 
   var $ = fd.libs.$;
     
-  function getIndent( carousel, nextPage) {
+  function updatePager(carousel) {
+    var currentPage = +carousel.data('carousel-page'),
+        nrPages = +carousel.data('carousel-nrpages'),
+        pager = carousel.find('ul.pager'),
+        i, pagerContent = "";
+
+    for (i = 0; i < nrPages; i++) {
+      pagerContent += '<li data-page="'+i+'"'+(i === +currentPage ? ' class="current"': '')+'><span>'+(i+1)+'</span></li>';
+    }
+
+    if (pager.size() < 1) {
+      pager = $('<ul class="pager"></ul>');
+      carousel.append(pager);
+    }
+
+    pager.html(pagerContent);
+  }
+
+  function changePage(carousel, direction, toPage) {
 
     var $mask = $('[data-component="carousel-mask"]', carousel ),
         $list = $('[data-component="carousel-list"]', carousel ),
@@ -17,14 +35,16 @@ var FreshDirect = FreshDirect || {};
         nrPages = Math.ceil($list.children().size() / itemPerPage),
         newPage, result, targetElem;
 
-    if (nextPage) {
+    if (direction === "next") {
       carousel.removeClass('first');
 
       newPage = nrPages - 1 > currentPage ? currentPage + 1 : nrPages - 1;
-    } else {
+    } else if (direction === "prev") {
       carousel.removeClass('last');
 
       newPage = currentPage > 0 ? currentPage - 1 : 0;
+    } else {
+      newPage = toPage || toPage === 0 ? toPage : currentPage;
     }
 
     try {
@@ -41,34 +61,51 @@ var FreshDirect = FreshDirect || {};
     }
 
     carousel.data('carousel-page', newPage);
+    carousel.data('carousel-nrpages', nrPages);
+    updatePager(carousel);
+
+    if (newPage !== currentPage) {
+      carousel.addClass('stepping');
+
+      if (result || result === 0) {
+        $('[data-component="carousel-list"]', carousel).css("left", result + "px");
+      }
+
+      setTimeout(function () {
+        carousel.removeClass('stepping');
+      }, 400);
+    }
 
     return result;
   }
 
   
   var carousel = {
+      changePage: changePage,
       handleClick:function(event){
-            var carousel = $(event.currentTarget), 
-                direction,indent;
+        var carousel = $(event.currentTarget), 
+            direction;
 
-            if( direction = $(event.target).data('carousel-nav') ) {
-                
-                carousel.addClass('stepping');
-  
-                indent = getIndent(carousel, direction === "next" );
-  
-                if (indent || indent === 0) {
-                  $('[data-component="carousel-list"]', carousel ).css("left", indent + "px");
-                }
+        direction = $(event.target).data('carousel-nav');
 
-                setTimeout(function () {
-                  carousel.removeClass('stepping');
-                }, 400);
-            }
+        if (direction) {
+          changePage(carousel, direction);
+        }
       }
   };
   
-    $(document).on('click','[data-component="carousel"]',carousel.handleClick.bind(carousel));  
+  $(document).on('click','[data-component="carousel"]',carousel.handleClick.bind(carousel));  
+  $(document).on('click','[data-component="carousel"] .pager [data-page]', function (e) {
+    var el = $(e.currentTarget),
+        page = el.attr('data-page');
+
+    changePage(el.closest('[data-component="carousel"]'), null, page);
+  });
+
+  // initialize pager
+  $('[data-component="carousel"]').each(function (i, el) {
+    changePage($(el), null);
+  });
 
   fd.modules.common.utils.register("components", "carousel", carousel, fd);
 }(FreshDirect));
