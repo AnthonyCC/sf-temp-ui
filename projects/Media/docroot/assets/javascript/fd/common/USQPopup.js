f402232b5be637ba9db5abda09597d0062cab5a7
veremeev <veremeev@842219ff-9341-0410-a943-573460bdd4de>
2014-10-14 20:39:12 +0000

    [APPBUG-2569] Wine warning page background NOT grayed out completely Additionally, I made background to froze once the popup is open
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@29030 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/fd/common/USQPopup.js
+++ projects/Media/docroot/assets/javascript/fd/common/USQPopup.js
@@ -30,11 +30,12 @@ var FreshDirect = FreshDirect || {};
         	$.ajax({
         		url: '/media/editorial/site_pages/health_warning_overlay.html',
         		context: this
-        	}).done(function(data) {
+        	}).done(function(data) {       		        		
         		popupContent = $('<div class="USQPopupContent"></div>').html(data).prepend('<a class="container-close USQ-close">Close</a>');
         		this.container = $('<div id="USQPopup"></div>').hide().append(popupContent).appendTo($('body'));
         		this.opened = true;
         		this.container.show();
+        		changeHeightOfBack()
         	});
           
 
@@ -59,13 +60,16 @@ var FreshDirect = FreshDirect || {};
     
     open: {
       value: function () {
-
         if (!this.container) {
           this.initPopup();
         } else {
             this.opened = true;
             this.container.show();
+            changeHeightOfBack()
         }
+        //hiding the background body scrollbar and adding margin with width of scrollbar, so the content will not move
+        $("body").css("overflow","hidden");
+        $("body").css("margin-right","17px");
       }
     },
 
@@ -79,15 +83,29 @@ var FreshDirect = FreshDirect || {};
 
     close: {
       value: function () {
-
         if (this.container) {
           this.opened = false;
           this.container.hide();
         }
+      //returning body's properties back to normal
+        $("body").css("overflow","auto");
+        $("body").css("margin-right","0px");
       }
     }
 
   });
+  
+  //changes background size if the window is resized
+  $(window).on('resize', function(){ changeHeightOfBack(); });
+  
+  //changes the background size of popup, so it will be bigger if the window is too small
+  function changeHeightOfBack(){
+	  $('head').append('<style>#USQPopup:before{height:100%;}</style>');
+	  if ($("#USQPopup").height() < $("#USQPopup div.USQPopupContent").height()){
+			var sum = $("#USQPopup div.USQPopupContent").height() + 25;
+			$('head').append('<style>#USQPopup:before{height:'+ sum + 'px;}</style>');
+		} 
+  }
   
   USQPopup.state=Bacon.mergeAll([$(document).asEventStream('click','#USQAcceptButton').map(true),
                                  $(document).asEventStream('click','#USQCancelButton').map(false)]).toProperty(FreshDirect.USQLegalWarning.checkHealthCondition('freshdirect.healthwarning','1'));
