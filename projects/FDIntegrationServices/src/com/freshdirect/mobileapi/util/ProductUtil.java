cba22e992eabaf5f8aaf3a30b7aaf374ae214d77
PPatel <PPatel@842219ff-9341-0410-a943-573460bdd4de>
2014-11-20 00:09:27 +0000

    Reverting custom dispatch servlet and ProductUtil readHtml changes (AA)
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@29509 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDIntegrationServices/src/com/freshdirect/mobileapi/util/ProductUtil.java
+++ projects/FDIntegrationServices/src/com/freshdirect/mobileapi/util/ProductUtil.java
@@ -1,13 +1,10 @@
 package com.freshdirect.mobileapi.util;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 import org.apache.log4j.Logger;
@@ -25,18 +22,13 @@ public class ProductUtil {
     }
 
     public static URL resolve(String rootPath, String childPath) throws IOException {
-    	
-    	//I am changing the implementation of this to create two seperate URLs and then test the strings
-    	//because the previous implementation throws an Exception when the rootpath is not 
-    	//local to the machine.
-    	//-- Michael Cress
-        URL rootPathURL = new URL(rootPath);
+        URL url = new URL(rootPath);
         if (childPath.startsWith("/")) {
             childPath = childPath.substring(1, childPath.length());
         }
-        URL url = new URL(rootPathURL, childPath);
+        url = new URL(url, childPath);
 
-        if (!url.toString().startsWith( /*rootPath*/ rootPathURL.toString())) {
+        if (!url.toString().startsWith(rootPath)) {
             throw new IOException("Child path not under root");
         }
 
@@ -63,50 +55,15 @@ public class ProductUtil {
     }
 
     public static String readHtml(Html htmlContent){
+        String result = "";
         if ((htmlContent != null) && (htmlContent.getPath() != null)) {
             try {
-            	
-            	//Michael Cress
-            	//Fixing the path here to use an absolute path
-            	//and then using an HttpUrlConnection to read the data rather than calling the readContent method.
-            	//I was getting an exception when doing that.
-            	StringBuilder path = new StringBuilder();
-            	path.append("http://freshdirect.com");
-            	path.append( htmlContent.getPath() );	//getPath() already returns a string with a '/' prepended.
-
-				URL url = new URL( path.toString() );
-//                return readContent(ProductUtil.resolve( /*htmlContent.getPath()*/ "http://freshdirect.com",  path.toString() ));
-//				return readContent( url );
-            	
-				HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		 
-				// optional default is GET
-				con.setRequestMethod("GET");
-		 
-				//add request header
-				con.setRequestProperty("User-Agent", "FreshDirect Mobile API");
-		 
-				int responseCode = con.getResponseCode();
-		 
-				if( responseCode == 200 )
-				{
-					BufferedReader in = new BufferedReader( new InputStreamReader(con.getInputStream() ) );
-					String inputLine;
-					StringBuffer responseStr = new StringBuffer();
-			 
-					while ((inputLine = in.readLine()) != null) {
-						responseStr.append(inputLine);
-					}
-					in.close();
-	            	
-	            	return responseStr.toString();
-				}
-            	
+                result = readContent(ProductUtil.resolve(htmlContent.getPath()));
             } catch (IOException e) {
-                LOG.warn("HTML Content file " + htmlContent.getPath() + " not found");
+                LOG.warn("HTML Content filr " + htmlContent.getPath() + " not found");
             }
         }
-        return "";
+        return result;
     }
 
 }
