de398ab9b49df13a5b1097103f3b4f357e3e70e0
ghorak <ghorak@842219ff-9341-0410-a943-573460bdd4de>
2014-12-08 13:14:30 +0000

    #APPBUG-2617 globalnav fixes (submenu positioning fix)
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@29636 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/fd/components/globalnavpopup.js
+++ projects/Media/docroot/assets/javascript/fd/components/globalnavpopup.js
@@ -7,71 +7,86 @@ var FreshDirect = FreshDirect || {};
   // TODO
   // - iPad - add class on touch, prevent click
 
-  $(document).ready(function () {
-    // copy menu popups to menu items
-    ["globalnav-item", "globalnav-submenu-item"].forEach(function (item) {
-      $('[data-component="globalnav-menu"] [data-component="'+item+'"]').each(function () {
-        var $menuitem = $(this),
-            $popupcontent = $menuitem.find('[data-component="globalnav-popup-body"]'),
-            $container = $('[data-component="globalnav-menu"]'),
-            left = $menuitem.offset().left - $container.offset().left,
-            center, width, id;
-
-        if (left > $container.width() / 2) {
-          $menuitem.addClass('alignPopupRight');
-        }
-
-        if ($popupcontent.size() === 0) {
-          id = $menuitem.data('id');
-          $popupcontent = $('[data-component="globalnav-popups"] [data-id="'+id+'"]').first();
-          if ($popupcontent.size() > 0) {
-            $popupcontent.insertAfter($menuitem.children('span').first());
-            if ($popupcontent.attr('data-popup-type') === 'superdepartment') {
-              center = left + $menuitem.outerWidth() / 2;
-              width = 160; // 2 * 80 for the gradients
-              $popupcontent.find('li.submenuitem').each(function () {
-                width += $(this).outerWidth();
-              });
-              $popupcontent.find('.subdepartments').css({
-                'padding-left': Math.max(0, Math.min(center - width / 2, 960 - width))
-              });
-            }
-          }
-        }
-      });
-    });
-
-    // set popup height
-    $('[data-component="globalnav-popup-body"]').each(function () {
-      var $el = $(this),
-          depHeight = +$el.find('.department').outerHeight();
-
-      $el.find('.heroimg_cont').height(depHeight);
-    });
-
-    // for dynamic fallback
-    $('[data-component="globalnav-menu"]').on('mouseover', '[data-component="globalnav-item"],[data-component="globalnav-submenu-item"]', function (e) {
-      var $menuitem = $(e.currentTarget),
+  // copy menu popups to menu items
+  ["globalnav-item", "globalnav-submenu-item"].forEach(function (item) {
+    $('[data-component="globalnav-menu"] [data-component="'+item+'"]').each(function () {
+      var $menuitem = $(this),
           $popupcontent = $menuitem.find('[data-component="globalnav-popup-body"]'),
-          id;
+          $container = $('[data-component="globalnav-menu"]'),
+          left = $menuitem.offset().left - $container.offset().left,
+          center, width, id;
+
+      if (left > $container.width() / 2) {
+        $menuitem.addClass('alignPopupRight');
+      }
 
       if ($popupcontent.size() === 0) {
         id = $menuitem.data('id');
         $popupcontent = $('[data-component="globalnav-popups"] [data-id="'+id+'"]').first();
         if ($popupcontent.size() > 0) {
           $popupcontent.insertAfter($menuitem.children('span').first());
+          if ($popupcontent.attr('data-popup-type') === 'superdepartment') {
+            center = left + $menuitem.outerWidth() / 2;
+            width = 160; // 2 * 80 for the gradients
+            $popupcontent.find('li.submenuitem').each(function () {
+              width += $(this).outerWidth();
+            });
+            $popupcontent.find('.subdepartments').css({
+              'padding-left': Math.max(0, Math.min(center - width / 2, 960 - width))
+            });
+          }
         }
       }
     });
+  });
 
-    $('[data-component="globalnav-item"],[data-component="globalnav-submenu-item"]').on("touchstart", function (e) {
-      var $t = $(this);
+  // set popup height
+  $('[data-component="globalnav-popup-body"]').each(function () {
+    var $el = $(this),
+        depHeight = +$el.find('.department').outerHeight();
 
-      if (!$t.hasClass('touched')) {
-        e.preventDefault();
-        $t.parent().find('.touched').removeClass('touched');
-        $t.addClass('touched');
+    $el.find('.heroimg_cont').height(depHeight);
+  });
+
+  // for dynamic fallback
+  $('[data-component="globalnav-menu"]').on('mouseover', '[data-component="globalnav-item"],[data-component="globalnav-submenu-item"]', function (e) {
+    var $menuitem = $(e.currentTarget),
+        $popupcontent = $menuitem.find('[data-component="globalnav-popup-body"]'),
+        $container = $('[data-component="globalnav-menu"]'),
+        left = $menuitem.offset().left - $container.offset().left,
+        id, center, width;
+
+    if (left > $container.width() / 2) {
+      $menuitem.addClass('alignPopupRight');
+    }
+
+    if ($popupcontent.size() === 0) {
+      id = $menuitem.data('id');
+      $popupcontent = $('[data-component="globalnav-popups"] [data-id="'+id+'"]').first();
+      if ($popupcontent.size() > 0) {
+        $popupcontent.insertAfter($menuitem.children('span').first());
       }
-    });
+    }
+
+    if ($popupcontent.attr('data-popup-type') === 'superdepartment') {
+      center = left + $menuitem.outerWidth() / 2;
+      width = 160; // 2 * 80 for the gradients
+      $popupcontent.find('li.submenuitem').each(function () {
+        width += $(this).outerWidth();
+      });
+      $popupcontent.find('.subdepartments').css({
+        'padding-left': Math.max(0, Math.min(center - width / 2, 960 - width))
+      });
+    }
+  });
+
+  $('[data-component="globalnav-item"],[data-component="globalnav-submenu-item"]').on("touchstart", function (e) {
+    var $t = $(this);
+
+    if (!$t.hasClass('touched')) {
+      e.preventDefault();
+      $t.parent().find('.touched').removeClass('touched');
+      $t.addClass('touched');
+    }
   });
 }(FreshDirect, FreshDirect.libs.$));
