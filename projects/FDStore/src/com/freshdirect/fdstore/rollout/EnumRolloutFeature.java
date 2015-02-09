4b3b75cb5bc7fdb68cf857e91f077485b70ce6db
batchley <batchley@842219ff-9341-0410-a943-573460bdd4de>
2015-02-09 22:23:39 +0000

    [APPDEV-4034] Fly-out recommender Test
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@30230 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDStore/src/com/freshdirect/fdstore/rollout/EnumRolloutFeature.java
+++ projects/FDStore/src/com/freshdirect/fdstore/rollout/EnumRolloutFeature.java
@@ -1,5 +1,6 @@
 package com.freshdirect.fdstore.rollout;
 
 public enum EnumRolloutFeature {
-	pdplayout2014, pplayout2014, leftnav2014, searchredesign2014, leftnavtut2014
+	pdplayout2014, pplayout2014, leftnav2014, searchredesign2014, leftnavtut2014, 
+	browseflyoutrecommenders //fly-out recommended products on "browse" pages (transactionalPopup)
 }
