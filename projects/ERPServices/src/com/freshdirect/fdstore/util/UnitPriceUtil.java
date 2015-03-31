e096847ce7b9f6a43c1cb3a69c6c9719e7db8157
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2015-03-31 23:30:19 +0000

    Unit Pricing New Strategy for Mobile - Build Fix
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@30887 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/fdstore/util/UnitPriceUtil.java
+++ projects/ERPServices/src/com/freshdirect/fdstore/util/UnitPriceUtil.java
@@ -1,27 +1,58 @@
 package com.freshdirect.fdstore.util;
 
+import java.text.DecimalFormat;
+
+import com.freshdirect.fdstore.FDSalesUnit;
+
 /**
  * 
  * @author ksriram
  *
  */
 public class UnitPriceUtil {
-
+	
+	private static String FORMAT_STR = "0.00";
+	
 	/**
 	 * Method to format the unitPriceUOM from SAP, for display.
 	 * @param unitPriceUOM
 	 * @return
 	 */
 	public static String getFormattedUnitPriceUOM(String unitPriceUOM) {
+		
 		String formattedUnitPriceUOM = unitPriceUOM;
 		if(null != unitPriceUOM && !"".equalsIgnoreCase(unitPriceUOM)){
-			if("CT".equalsIgnoreCase(unitPriceUOM)){
+			if("CT".equalsIgnoreCase(unitPriceUOM)){ // CT in SAP is Carton which will be displayed as ctn in website
 				formattedUnitPriceUOM = "ctn";
-			}else if("CN".equalsIgnoreCase(unitPriceUOM)){
+			}else if("CN".equalsIgnoreCase(unitPriceUOM)){ // CN is count in SAP which will be displayed as ct in website
 				formattedUnitPriceUOM = "ct";
 			}
 		}
 		return formattedUnitPriceUOM;
+	}
+	
+	public static String getUnitPrice(FDSalesUnit su, double defaultPrice) {
+		
+		String unitPrice = null;
+		if (su != null) {
+			// validate unit price values
+			final int n = su.getUnitPriceNumerator();
+			final int d = su.getUnitPriceDenominator();
+			if (n > 0 && n > 0) {
+				final double p = (defaultPrice * n) / d;
+
+				unitPrice = formatDecimalToString(p);
+			}
+		}
+		return unitPrice;
+	}
+	
+	private static String formatDecimalToString(double number) {
+		DecimalFormat decimalFormat = new DecimalFormat( FORMAT_STR );
+		String strNumber = decimalFormat.format(number);
+		strNumber = strNumber.replaceAll(",", ".");
+
+		return strNumber;
 	}
 
 }
