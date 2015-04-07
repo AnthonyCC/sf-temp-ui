83f1c303b8d8ee4712d117612672215f0df6847d
zholoda <zholoda@842219ff-9341-0410-a943-573460bdd4de>
2015-04-07 08:58:09 +0000

    [APPDEV-4105] QuickShop 2 - Sort label Ordered Frequently (most) must be changed to Most Frequently Ordered
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@31038 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/quickshop/QuickShopSortType.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/quickshop/QuickShopSortType.java
@@ -4,10 +4,10 @@ import com.freshdirect.fdstore.content.SortTypeI;
 
 public enum QuickShopSortType implements SortTypeI {
 
-	    BY_FREQUENCY(3, "freq", "Ordered Frequently (most)","Ordered Frequently (most)","Ordered Frequently (least)" ),
+	    BY_FREQUENCY(3, "freq", "Most Frequently Ordered","Most Frequently Ordered","Most Frequently Ordered" ),
 		BY_SALE(1, "sale", "Sale", "Sale", "Sale"),
 		BY_RECENCY(2, "recency", "Ordered Recently", "Ordered Recently", "Ordered Recently"),
-		BY_NAME(0, "name", "A-Z", "A-Z", "Z-A"),
+		BY_NAME(0, "name", "A-Z", "A-Z", "A-Z"),
 		BY_YOUR_FAVORITES(5, "favs", "Your Favorites", "Your Favorites", "Your Favorites"),
 		BY_EXPERT_RATING(4, "expr", "Expert Rating", "Expert Rating", "Expert Rating");
 	    
@@ -17,7 +17,7 @@ public enum QuickShopSortType implements SortTypeI {
 	    private String textAsc;
 	    private String textDesc;
 
-	    QuickShopSortType(int t, String label, String text, String textAsc, String textDesc) {
+	    private QuickShopSortType(int t, String label, String text, String textAsc, String textDesc) {
 	    	this.type = t;
 	    	this.label = label;
 	    	this.text = text;
@@ -27,12 +27,12 @@ public enum QuickShopSortType implements SortTypeI {
 
 	    @Override
 		public int getType() {
-	    	return this.type;
+	    	return type;
 	    }
 	    
 	    @Override
 		public String getLabel() {
-	    	return this.label;
+	    	return label;
 	    }
 	    
 	    @Override
