1aed309f6053b92d237670954b55f931e2173022
gsebestyen <gsebestyen@842219ff-9341-0410-a943-573460bdd4de>
2015-05-08 08:47:34 +0000

    [APPDEV-4031] Improve CMS change event capture
    Add logging capability, sort out null content nodes
    
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@32037 842219ff-9341-0410-a943-573460bdd4de

--- projects/CMS/src/com/freshdirect/cms/cache/ContentCacheService.java
+++ projects/CMS/src/com/freshdirect/cms/cache/ContentCacheService.java
@@ -1,7 +1,6 @@
 package com.freshdirect.cms.cache;
 
 import java.util.Collection;
-import java.util.Iterator;
 
 import com.freshdirect.cms.ContentKey;
 import com.freshdirect.cms.ContentNodeI;
@@ -22,13 +21,20 @@ public class ContentCacheService {
 	}
 	
 	public void invalidateContentNode(Collection<ContentNodeI> nodes) {
-		for (Iterator<ContentNodeI> contentNodeIterator = nodes.iterator(); contentNodeIterator.hasNext();) {
-			ContentNodeI node = contentNodeIterator.next();
+		if (nodes == null || nodes.isEmpty()) {
+			return;
+		}
+
+
+		for (final ContentNodeI node : nodes) {
+			if (node == null)
+				continue;
+
 			contentCacheInterceptor.getCache().remove(node.getKey());
+
 			// invalidate dependents also
 			// FIXME this is suboptimal, but fixes implicit node-creation issues
-			for (Iterator<ContentKey> contentKeyIterator = ContentNodeUtil.getAllRelatedContentKeys(node).iterator(); contentKeyIterator.hasNext();) {
-				ContentKey k = contentKeyIterator.next();
+			for (final ContentKey k : ContentNodeUtil.getAllRelatedContentKeys(node)) {
 				contentCacheInterceptor.getCache().remove(k);
 			}
 		}
--- projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/cache/service/CmsChangePropagatorService.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/cache/service/CmsChangePropagatorService.java
@@ -3,15 +3,19 @@ package com.freshdirect.webapp.ajax.cache.service;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
 import com.freshdirect.cms.ContentKey;
 import com.freshdirect.cms.ContentNodeI;
 import com.freshdirect.cms.application.CmsManager;
 import com.freshdirect.cms.cache.ContentCacheService;
 import com.freshdirect.cms.search.ContentSearchServiceI;
 import com.freshdirect.framework.conf.FDRegistry;
+import com.freshdirect.framework.util.log.LoggerFactory;
 
 public class CmsChangePropagatorService {
-
+	private static final Logger LOGGER = LoggerFactory.getInstance(CmsChangePropagatorService.class);
+	
 	private static final CmsChangePropagatorService INSTANCE = new CmsChangePropagatorService();
 
 	private final ContentSearchServiceI searchService;
@@ -26,8 +30,12 @@ public class CmsChangePropagatorService {
 
 	public void propagateCmsChangesToCaches(Set<String> contentKeys) {
 		if (contentKeys != null) {
+			LOGGER.info("Receive CMS content keys: " + contentKeys);
+
 			Set<ContentNodeI> contentNodes = decodeToContentNodes(contentKeys);
-			
+
+			LOGGER.debug(".. transformed to " + contentNodes.size() + " nodes");
+
 			// invalidate content node cache
 			ContentCacheService.defaultService().invalidateContentNode(contentNodes);
 
@@ -39,7 +47,10 @@ public class CmsChangePropagatorService {
 			try {
 				CmsManager.getInstance().rebuildIndices(contentNodes);
 			} catch (Exception exc) {
+				LOGGER.error("Error occurred while rebuilding search indices", exc);
 			}
+		} else {
+			LOGGER.warn("No keys captured");
 		}
 	}
 
@@ -51,7 +62,11 @@ public class CmsChangePropagatorService {
 		for (String contentKeyText : contentKeys) {
 			ContentKey contentKey = ContentKey.decode(contentKeyText);
 			ContentNodeI node = contentKey.lookupContentNode();
-			contentNodes.add(node);
+			if (node != null) {
+				contentNodes.add(node);
+			} else {
+				LOGGER.warn("Content node not found " + contentKeyText);
+			}
 		}
 		return contentNodes;
 	}
