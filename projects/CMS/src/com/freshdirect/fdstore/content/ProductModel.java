01e7b78246214b3b682ed2ccbfb0f2124b77953b
dhkumar <dhkumar@842219ff-9341-0410-a943-573460bdd4de>
2015-06-22 10:50:29 +0000

    APPDEV -4111 Content Metadata changes
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@33574 842219ff-9341-0410-a943-573460bdd4de

--- projects/CMS/src/com/freshdirect/fdstore/content/ProductModel.java
+++ projects/CMS/src/com/freshdirect/fdstore/content/ProductModel.java
@@ -1070,4 +1070,8 @@ public interface ProductModel extends AvailabilityI, YmalSource, YmalSetSource, 
 	public EnumProductLayout getSpecialLayout();
 
 	public List<ProductModel> getCompleteTheMeal();
+	
+	public String getPageTitle();
+	
+	public String getSEOMetaDescription();
 }
