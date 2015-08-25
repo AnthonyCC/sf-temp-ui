96e02a98cced03ccea22b06486c34960e91294f9
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2015-08-25 00:06:05 +0000

    FDX-564/FDX-565 API Changes for Faq Switch
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@35346 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDIntegrationServices/src/com/freshdirect/mobileapi/controller/HelpController.java
+++ projects/FDIntegrationServices/src/com/freshdirect/mobileapi/controller/HelpController.java
@@ -9,8 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Category;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.freshdirect.fdstore.EnumEStoreId;
 import com.freshdirect.fdstore.FDException;
 import com.freshdirect.fdstore.FDStoreProperties;
+import com.freshdirect.fdstore.content.ContentFactory;
 import com.freshdirect.framework.util.log.LoggerFactory;
 import com.freshdirect.mobileapi.exception.JsonException;
 import com.freshdirect.mobileapi.exception.NoSessionException;
@@ -25,36 +27,31 @@ public class HelpController extends BaseController {
         return false;
     }
     
+    private static final String PAYLOAD_EXTN = ".json";
+    
     public static final String HELP_ACTION = "help";
-
     public static final String CONTACT_US_ACTION = "contactUs";
-    public static final String TERMS_OF_USE_ACTION = "termsOfUse";
-
     public static final String LEARN_MORE_PROMO_ACTION = "learnMorePromo";
+    public static final String TERMS_OF_USE_ACTION = "termsOfUse";
+    public static final String DP_TERMS_AND_CONDITIONS_ACTION = "deliveryPassTermsAndConditions";
+    
+    private String helpPath = "/media/mobile/iphone/help/help";
+    private String costumerServicePath = "/media/mobile/iphone/contact_us/customer_service_hours.json";
+    private String learnMorePromoPath = "/media/mobile/iphone/home/learn_more_promo.json";
+    private String customerAgreementPath = "/media/mobile/iphone/customer_agreement/customer_agreement.json";
+    private String dpAgreementPath = "/media/editorial/site_pages/deliverypass/DP_terms.json";
+    
+    
     public static final String SMS_TERMS_OF_USE = "smstermsofuse";
     public static final String ALCOHOL_REST = "alcoholrestrictions";
     public static final String ALCOHOL_AGE_VERI = "alcoholageverification";
     public static final String BACKUP_DELI_AUTH = "backupdeliveryauthorization";
 
-    private String costumerServicePath = "/media/mobile/iphone/contact_us/customer_service_hours.json";
-
-    private String learnMorePromoPath = "/media/mobile/iphone/home/learn_more_promo.json";
-
-    private String helpPath = "/media/mobile/iphone/help/help.json";
-    
-    private String customerAgreementPath = "/media/mobile/iphone/customer_agreement/customer_agreement.json";
-        
     private String smsTermsOfUsePath = "/media/mobile/iphone/sms_terms_of_use/sms_terms_of_use.json";
-    
     private String alcoholRestrictions = "/media/mobile/iphone/alcohol_restrictions/alcohol_restrictions.json";
-    
     private String alcoholAgeVerification = "/media/mobile/iphone/alcohol_restrictions/alcoholAgeVerification.json";
-    
     private String backupdeliveryauthorization = "/media/mobile/iphone/backup_delivery_authorization/backupDeliveryAuthorization.json";
-    
-    private String dpAgreementPath = "/media/editorial/site_pages/deliverypass/DP_terms.json";
-    
-    public static final String DP_TERMS_AND_CONDITIONS_ACTION = "deliveryPassTermsAndConditions";
+          
 
     @Override
     protected ModelAndView processRequest(HttpServletRequest request, HttpServletResponse response, ModelAndView model, String action,
@@ -62,6 +59,13 @@ public class HelpController extends BaseController {
         String data = "";
         URL remoteUrl = null;
         String mediaPath = FDStoreProperties.getMediaPath();
+        
+        String storeKey = ContentFactory.getInstance().getCurrentUserContext() != null 
+        									&& ContentFactory.getInstance().getCurrentUserContext().getStoreContext() != null
+        										&& ContentFactory.getInstance().getCurrentUserContext().getStoreContext().getEStoreId() != null
+        											&& !EnumEStoreId.FD.equals(ContentFactory.getInstance().getCurrentUserContext().getStoreContext().getEStoreId()) 
+        													? ContentFactory.getInstance().getCurrentUserContext().getStoreContext().getEStoreId().getContentId().toLowerCase() : null;
+        
         if (CONTACT_US_ACTION.equals(action)) {
             try {
                 remoteUrl = new URL(mediaPath + costumerServicePath);
@@ -132,7 +136,11 @@ public class HelpController extends BaseController {
             }
         } else {
             try {
-                remoteUrl = new URL(mediaPath + helpPath);
+            	if(storeKey != null) {
+            		remoteUrl = new URL(mediaPath + helpPath + "_" + storeKey + PAYLOAD_EXTN);
+            	} else {
+            		remoteUrl = new URL(mediaPath + helpPath + PAYLOAD_EXTN);
+            	}
                 data = ProductUtil.readContent(remoteUrl);
             } catch (IOException e) {
                 LOGGER.warn("Unable to rerieve data from " + remoteUrl.toString());
