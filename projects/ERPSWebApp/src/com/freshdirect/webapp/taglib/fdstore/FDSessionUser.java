150c39e3cbdf08ca53c7a99dad1c83612b32aa1e
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2015-09-10 17:16:14 +0000

    zone id issue.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@35753 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/FDSessionUser.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/FDSessionUser.java
@@ -325,7 +325,8 @@ public class FDSessionUser implements FDUserI, HttpSessionBindingListener {
             Date endDate = new Date(lastRequestDate);
             for (int i = 0; i < logEntries.size(); i++) {
                 logEntries.get(i).setStartEndTime(startDate, endDate);
-                logEntries.get(i).setZoneId(user.getUserContext().getPricingContext().getZoneInfo().toString());
+                if(user.getUserContext().getPricingContext().getZoneInfo()!=null)
+                	logEntries.get(i).setZoneId(user.getUserContext().getPricingContext().getZoneInfo().getPricingZoneId());
                 System.out.println("SessionImpressionLogEntry:sessionId:"+logEntries.get(i).getSessionId());
             }
             
