92b67036b3b34f11e4e192cc89adeef917ebeb9b
ketki <ketki@842219ff-9341-0410-a943-573460bdd4de>
2015-09-18 21:28:24 +0000

    Model classes for fdx api call
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@35954 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/ERPServices/src/com/freshdirect/routing/ejb/OrderModifyCommand.java
@@ -0,0 +1,69 @@
+package com.freshdirect.routing.ejb;
+
+public class OrderModifyCommand implements java.io.Serializable{
+		
+		private String reservationId;
+		
+		private String sapOrderNumber;
+		
+		private Double tip;
+		
+		private String saleId;
+		
+		private String parentOrderId;
+		
+		private String orderMobileNumber;
+		
+	
+		public String getReservationId() {
+			return reservationId;
+		}
+
+		public void setReservationId(String reservationId) {
+			this.reservationId = reservationId;
+		}
+
+		public String getSapOrderNumber() {
+			return sapOrderNumber;
+		}
+
+		public void setSapOrderNumber(String sapOrderNumber) {
+			this.sapOrderNumber = sapOrderNumber;
+		}
+
+		public Double getTip() {
+			return tip;
+		}
+
+		public void setTip(Double tip) {
+			this.tip = tip;
+		}
+
+		public String getSaleId() {
+			return saleId;
+		}
+
+		public void setSaleId(String saleId) {
+			this.saleId = saleId;
+		}
+
+		public String getParentOrderId() {
+			return parentOrderId;
+		}
+
+		public void setParentOrderId(String parentOrderId) {
+			this.parentOrderId = parentOrderId;
+		}
+
+		public String getOrderMobileNumber() {
+			return orderMobileNumber;
+		}
+
+		public void setOrderMobileNumber(String orderMobileNumber) {
+			this.orderMobileNumber = orderMobileNumber;
+		}
+		
+		
+	}
+
+
