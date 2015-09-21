abdfbcc1e141f962b958a4ce2aa58cda3f216e48
psingh <psingh@842219ff-9341-0410-a943-573460bdd4de>
2015-09-21 18:29:15 +0000

    APPBUG-3640: product sample bug
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@35993 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/cart/AddToCartServlet.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/cart/AddToCartServlet.java
@@ -272,7 +272,7 @@ public class AddToCartServlet extends BaseJsonServlet {
                 if (!reqData.isIgnoreRedirect()) {
                     responseData.setRedirectUrl("/view_cart.jsp");
                 }
-            } else if (("view_cart".equals(reqData.getEventSource()) || "TRY".equals(reqData.getEventSource()))
+            } else if (("ps_caraousal".equals(reqData.getEventSource()) || "view_cart".equals(reqData.getEventSource()) || "TRY".equals(reqData.getEventSource()))
                     && ONLY_OLD_VIEW_CART_PAGES_PATTERN.matcher(referer).matches()) {
                 if (!reqData.isIgnoreRedirect()) {
                     responseData.setRedirectUrl(referer);
