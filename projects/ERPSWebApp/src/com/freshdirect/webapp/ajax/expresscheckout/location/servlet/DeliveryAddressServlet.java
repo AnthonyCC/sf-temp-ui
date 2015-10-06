f1646560f94064b7595e7e2e39792591164e6118
zholoda <zholoda@842219ff-9341-0410-a943-573460bdd4de>
2015-10-06 08:47:49 +0000

    [APPBUG-3716] - Do not select address with null delivery address id
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@36371 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/expresscheckout/location/servlet/DeliveryAddressServlet.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/expresscheckout/location/servlet/DeliveryAddressServlet.java
@@ -26,84 +26,90 @@ import com.freshdirect.webapp.checkout.RedirectToPage;
 
 public class DeliveryAddressServlet extends BaseJsonServlet {
 
-	private static final String ADDRESS_BY_ID_KEY = "address_by_id";
+    private static final String ADDRESS_BY_ID_KEY = "address_by_id";
 
-	private static final long serialVersionUID = -7582639712245761241L;
+    private static final long serialVersionUID = -7582639712245761241L;
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response, FDUserI user) throws HttpErrorResponse {
-		try {
-			FormDataRequest deliveryAddressRequest = BaseJsonServlet.parseRequestData(request, FormDataRequest.class);
-			PageAction pageAction = FormDataService.defaultService().getPageAction(deliveryAddressRequest);
-			ValidationResult validationResult = new ValidationResult();
-			FormDataResponse deliveryAddressResponse = FormDataService.defaultService().prepareFormDataResponse(deliveryAddressRequest, validationResult);
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response, FDUserI user) throws HttpErrorResponse {
+        try {
+            FormDataRequest deliveryAddressRequest = BaseJsonServlet.parseRequestData(request, FormDataRequest.class);
+            PageAction pageAction = FormDataService.defaultService().getPageAction(deliveryAddressRequest);
+            ValidationResult validationResult = new ValidationResult();
+            FormDataResponse deliveryAddressResponse = FormDataService.defaultService().prepareFormDataResponse(deliveryAddressRequest, validationResult);
 
-			if (pageAction != null) {
-				switch (pageAction) {
-				case GET_DELIVERY_ADDRESS_METHOD: {
-					String deliveryAddressId = FormDataService.defaultService().get(deliveryAddressRequest, "id");
-					deliveryAddressResponse.getSubmitForm().getResult().put(ADDRESS_BY_ID_KEY, SinglePageCheckoutFacade.defaultFacade().loadAddressById(user, deliveryAddressId));
-					break;
-				}
-				case ADD_DELIVERY_ADDRESS_METHOD: {
-					List<ValidationError> validationErrors = DeliveryAddressService.defaultService().addDeliveryAddressMethod(deliveryAddressRequest, request.getSession(), user);
-					validationResult.getErrors().addAll(validationErrors);
-					break;
-				}
-				case EDIT_DELIVERY_ADDRESS_METHOD: {
-					List<ValidationError> validationErrors = DeliveryAddressService.defaultService().editDeliveryAddressMethod(deliveryAddressRequest, request.getSession(), user);
-					validationResult.getErrors().addAll(validationErrors);
-					break;
-				}
-				case DELETE_DELIVERY_ADDRESS_METHOD: {
-					DeliveryAddressService.defaultService().deleteDeliveryAddressMethod(deliveryAddressRequest, request.getSession(), user);
-					break;
-				}
-				case SELECT_DELIVERY_ADDRESS_METHOD: {
-					String deliveryAddressId = FormDataService.defaultService().get(deliveryAddressRequest, "id");
-					String ebtPaymentRemovalApproved = FormDataService.defaultService().get(deliveryAddressRequest, "ebtPaymentRemovalApproved");
-					List<ValidationError> validationErrors = new ArrayList<ValidationError>();
-					ErpAddressModel deliveryAddress = user.getShoppingCart().getDeliveryAddress();
-                        if (ebtPaymentRemovalApproved == null && deliveryAddressId != null && deliveryAddress != null && !deliveryAddressId.equals(deliveryAddress.getId())) {
-						validationErrors.addAll(DeliveryAddressService.defaultService().checkEbtAddressPaymentSelectionByAddressId(user, deliveryAddressId));
-					}
-					if (validationErrors.isEmpty()) {
-						String pickupPhone = FormDataService.defaultService().get(deliveryAddressRequest, deliveryAddressId +  "_phone");
-						validationErrors = DeliveryAddressService.defaultService().selectDeliveryAddressMethod(deliveryAddressId, pickupPhone, pageAction.actionName, request.getSession(), user);
-						PaymentService.defaultService().deselectEbtPayment(user, request.getSession());
-					}
-					validationResult.getErrors().addAll(validationErrors);
-				}
-				default:
-					break;
-				}
-				if (!PageAction.GET_DELIVERY_ADDRESS_METHOD.equals(pageAction)) {
-					deliveryAddressResponse.getSubmitForm().setResult(SinglePageCheckoutFacade.defaultFacade().loadByPageAction(user, request, pageAction, validationResult));
-				}
-			}
-			deliveryAddressResponse.getSubmitForm().setSuccess(validationResult.getErrors().isEmpty());
-			writeResponseData(response, deliveryAddressResponse);
-		} catch (FDResourceException e) {
-			returnHttpError(500, "Failed to submit delivery address action.", e);
-		} catch (IOException e) {
-			returnHttpError(500, "Failed to load Learn More media for restriction message.", e);
-		} catch (TemplateException e) {
-			returnHttpError(500, "Failed to render Learn More HTML media for restricion message.", e);
-		} catch (JspException e) {
-			returnHttpError(500, "Failed to check delivery pass status.", e);
-		} catch (RedirectToPage e) {
-			returnHttpError(500, "Failed to load checkout page data due to technical difficulties.", e);
-		}
-	}
+            if (pageAction != null) {
+                switch (pageAction) {
+                    case GET_DELIVERY_ADDRESS_METHOD: {
+                        String deliveryAddressId = FormDataService.defaultService().get(deliveryAddressRequest, "id");
+                        deliveryAddressResponse.getSubmitForm().getResult().put(ADDRESS_BY_ID_KEY,
+                                SinglePageCheckoutFacade.defaultFacade().loadAddressById(user, deliveryAddressId));
+                        break;
+                    }
+                    case ADD_DELIVERY_ADDRESS_METHOD: {
+                        List<ValidationError> validationErrors = DeliveryAddressService.defaultService().addDeliveryAddressMethod(deliveryAddressRequest, request.getSession(),
+                                user);
+                        validationResult.getErrors().addAll(validationErrors);
+                        break;
+                    }
+                    case EDIT_DELIVERY_ADDRESS_METHOD: {
+                        List<ValidationError> validationErrors = DeliveryAddressService.defaultService().editDeliveryAddressMethod(deliveryAddressRequest, request.getSession(),
+                                user);
+                        validationResult.getErrors().addAll(validationErrors);
+                        break;
+                    }
+                    case DELETE_DELIVERY_ADDRESS_METHOD: {
+                        DeliveryAddressService.defaultService().deleteDeliveryAddressMethod(deliveryAddressRequest, request.getSession(), user);
+                        break;
+                    }
+                    case SELECT_DELIVERY_ADDRESS_METHOD: {
+                        String deliveryAddressId = FormDataService.defaultService().get(deliveryAddressRequest, "id");
+                        String ebtPaymentRemovalApproved = FormDataService.defaultService().get(deliveryAddressRequest, "ebtPaymentRemovalApproved");
+                        List<ValidationError> validationErrors = new ArrayList<ValidationError>();
+                        ErpAddressModel deliveryAddress = user.getShoppingCart().getDeliveryAddress();
+                        if (deliveryAddressId != null) {
+                            if (ebtPaymentRemovalApproved == null && deliveryAddress != null && !deliveryAddressId.equals(deliveryAddress.getId())) {
+                                validationErrors.addAll(DeliveryAddressService.defaultService().checkEbtAddressPaymentSelectionByAddressId(user, deliveryAddressId));
+                            }
+                            if (validationErrors.isEmpty()) {
+                                String pickupPhone = FormDataService.defaultService().get(deliveryAddressRequest, deliveryAddressId + "_phone");
+                                validationErrors = DeliveryAddressService.defaultService().selectDeliveryAddressMethod(deliveryAddressId, pickupPhone, pageAction.actionName,
+                                        request.getSession(), user);
+                                PaymentService.defaultService().deselectEbtPayment(user, request.getSession());
+                            }
+                        }
+                        validationResult.getErrors().addAll(validationErrors);
+                    }
+                    default:
+                        break;
+                }
+                if (!PageAction.GET_DELIVERY_ADDRESS_METHOD.equals(pageAction)) {
+                    deliveryAddressResponse.getSubmitForm().setResult(SinglePageCheckoutFacade.defaultFacade().loadByPageAction(user, request, pageAction, validationResult));
+                }
+            }
+            deliveryAddressResponse.getSubmitForm().setSuccess(validationResult.getErrors().isEmpty());
+            writeResponseData(response, deliveryAddressResponse);
+        } catch (FDResourceException e) {
+            returnHttpError(500, "Failed to submit delivery address action.", e);
+        } catch (IOException e) {
+            returnHttpError(500, "Failed to load Learn More media for restriction message.", e);
+        } catch (TemplateException e) {
+            returnHttpError(500, "Failed to render Learn More HTML media for restricion message.", e);
+        } catch (JspException e) {
+            returnHttpError(500, "Failed to check delivery pass status.", e);
+        } catch (RedirectToPage e) {
+            returnHttpError(500, "Failed to load checkout page data due to technical difficulties.", e);
+        }
+    }
 
-	@Override
-	protected int getRequiredUserLevel() {
-		return FDUserI.SIGNED_IN;
-	}
+    @Override
+    protected int getRequiredUserLevel() {
+        return FDUserI.SIGNED_IN;
+    }
 
-	@Override
-	protected boolean synchronizeOnUser() {
-		return false;
-	}
+    @Override
+    protected boolean synchronizeOnUser() {
+        return false;
+    }
 
 }
