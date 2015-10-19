4537f388ebe4dcab0931c8f25613e48c269fa040
ghorak <ghorak@842219ff-9341-0410-a943-573460bdd4de>
2015-10-19 10:33:37 +0000

    #APPDEV-4476 fixed grid w/ 4 items per line - feature DISABLED (it may not be included in Oct R4)
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@36771 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/fd/common/dispatcher.js
+++ projects/Media/docroot/assets/javascript/fd/common/dispatcher.js
@@ -4,12 +4,15 @@ var FreshDirect = FreshDirect || {};
 (function (fd) {
   "use strict";
 
+  // TODO: set this to true if #APPDEV-4476 is released (or if any other A/B testing feature requires it)
+  var DECORATE_BODY = false;
+
   var bus = new Bacon.Bus();
 
   var signal = function(to,body){
     try {
       // add metadata and abFeatures to body if not present
-      if (body && typeof body === 'object') {
+      if (DECORATE_BODY && body && typeof body === 'object') {
         body.metadata = body.metadata || fd.metaData;
         body.abFeatures = body.abFeatures || fd.features && fd.features.active;
       }
