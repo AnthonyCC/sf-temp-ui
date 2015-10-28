f241b9da51f17552e424e1bb692d39b77cb4b792
jjoseph <jjoseph@842219ff-9341-0410-a943-573460bdd4de>
2015-10-28 17:47:06 +0000

    APPDEV-4381:Legal - Terms of Use, Initial Check-in
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@37156 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/action/fdstore/RegistrationAction.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/action/fdstore/RegistrationAction.java
@@ -421,7 +421,9 @@ public class RegistrationAction extends WebActionSupport {
 				customerInfo.setReferralProgId(user.getLastRefProgId());
 				customerInfo.setReferralProgInvtId(user.getLastRefProgInvtId());
 				
-
+				/*  APPDEV-4381   */
+				customerInfo.setFdTcAgree("X");
+				customerInfo.setFdTcAgreeDate(new Date());
 				
 				erpCustomer.setCustomerInfo(customerInfo);
 				ErpAddressModel erpAddress = null;
