1d1e6163d16c83790eb2c1f731eeeb6cd9303c72
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2015-10-31 14:13:32 +0000

    update in process state.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@37249 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDStore/src/com/freshdirect/fdstore/customer/ejb/FDUserDAO.java
+++ projects/FDStore/src/com/freshdirect/fdstore/customer/ejb/FDUserDAO.java
@@ -1198,7 +1198,7 @@ public class FDUserDAO {
 		
 	}
 
-	private static final String IS_READY_FOR_PICK = "select 1 from cust.sale where (IN_MODIFY is null OR status IN ('PRC', 'PNA')) and id = ?";
+	private static final String IS_READY_FOR_PICK = "select 1 from cust.sale where IN_MODIFY is null AND status NOT IN ('NEW', 'NSM', 'MOD', 'MOC', 'AUF', 'CAN', 'AVE') and id = ?";
 
 	public static boolean isReadyForPick(Connection conn, String id) throws SQLException {
 
