22621749133b636f8268406b459ff585a6a73cab
aneela <aneela@842219ff-9341-0410-a943-573460bdd4de>
2016-01-09 00:35:17 +0000

    Fixing the merge issue.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@39525 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/expresscheckout/service/SinglePageCheckoutFacade.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/expresscheckout/service/SinglePageCheckoutFacade.java
@@ -249,16 +249,15 @@ public class SinglePageCheckoutFacade {
                 result.put(SUB_TOTAL_BOX_JSON_KEY, CartDataService.defaultService().loadCartDataSubTotalBox(request, user));
                 break;
             }            
+            
+            case APPLY_CSR_METHOD:{
+                result.put(SUB_TOTAL_BOX_JSON_KEY, CartDataService.defaultService().loadCartDataSubTotalBox(request, user));
+                break;
+            }
             case APPLY_ETIP:{            	            	
             	CartData loadCartData = CartDataService.defaultService().loadCartData(request, user);
             	loadCartData.setTipAppliedTick(true);
                 result.put(CART_DATA_JSON_KEY, SoyTemplateEngine.convertToMap(loadCartData));
-                result.put(SUB_TOTAL_BOX_JSON_KEY, CartDataService.defaultService().loadCartDataSubTotalBox(request, user));
-                break;
-            }
-                break;
-            }
-            case APPLY_CSR_METHOD:{
                 result.put(SUB_TOTAL_BOX_JSON_KEY, CartDataService.defaultService().loadCartDataSubTotalBox(request, user));
                 break;
             }
