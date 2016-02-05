b667794a4f81cc04b5d8343422c50157e6601bf1
vduka <vduka@842219ff-9341-0410-a943-573460bdd4de>
2016-02-05 16:05:29 +0000

    [APPBUG-4336] Fixed Primary home selector path matching
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@40402 842219ff-9341-0410-a943-573460bdd4de

--- projects/cms-gwt/src/com/freshdirect/cms/ui/client/fields/PrimaryHomeSelectorField.java
+++ projects/cms-gwt/src/com/freshdirect/cms/ui/client/fields/PrimaryHomeSelectorField.java
@@ -32,248 +32,243 @@ import com.freshdirect.cms.ui.model.GwtNodeContext;
 import com.freshdirect.cms.ui.model.OneToManyModel;
 
 public class PrimaryHomeSelectorField extends OneToManyRelationField {
-	private String storeKey;
-	
-	// Store Key -> {set parent category keys}
-	private Map<String,Set<String>> mapping;
 
-	private GwtNodeContext contexts;
+    private String storeKey;
 
-	private static final Set<String> allowedTypes = new HashSet<String>();
-	static {
-		allowedTypes.add("Category");
-	}
-	
-	public PrimaryHomeSelectorField(GwtNodeContext contexts, Map<String,Set<String>> mapping, String storeKey) {
-		super("PRIMARY_HOME", allowedTypes, false, false, "Product");
-		
-		this.contexts = contexts;
-		this.storeKey = storeKey;
-		this.mapping = mapping;
-	}
+    // Store Key -> {set parent category keys}
+    private Map<String, Set<String>> mapping;
 
-	@Override
-	protected void initDNDListener() {
-		// DO NOTHING
-	}
-	
-	private ToolButton addPrimaryHomeButton;
-	
-	@Override
-	protected void setupTopToolbar(ToolBar aToolBar) {
-		addPrimaryHomeButton = new ToolButton( "add-relation" );
-		addPrimaryHomeButton.setToolTip( new ToolTipConfig( "ADD", "Add a relation..." ) );
-	}
+    private GwtNodeContext contexts;
 
-	@Override
-	protected List<ColumnConfig> setupExtraColumns() {
-		List<ColumnConfig> config = new ArrayList<ColumnConfig>();
+    private static final Set<String> allowedTypes = new HashSet<String>();
+    static {
+        allowedTypes.add("Category");
+    }
 
-		{
-			ColumnConfig column = new ColumnConfig();
-			column.setId("label");
-			column.setHeader("label");
-			column.setSortable(false);
-			column.setMenuDisabled(true);
-			column.setWidth(MAIN_LABEL_WIDTH);
-			// column.setRenderer(Renderers.GRID_LINK_RENDERER);
-			column.setRenderer(new Renderers.LabelRenderer("label", "label"));
-			config.add(column);
-		}
-		
-		// [change] button
-		{
-			ColumnConfig cfg = new ColumnConfig();
-			cfg.setId("change");
-			cfg.setSortable(false);
-			cfg.setFixed(true);
-			cfg.setMenuDisabled(true);
-			cfg.setWidth(80);
-			cfg.setStyle("vertical-align: middle;");
-			cfg.setAlignment(HorizontalAlignment.RIGHT);
-			cfg.setRenderer(new GridCellRenderer<OneToManyModel>() {
-				@Override
-				public Object render(final OneToManyModel model, String property,
-						ColumnData config, int rowIndex, int colIndex,
-						final ListStore<OneToManyModel> store, Grid<OneToManyModel> grid) {
-					
-					final int row = store.indexOf( model );
+    public PrimaryHomeSelectorField(GwtNodeContext contexts, Map<String, Set<String>> mapping, String storeKey) {
+        super("PRIMARY_HOME", allowedTypes, false, false, "Product");
 
-					Button b = new Button("change",
-						new SelectionListener<ButtonEvent>() {
-							@Override
-							public void componentSelected(ButtonEvent ce) {
-								ContentTreePopUp popup = setupPopup(model);
-	
-								popup.show();
-                popup.setSize(401, 601);
-                popup.setSize(650, 600);
-							}
-					});
+        this.contexts = contexts;
+        this.storeKey = storeKey;
+        this.mapping = mapping;
+    }
 
-					b.setId("chg-btn" + model.getKey());
-					
-					// adjust visual state
-					boolean flag = Boolean.valueOf( (String) model.get("_enabled"));
-					b.setEnabled(flag);
-	
-					return b;
-				}
-			});
-	
-			config.add(cfg);
-		}
+    @Override
+    protected void initDNDListener() {
+        // DO NOTHING
+    }
 
-		return config;
-	}
+    private ToolButton addPrimaryHomeButton;
 
+    @Override
+    protected void setupTopToolbar(ToolBar aToolBar) {
+        addPrimaryHomeButton = new ToolButton("add-relation");
+        addPrimaryHomeButton.setToolTip(new ToolTipConfig("ADD", "Add a relation..."));
+    }
 
+    @Override
+    protected List<ColumnConfig> setupExtraColumns() {
+        List<ColumnConfig> config = new ArrayList<ColumnConfig>();
 
-	/**
-	 * Prepare a popup for Primary Home selection
-	 */
-	private ContentTreePopUp setupPopup(OneToManyModel model) {
-		final ContentTreePopUp popup = ContentTreePopUp.getPrimaryHomePopup(getAllowedTypes());
+        {
+            ColumnConfig column = new ColumnConfig();
+            column.setId("label");
+            column.setHeader("label");
+            column.setSortable(false);
+            column.setMenuDisabled(true);
+            column.setWidth(MAIN_LABEL_WIDTH);
+            // column.setRenderer(Renderers.GRID_LINK_RENDERER);
+            column.setRenderer(new Renderers.LabelRenderer("label", "label"));
+            config.add(column);
+        }
 
-		final NodeTree tree =  popup.getTreepanel();
-		
-		final TreeStore<TreeContentNodeModel> _treeStore = tree.getMainStore(); 
+        // [change] button
+        {
+            ColumnConfig cfg = new ColumnConfig();
+            cfg.setId("change");
+            cfg.setSortable(false);
+            cfg.setFixed(true);
+            cfg.setMenuDisabled(true);
+            cfg.setWidth(80);
+            cfg.setStyle("vertical-align: middle;");
+            cfg.setAlignment(HorizontalAlignment.RIGHT);
+            cfg.setRenderer(new GridCellRenderer<OneToManyModel>() {
 
-		_treeStore.removeAll();
+                @Override
+                public Object render(final OneToManyModel model, String property, ColumnData config, int rowIndex, int colIndex, final ListStore<OneToManyModel> store,
+                        Grid<OneToManyModel> grid) {
 
-		final String selKey = model.get("_storeKey");
+                    final int row = store.indexOf(model);
 
-		// setup tree popup with 'parent' categories just for the selected store
+                    Button b = new Button("change", new SelectionListener<ButtonEvent>() {
 
-		for (String p : contexts.getPaths()) {
-			if (p.contains(selKey)) {
-				String[] _pathFragments = p.split("\\|");
-				//String[] _labelFragments = contexts.getLabel(p).split(" > ");
-				
-				TreeContentNodeModel alma;
-				alma = new TreeContentNodeModel("Category",
-						contexts.getLabel(p),
-						_pathFragments[_pathFragments.length-2]);
-				alma.setHasChildren(false);
-				_treeStore.insert(alma, 0, false);
-			}
-		}
-		
-		tree.hideToolbar();
-		popup.setHeading(getFieldLabel());
-		popup.addListener(Events.Select, new Listener<BaseEvent>() {
-			public void handleEvent(BaseEvent be) {
-				addOneToManyModels(popup.getSelectedItems());
-			}
-		});
-		return popup;
-	}
+                        @Override
+                        public void componentSelected(ButtonEvent ce) {
+                            ContentTreePopUp popup = setupPopup(model);
 
-	/**
-	 * Event hook invoked when context toolbar is changed
-	 * 
-	 * @param storeKey
-	 */
-	public void changeStoreKey(String storeKey) {
-		this.storeKey = storeKey;
+                            popup.show();
+                            popup.setSize(401, 601);
+                            popup.setSize(650, 600);
+                        }
+                    });
 
-		adjustValues();
-	}	
+                    b.setId("chg-btn" + model.getKey());
 
+                    // adjust visual state
+                    boolean flag = Boolean.valueOf((String) model.get("_enabled"));
+                    b.setEnabled(flag);
 
-	/**
-	 * Adjust list values according to changed store key
-	 */
-	protected void adjustValues() {
-		if (this.storeKey == null) {
-			// disable all ...
-			for (OneToManyModel m : store.getModels()) {
-				Component c = ComponentManager.get().get("chg-btn" + m.getKey());
-				if (c instanceof Button) {
-					((Button)c).setEnabled(false);
-				}
-			}
-			return;
-		}
-		
-		List<OneToManyModel> parentz = store.getModels();
-		for (OneToManyModel m : parentz) {
-			final String _pKey = m.getKey();
-			for (String sKey : mapping.keySet()) {
-				if (mapping.get(sKey).contains(_pKey)) {
-					final boolean flag = sKey.equals(storeKey);
-					m.set("_storeKey", sKey);
-					m.set("_storeId", sKey.split(":")[1]);
-					
-					
+                    return b;
+                }
+            });
+
+            config.add(cfg);
+        }
+
+        return config;
+    }
+
+    /**
+     * Prepare a popup for Primary Home selection
+     */
+    private ContentTreePopUp setupPopup(OneToManyModel model) {
+        final ContentTreePopUp popup = ContentTreePopUp.getPrimaryHomePopup(getAllowedTypes());
+
+        final NodeTree tree = popup.getTreepanel();
+
+        final TreeStore<TreeContentNodeModel> _treeStore = tree.getMainStore();
+
+        _treeStore.removeAll();
+
+        final String selKey = model.get("_storeKey");
+
+        // setup tree popup with 'parent' categories just for the selected store
+
+        for (String p : contexts.getPaths()) {
+            if (p.contains(selKey)) {
+                String[] _pathFragments = p.split("\\|");
+                // String[] _labelFragments = contexts.getLabel(p).split(" > ");
+
+                TreeContentNodeModel alma;
+                alma = new TreeContentNodeModel("Category", contexts.getLabel(p), _pathFragments[_pathFragments.length - 2]);
+                alma.setHasChildren(false);
+                _treeStore.insert(alma, 0, false);
+            }
+        }
+
+        tree.hideToolbar();
+        popup.setHeading(getFieldLabel());
+        popup.addListener(Events.Select, new Listener<BaseEvent>() {
+
+            public void handleEvent(BaseEvent be) {
+                addOneToManyModels(popup.getSelectedItems());
+            }
+        });
+        return popup;
+    }
+
+    /**
+     * Event hook invoked when context toolbar is changed
+     * 
+     * @param storeKey
+     */
+    public void changeStoreKey(String storeKey) {
+        this.storeKey = storeKey;
+
+        adjustValues();
+    }
+
+    /**
+     * Adjust list values according to changed store key
+     */
+    protected void adjustValues() {
+        if (this.storeKey == null) {
+            // disable all ...
+            for (OneToManyModel m : store.getModels()) {
+                Component c = ComponentManager.get().get("chg-btn" + m.getKey());
+                if (c instanceof Button) {
+                    ((Button) c).setEnabled(false);
+                }
+            }
+            return;
+        }
+
+        List<OneToManyModel> parentz = store.getModels();
+        for (OneToManyModel m : parentz) {
+            final String _pKey = m.getKey();
+            for (String sKey : mapping.keySet()) {
+                if (mapping.get(sKey).contains(_pKey)) {
+                    final boolean flag = sKey.equals(storeKey);
+                    m.set("_storeKey", sKey);
+                    m.set("_storeId", sKey.split(":")[1]);
+
                     for (String p : contexts.getPaths()) {
                         // Check if path ends with top category key
-                        if (p.endsWith(_pKey)) {
+                        if (p.contains("|" + _pKey + "|")) {
                             m.set("label", contexts.getLabel(p));
                             break;
                         }
                     }
-					
-					m.set("_enabled", flag ? "true" : "false");
 
-					// adjust visual state
-					Component c = ComponentManager.get().get("chg-btn" + m.getKey());
-					if (c instanceof Button) {
-						((Button)c).setEnabled(flag);
-					}
-					break;
-				}
-			}
-		}		
-	}
-	
-	@Override
-	protected boolean addSingleValueToStore(OneToManyModel value) {
-		if (value == null) {
-			return false;
-		}
-		
-		final String sKey = findStoreKey(value.getKey());
-		if (sKey == null) {
-			CmsGwt.debug("Ooops, no store key found for selected category " + value.getKey());
-			return false;
-		}
-		
-		// cat key -> store key
-		List<OneToManyModel> newVals = new ArrayList<OneToManyModel>();
-		for (OneToManyModel m : store.getModels()) {
-			final String key = m.getKey();
-			String ssKey = findStoreKey(key);
-			
-			if (!sKey.equalsIgnoreCase(ssKey)) {
-				newVals.add(m);
-			} else {
-				CmsGwt.debug("Removing parent cat " + key);
-			}
-		}
-		newVals.add(value);
-		
-		setValue(newVals);
-		
-		return true;
-	}
+                    m.set("_enabled", flag ? "true" : "false");
 
-	
-	@Override
-	public void setValue(List<OneToManyModel> values) {
-		super.setValue(values);
-		
-		adjustValues();
-	}
+                    // adjust visual state
+                    Component c = ComponentManager.get().get("chg-btn" + m.getKey());
+                    if (c instanceof Button) {
+                        ((Button) c).setEnabled(flag);
+                    }
+                    break;
+                }
+            }
+        }
+    }
 
-	private String findStoreKey(String catKey) {
-		for (String storeKey : mapping.keySet()) {
-			for (String aCatKey : mapping.get(storeKey)) {
-				if (aCatKey.equalsIgnoreCase(catKey)) {
-					return storeKey;
-				}
-			}
-		}
-		return null;
-	}
+    @Override
+    protected boolean addSingleValueToStore(OneToManyModel value) {
+        if (value == null) {
+            return false;
+        }
+
+        final String sKey = findStoreKey(value.getKey());
+        if (sKey == null) {
+            CmsGwt.debug("Ooops, no store key found for selected category " + value.getKey());
+            return false;
+        }
+
+        // cat key -> store key
+        List<OneToManyModel> newVals = new ArrayList<OneToManyModel>();
+        for (OneToManyModel m : store.getModels()) {
+            final String key = m.getKey();
+            String ssKey = findStoreKey(key);
+
+            if (!sKey.equalsIgnoreCase(ssKey)) {
+                newVals.add(m);
+            } else {
+                CmsGwt.debug("Removing parent cat " + key);
+            }
+        }
+        newVals.add(value);
+
+        setValue(newVals);
+
+        return true;
+    }
+
+    @Override
+    public void setValue(List<OneToManyModel> values) {
+        super.setValue(values);
+
+        adjustValues();
+    }
+
+    private String findStoreKey(String catKey) {
+        for (String storeKey : mapping.keySet()) {
+            for (String aCatKey : mapping.get(storeKey)) {
+                if (aCatKey.equalsIgnoreCase(catKey)) {
+                    return storeKey;
+                }
+            }
+        }
+        return null;
+    }
 }
