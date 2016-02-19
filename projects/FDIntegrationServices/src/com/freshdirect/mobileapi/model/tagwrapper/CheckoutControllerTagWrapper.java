947b7008acc21ab5e9b777095145ae07d25f4db4
psingh <psingh@842219ff-9341-0410-a943-573460bdd4de>
2016-02-19 15:29:27 +0000

    APPBUG-4322: Default sales tax amount is displaying for all modify order flow
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@40890 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDIntegrationServices/src/com/freshdirect/mobileapi/model/tagwrapper/CheckoutControllerTagWrapper.java
+++ projects/FDIntegrationServices/src/com/freshdirect/mobileapi/model/tagwrapper/CheckoutControllerTagWrapper.java
@@ -79,10 +79,10 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     public ResultBundle submitOrder() throws FDException {
         addExpectedSessionValues(new String[] { SESSION_PARAM_PROCESSING_ORDER, SESSION_PARAM_MAKEGOOD_COMPLAINT,
                 SESSION_PARAM_APPLICATION, SESSION_PARAM_MAKE_GOOD_ORDER, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_CUSTOMER_SERVICE_REP,
-                SESSION_PARAM_RECENT_ORDER_NUMBER, SessionName.PENDING_CONVERSION_ORDER_MODIFIED_MODELS, SessionName.PENDING_HELP_EMAIL_EVENT, SessionName.PENDING_SHOP_9_MODELS }, new String[] { SESSION_PARAM_PROCESSING_ORDER, SESSION_PARAM_USER,
+                SESSION_PARAM_RECENT_ORDER_NUMBER, SessionName.PENDING_CONVERSION_ORDER_MODIFIED_MODELS, SessionName.PENDING_HELP_EMAIL_EVENT, SessionName.PENDING_SHOP_9_MODELS,TAXATION_TYPE_SESSION }, new String[] { SESSION_PARAM_PROCESSING_ORDER, SESSION_PARAM_USER,
                 SESSION_PARAM_SMART_STORE_PREV_RECOMMENDATIONS, SESSION_PARAM_AUTHORIZED_PEOPLE, SESSION_PARAM_PICKUP_AGREEMENT,
                 SESSION_PARAM_RECENT_ORDER_NUMBER, SESSION_PARAM_DLV_PASS_SESSION_ID, SESSION_PARAM_REFERENCED_ORDER,
-				SESSION_PARAM_MAKE_GOOD_ORDER, SessionName.PENDING_CONVERSION_ORDER_MODIFIED_MODELS, SessionName.PENDING_HELP_EMAIL_EVENT, SessionName.PENDING_SHOP_9_MODELS,SESSION_PARAM_AUTH_FAIL_MESSAGE }); //gets,sets
+				SESSION_PARAM_MAKE_GOOD_ORDER, SessionName.PENDING_CONVERSION_ORDER_MODIFIED_MODELS, SessionName.PENDING_HELP_EMAIL_EVENT, SessionName.PENDING_SHOP_9_MODELS,SESSION_PARAM_AUTH_FAIL_MESSAGE,TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { SESSION_PARAM_RECENT_ORDER_NUMBER, REQ_PARAM_IGNORE_PROMO_ERRORS, TAXATION_TYPE }, new String[] { REQ_PARAM_PAYMENT_METHOD_ID,
                 REQ_PARAM_BILLING_REF, TAXATION_TYPE });//gets,sets
         getWrapTarget().setActionName(ACTION_SUBMIT_ORDER);
@@ -125,8 +125,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     }
 
     public ResultBundle setPaymentMethod(String paymentMethodId, String billingReference) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT,SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT,SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { REQ_PARAM_CHEF_TABLE, REQ_PARAM_PAYMENT_METHOD_ID, REQ_PARAM_BILLING_REF,
                 REQ_PARAM_GIFT_CARD, REQ_PARAM_DONATION, REQ_PARAM_BYPASS_ACCOUNT_CHECK, TAXATION_TYPE }, new String[] { REQ_PARAM_PAYMENT_METHOD_ID,
                 REQ_PARAM_BILLING_REF, TAXATION_TYPE });//gets,sets
@@ -139,8 +139,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     }
 
     public ResultBundle addAndSetDeliveryAddress(DeliveryAddressRequest deliveryAddress) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SessionName.SIGNUP_WARNING, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SessionName.SIGNUP_WARNING, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] {EnumUserInfoName.DLV_FIRST_NAME.getCode(),EnumUserInfoName.DLV_LAST_NAME.getCode(),EnumUserInfoName.DLV_HOME_PHONE.getCode(),EnumUserInfoName.DLV_HOME_PHONE_EXT.getCode(),
         		EnumUserInfoName.DLV_ADDRESS_1.getCode(),EnumUserInfoName.DLV_ADDRESS_2.getCode(),EnumUserInfoName.DLV_APARTMENT.getCode(),EnumUserInfoName.DLV_CITY.getCode(),EnumUserInfoName.DLV_STATE.getCode(),EnumUserInfoName.DLV_ZIPCODE.getCode(),
         		EnumUserInfoName.DLV_COUNTRY.getCode(),EnumUserInfoName.DLV_DELIVERY_INSTRUCTIONS.getCode(),EnumUserInfoName.DLV_ALTERNATE_DELIVERY.getCode(),EnumUserInfoName.DLV_ALT_FIRSTNAME.getCode(),
@@ -218,8 +218,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     }
 
     public ResultBundle addPaymentMethod(PaymentMethodRequest paymentMethod) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { REQ_PARAM_CARD_EXP_MONTH, REQ_PARAM_CARD_EXP_YEAR, REQ_PARAM_CARD_BRAND,
         		REQ_PARAM_ACCOUNT_NUMBER, REQ_PARAM_ABA_ROUTE_NUMBER, REQ_PARAM_BANK_NAME, REQ_PARAM_BYPASS_BAD_ACCOUNT_CHECK, REQ_PARAM_TERMS,
         		REQ_PARAM_ACCOUNT_NUMBER_VERIFY,REQ_PARAM_BANK_ACCOUNT_TYPE,REQ_PARAM_ACCOUNT_HOLDER,REQ_PARAM_BIL_ADDRESS_1,
@@ -257,8 +257,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     }
     
     public ResultBundle addPaymentMethodEx(PaymentMethodRequest paymentMethod) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { REQ_PARAM_CARD_EXP_MONTH, REQ_PARAM_CARD_EXP_YEAR, REQ_PARAM_CARD_BRAND,
         		REQ_PARAM_ACCOUNT_NUMBER, REQ_PARAM_ABA_ROUTE_NUMBER, REQ_PARAM_BANK_NAME, REQ_PARAM_BYPASS_BAD_ACCOUNT_CHECK, REQ_PARAM_TERMS,
         		REQ_PARAM_ACCOUNT_NUMBER_VERIFY,REQ_PARAM_BANK_ACCOUNT_TYPE,REQ_PARAM_ACCOUNT_HOLDER,REQ_PARAM_BIL_ADDRESS_1,
@@ -310,8 +310,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     }
 
     public ResultBundle addAndSetPaymentMethod(PaymentMethodRequest paymentMethod) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { REQ_PARAM_CARD_EXP_MONTH, REQ_PARAM_CARD_EXP_YEAR, REQ_PARAM_CARD_BRAND,
         		REQ_PARAM_ACCOUNT_NUMBER, REQ_PARAM_ABA_ROUTE_NUMBER, REQ_PARAM_BANK_NAME, REQ_PARAM_BYPASS_BAD_ACCOUNT_CHECK, REQ_PARAM_TERMS,
         		REQ_PARAM_ACCOUNT_NUMBER_VERIFY,REQ_PARAM_BANK_ACCOUNT_TYPE,REQ_PARAM_ACCOUNT_HOLDER,REQ_PARAM_BIL_ADDRESS_1,
@@ -366,8 +366,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     }
 
     public ResultBundle editPaymentMethod(PaymentMethodRequest paymentMethod) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SESSION_PARAM_PYMT_VERIFYFLD, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { REQ_PARAM_CARD_EXP_MONTH, REQ_PARAM_CARD_EXP_YEAR, REQ_PARAM_CARD_BRAND,
         		REQ_PARAM_ACCOUNT_NUMBER, REQ_PARAM_ABA_ROUTE_NUMBER, REQ_PARAM_BANK_NAME, REQ_PARAM_BYPASS_BAD_ACCOUNT_CHECK, REQ_PARAM_TERMS,
         		REQ_PARAM_ACCOUNT_NUMBER_VERIFY,REQ_PARAM_BANK_ACCOUNT_TYPE,REQ_PARAM_ACCOUNT_HOLDER,REQ_PARAM_BIL_ADDRESS_1,
@@ -403,8 +403,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     }
 
     public ResultBundle deletePaymentMethod(String paymentMethodId) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { REQ_PARAM_DELETE_PAYMENT_ID, TAXATION_TYPE}, new String[] {TAXATION_TYPE});//gets,sets
         addRequestValue(REQ_PARAM_DELETE_PAYMENT_ID, paymentMethodId);
         getWrapTarget().setActionName(ACTION_DELETE_PAYMENT_METHOD);
@@ -413,8 +413,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     }
 
     public ResultBundle deletePaymentMethodEx(String paymentMethodId) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { REQ_PARAM_DELETE_PAYMENT_ID, TAXATION_TYPE}, new String[] {TAXATION_TYPE});//gets,sets
         addRequestValue(REQ_PARAM_DELETE_PAYMENT_ID, paymentMethodId);
         getWrapTarget().setActionName(ACTION_DELETE_PAYMENT_METHOD);
@@ -438,7 +438,7 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
         }
     
     public ResultBundle reserveDeliveryTimeslot(String deliveryTimeslotId) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_MAKE_GOOD_ORDER }, new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }, new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { REQ_PARAM_CHEF_TABLE, REQ_PARAM_SLOT_ID, TAXATION_TYPE }, new String[] { REQ_PARAM_SLOT_ID,TAXATION_TYPE });//gets,sets
         addRequestValue(REQ_PARAM_SLOT_ID, deliveryTimeslotId);
         getWrapTarget().setActionName(ACTION_RESERVE_DELIVERY_TIMESLOT);
@@ -455,8 +455,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
      * WHAT: constant in tag class that defines URL for page verification.
      */
     public ResultBundle setCheckoutDeliveryAddress(SessionUser user, String selectAddressId, DeliveryAddressType type) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SessionName.SIGNUP_WARNING, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SessionName.SIGNUP_WARNING, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         LOGGER.debug("setCheckoutDeliveryAddress[START] :"+ selectAddressId + ","+type);
         addExpectedRequestValues(new String[] { REQ_PARAM_CHEF_TABLE, REQ_PARAM_PAYMENT_METHOD_ID, REQ_PARAM_BILLING_REF, TAXATION_TYPE }, new String[] {
                 REQ_PARAM_PAYMENT_METHOD_ID, REQ_PARAM_BILLING_REF, TAXATION_TYPE });//gets,sets
@@ -502,8 +502,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     }
     
     public ResultBundle setCheckoutOrderMobileNumber(SessionUser user, String orderMobileNumber) throws FDException	 {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { REQ_PARAM_MOBILE_NUMBER, TAXATION_TYPE}, new String[] {TAXATION_TYPE});//gets,sets
         addRequestValue(REQ_PARAM_MOBILE_NUMBER, orderMobileNumber);
         addRequestValue("orderMobileNumber", orderMobileNumber);
@@ -529,8 +529,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     
     
     public ResultBundle deleteDeliveryAddress(String deleteShipToAddressId) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { REQ_PARAM_DELETE_SHIP_ADDRESS_ID, TAXATION_TYPE}, new String[] {TAXATION_TYPE});//gets,sets
         addRequestValue(REQ_PARAM_DELETE_SHIP_ADDRESS_ID, deleteShipToAddressId);
         getWrapTarget().setActionName(ACTION_DELETE_DELIVERY_ADDRESS);
@@ -539,8 +539,8 @@ public class CheckoutControllerTagWrapper extends ControllerTagWrapper implement
     }
     
     public ResultBundle deleteDeliveryAddressEx(String deleteShipToAddressId) throws FDException {
-        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER },
-                new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER }); //gets,sets
+        addExpectedSessionValues(new String[] { SESSION_PARAM_APPLICATION, SESSION_PARAM_CUSTOMER_SERVICE_REP, SESSION_PARAM_CRM_AGENT, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION },
+                new String[] { SESSION_PARAM_USER, SESSION_PARAM_MAKE_GOOD_ORDER, TAXATION_TYPE_SESSION }); //gets,sets
         addExpectedRequestValues(new String[] { REQ_PARAM_DELETE_SHIP_ADDRESS_ID, TAXATION_TYPE}, new String[] {TAXATION_TYPE});//gets,sets
         addRequestValue(REQ_PARAM_DELETE_SHIP_ADDRESS_ID, deleteShipToAddressId);
         getWrapTarget().setActionName(ACTION_DELETE_DELIVERY_ADDRESS);
