85911839d028216a874147b7afc6523fe3b97e77
gsebestyen <gsebestyen@842219ff-9341-0410-a943-573460bdd4de>
2016-04-13 09:35:46 +0000

    Give Hibernate generated FK constraints human readable names
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@42690 842219ff-9341-0410-a943-573460bdd4de

--- release/db/CMSDraftPublish/CMS-Admin-Database-create-ddl.sql
+++ release/db/CMSDraftPublish/CMS-Admin-Database-create-ddl.sql
@@ -3,6 +3,6 @@ create table Permission (id number(19,0) not null, name varchar2(255 char), prim
 create table Persona (id number(19,0) not null, name varchar2(255 char), primary key (id));
 create table Persona_Permission (Persona_id number(19,0) not null, permissions_id number(19,0) not null);
 create table UserPersona (userId varchar2(255 char) not null, name varchar2(255 char), persona_id number(19,0), primary key (userId));
-alter table Persona_Permission add constraint FK3b2su379nguu0xmlxs57fwayh foreign key (permissions_id) references Permission;
-alter table Persona_Permission add constraint FKp77hxeqgbq1e7q3kacj04j91l foreign key (Persona_id) references Persona;
-alter table UserPersona add constraint FK6oaolax3psuph7v2yg8oqxu00 foreign key (persona_id) references Persona;
+alter table Persona_Permission add constraint FK_PERSPERM_PERMISSION_ID foreign key (permissions_id) references Permission;
+alter table Persona_Permission add constraint FK_PERSPERM_PERSONA_ID foreign key (Persona_id) references Persona;
+alter table UserPersona add constraint FK_USERPERM_PERSONA_ID foreign key (persona_id) references Persona;
