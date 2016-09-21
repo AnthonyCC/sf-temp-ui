ecf50ae48e30d07b4c64c5f62b3cb331df875060
tsoltesz <tsoltesz@842219ff-9341-0410-a943-573460bdd4de>
2016-09-21 08:38:43 +0000

    [APPBUG-4783] displaying didYouMean even if there are results for the search
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@55565 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/search/SearchService.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/search/SearchService.java
@@ -36,8 +36,8 @@ public class SearchService {
 
     private static final SearchService INSTANCE = new SearchService();
 
-
     private static class UnbxdSearchResult {
+
         public final String suggestedTerm;
         public final List<String> spellingSuggestions;
 
@@ -50,29 +50,25 @@ public class SearchService {
         }
     }
 
-
-
     /**
-     * Run UNBXD search operation with the given search term
-     * Repeat the search when no products yielded with search suggestions
+     * Run UNBXD search operation with the given search term Repeat the search when no products yielded with search suggestions
      * 
      * @param searchTerm
      * @return result of the search operation wrapped into temporary class
      * 
-     * @throws IOException search request failed
+     * @throws IOException
+     *             search request failed
      */
     private static UnbxdSearchResult searchUnbxd(final String searchTerm) throws IOException {
         final UnbxdIntegrationService unbxdService = UnbxdIntegrationService.getDefaultService();
 
         final UnbxdSearchResult result;
-        
+
         // do the search
         UnbxdSearchResponseRoot searchResult = unbxdService.searchProducts(searchTerm);
 
-
         // No products found, repeat the search with suggestion if any
-        if ((searchResult.getResponse().getProducts() == null || searchResult.getResponse().getProducts().isEmpty())
-                && (searchResult.getDidYouMeans() != null && !searchResult.getDidYouMeans().isEmpty())) {
+        if (searchResult.getDidYouMeans() != null && !searchResult.getDidYouMeans().isEmpty()) {
 
             final String suggestedTerm = searchResult.getDidYouMeans().get(0).getSuggestion();
 
@@ -82,27 +78,31 @@ public class SearchService {
                 suggestions.add(dymItem.getSuggestion());
             }
 
-            LOGGER.debug("No product found for term '" + searchTerm + "'; look for suggested products using suggestion '" + suggestedTerm + "'");
+            if (searchResult.getResponse().getProducts() == null || searchResult.getResponse().getProducts().isEmpty()) {
+                LOGGER.debug("No product found for term '" + searchTerm + "'; look for suggested products using suggestion '" + suggestedTerm + "'");
+                // repeat search with first suggested term
+                searchResult = unbxdService.searchProducts(suggestedTerm);
+                result = new UnbxdSearchResult(suggestedTerm, suggestions, searchResult.getResponse().getProducts());
+            } else {
+                result = new UnbxdSearchResult(null, suggestions, searchResult.getResponse().getProducts());
+            }
 
-            // repeat search with first suggested term
-            searchResult = unbxdService.searchProducts(suggestedTerm);
-            
-            result = new UnbxdSearchResult(suggestedTerm, suggestions, searchResult.getResponse().getProducts() );
         } else {
-            result = new UnbxdSearchResult(null, Collections.<String>emptyList(), searchResult.getResponse().getProducts() );
+            result = new UnbxdSearchResult(null, Collections.<String> emptyList(), searchResult.getResponse().getProducts());
         }
 
         return result;
     }
 
-
-
     /**
      * Compose final search result from the ones made by Lucene and UNBXD
      * 
-     * @param searchTerm original search term
-     * @param luceneResult outcome of Lucene search
-     * @param unbxdResult outcome of UNBXD search
+     * @param searchTerm
+     *            original search term
+     * @param luceneResult
+     *            outcome of Lucene search
+     * @param unbxdResult
+     *            outcome of UNBXD search
      * 
      * @return composed search result
      */
@@ -120,11 +120,8 @@ public class SearchService {
         return finalResult;
     }
 
-
-    
     /**
-     * Iterate over UNBXD product results and transforms them to ProductModel instances
-     * sorting out non-displayable ones
+     * Iterate over UNBXD product results and transforms them to ProductModel instances sorting out non-displayable ones
      * 
      * @param unbxdProducts
      * @return
@@ -132,32 +129,31 @@ public class SearchService {
     private static List<FilteringSortingItem<ProductModel>> filterProducts(Collection<UnbxdSearchResponseProduct> unbxdProducts) {
         if (unbxdProducts == null || unbxdProducts.size() == 0)
             return Collections.emptyList();
-        
+
         List<FilteringSortingItem<ProductModel>> result = new ArrayList<FilteringSortingItem<ProductModel>>(unbxdProducts.size());
         for (final UnbxdSearchResponseProduct unbxdProduct : unbxdProducts) {
             final ContentNodeModel model = ContentFactory.getInstance().getContentNodeByKey(unbxdProduct.getContentKey());
             if (model instanceof ProductModel && ContentSearchUtil.isDisplayable((ProductModel) model)) {
-                result.add( new FilteringSortingItem<ProductModel>((ProductModel) model) );
+                result.add(new FilteringSortingItem<ProductModel>((ProductModel) model));
             }
         }
 
         return result;
     }
 
-
-
     public static SearchService getInstance() {
         return INSTANCE;
     }
 
-
-
     /**
      * Perform search operation
      * 
-     * @param searchTerm search Term
-     * @param cookies cookies (required by UNBXD feature check)
-     * @param user (required by UNBXD feature check)
+     * @param searchTerm
+     *            search Term
+     * @param cookies
+     *            cookies (required by UNBXD feature check)
+     * @param user
+     *            (required by UNBXD feature check)
      * @param requestUrl
      * @param referer
      * @return
