9e7dc67158e2f21b3bc11190523995b550be8c49
zholoda <zholoda@842219ff-9341-0410-a943-573460bdd4de>
2016-11-17 14:21:40 +0000

    Merged /FreshDirect/features/branches/2016_mobileapi_changes:r60921-60996 to trunk
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@60997 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDStore/src/com/freshdirect/fdstore/content/browse/sorter/AvailabilityComparator.java
+++ projects/FDStore/src/com/freshdirect/fdstore/content/browse/sorter/AvailabilityComparator.java
@@ -3,20 +3,19 @@ package com.freshdirect.fdstore.content.browse.sorter;
 import java.util.Comparator;
 
 import com.freshdirect.fdstore.content.FilteringProductItem;
-import com.freshdirect.fdstore.content.PopulatorUtil;
-import com.freshdirect.fdstore.content.SkuModel;
+import com.freshdirect.fdstore.content.ProductModel;
 
 
 public class AvailabilityComparator implements Comparator<FilteringProductItem> {
 	
 	@Override
 	public int compare(FilteringProductItem item1, FilteringProductItem item2) {
-		SkuModel sku1 = PopulatorUtil.getDefSku(item1.getProductModel());
-		SkuModel sku2 = PopulatorUtil.getDefSku(item2.getProductModel());
-		
-		boolean availabilty1 = sku1==null ? false : !sku1.isUnavailable();
-		boolean availabilty2 = sku2==null ? false : !sku2.isUnavailable();
+        ProductModel productModel1 = item1.getProductModel();
+        ProductModel productModel2 = item2.getProductModel();
 
+        boolean availabilty1 = productModel1==null ? false : !productModel1.isUnavailable();
+        boolean availabilty2 = productModel2==null ? false : !productModel2.isUnavailable();
+	    
 		if (availabilty1 && !availabilty2){
 			return -1;
 		} else if (!availabilty1 && availabilty2){
