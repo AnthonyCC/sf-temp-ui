470d7f8fad928b062216468f6bbbb946ed2417ff
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2016-12-12 20:06:12 +0000

    Separate UILib folder
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/features/branches/sf2_0_fe@62195 842219ff-9341-0410-a943-573460bdd4de

---
+++ util/bin/windows/link_stuff_Win7.bat
@@ -0,0 +1,26 @@
+@echo off
+rem Windows 7 script to set up shared directories in the development environment
+rem using built-in Win7 command MKLINK
+
+rem Assume the drive where the script is run holds the source code
+set JUNCROOTDRIVE=%CD:~0,2%
+
+rem The root folder holding our source code
+set JUNCROOTSRC=%JUNCROOTDRIVE%\FreshDirect\sf2_0\repo
+
+rem The root folder holding our '\media' folder
+set JUNCROOTMEDIA=%JUNCROOTDRIVE%\FreshDirect\content
+
+rem Media common DAM store
+MKLINK /D /J  %JUNCROOTSRC%\projects\Media\docroot\media				%JUNCROOTMEDIA%\media
+
+MKLINK /D /J %JUNCROOTSRC%\projects\FDWebSite\docroot\WEB-INF\shared	%JUNCROOTSRC%\projects\WebAppCommon\docroot\WEB-INF\shared
+MKLINK /D /J  %JUNCROOTSRC%\projects\FDWebSite\docroot\shared			%JUNCROOTSRC%\projects\WebAppCommon\docroot\shared
+MKLINK /D /J  %JUNCROOTSRC%\projects\FDWebSite\docroot\assets			%JUNCROOTSRC%\projects\Media\docroot\assets
+MKLINK /D /J  %JUNCROOTSRC%\projects\FDWebSite\docroot\ccassets			%JUNCROOTSRC%\projects\Media\docroot\ccassets
+MKLINK /D /J  %JUNCROOTSRC%\projects\FDWebSite\docroot\media_stat		%JUNCROOTSRC%\projects\Media\docroot\media_stat
+MKLINK /D /J %JUNCROOTSRC%\projects\FDWebSite\docroot\WEB-INF\tags	%JUNCROOTSRC%\projects\WebAppCommon\docroot\WEB-INF\shared\tags
+
+rem Dynamic media
+MKLINK /D /J  %JUNCROOTSRC%\projects\FDWebsite\docroot\media			%JUNCROOTMEDIA%\media
+
