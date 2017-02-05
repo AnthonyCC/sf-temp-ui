644177a94403f4b214e84e566a7e485d485e3333
jjoseph <jjoseph@842219ff-9341-0410-a943-573460bdd4de>
2017-02-05 17:57:24 +0000

    FDCMS CleanUp :TechRefresh  SonarQube critical error fix
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@64546 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/backoffice/data/ActionDataRequest.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/backoffice/data/ActionDataRequest.java
@@ -1,16 +1,25 @@
 package com.freshdirect.webapp.ajax.backoffice.data;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 
 
 public class ActionDataRequest implements Serializable{
 
 
-	private Map<String, Object> requestData;
+	private Map<String, Object> requestData =new HashMap<String, Object>();
 
-	private String action;
+	private String action; 
 
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
 
 	public Map<String, Object> getRequestData() {
 		return requestData;
