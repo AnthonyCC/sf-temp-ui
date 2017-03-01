01f81059aa49793d9f727a9581f489ccae743390
rgupta <rgupta@842219ff-9341-0410-a943-573460bdd4de>
2017-03-01 03:33:15 +0000

    AppDev-6008 : Add MOD and MOC order status for order modification.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@65302 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/OrderUtil.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/OrderUtil.java
@@ -35,8 +35,9 @@ public class OrderUtil {
         }
         Date now = new Date(); // now
         boolean beforeCutoffTime = now.before(deliveryCutoffTime);
-
-        return (EnumSaleStatus.NEW.equals(orderStatus) || EnumSaleStatus.SUBMITTED.equals(orderStatus) || EnumSaleStatus.AUTHORIZED.equals(orderStatus) || EnumSaleStatus.AVS_EXCEPTION.equals(orderStatus))
+        return (EnumSaleStatus.NEW.equals(orderStatus) || EnumSaleStatus.SUBMITTED.equals(orderStatus) || 
+        		EnumSaleStatus.AUTHORIZED.equals(orderStatus) || EnumSaleStatus.AVS_EXCEPTION.equals(orderStatus) ||
+        		EnumSaleStatus.MODIFIED.equals(orderStatus) || EnumSaleStatus.MODIFIED_CANCELED.equals(orderStatus))
                 && !EnumSaleType.DONATION.equals(saleType) && beforeCutoffTime;
     }
     
