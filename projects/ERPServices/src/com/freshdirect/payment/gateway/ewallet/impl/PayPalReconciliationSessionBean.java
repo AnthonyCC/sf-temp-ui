09ffdf87e85da99ffe8373e4b798fc87b8c6aa72
psingh <psingh@842219ff-9341-0410-a943-573460bdd4de>
2017-03-21 15:31:46 +0000

    FIN-26: Unable to rerun the Paypal settlement job
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@65906 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/payment/gateway/ewallet/impl/PayPalReconciliationSessionBean.java
+++ projects/ERPServices/src/com/freshdirect/payment/gateway/ewallet/impl/PayPalReconciliationSessionBean.java
@@ -64,7 +64,7 @@ public class PayPalReconciliationSessionBean extends SessionBeanSupport {
 			" values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, systimestamp, ?, ?, ?)";
 	private static final String ACQUIRE_PP_LOCK_UDPATE = "update cust.settlement " +
 			" set IS_LOCKED = 'Y' where id = ? and settlement_source = 'PP'";	
-	private static final String DELETE_EXISING_RECORDS = "delete cust.settlement where PROCESS_PERIOD_START = ? and AFFILIATE_ACCOUNT_ID is null";
+	private static final String DELETE_EXISING_RECORDS = "delete from cust.settlement where SETTLEMENT_SOURCE='PP' and PROCESS_PERIOD_START = ? and AFFILIATE_ACCOUNT_ID is null";
 	
 	public Map<String, Object>  acquirePPLock(Date date) {
 		boolean isNEwRecordRequired = false;
@@ -84,10 +84,10 @@ public class PayPalReconciliationSessionBean extends SessionBeanSupport {
 			conn = this.getConnection();
 			
 			if (date != null) {
-				ps_delete_existing_record = conn.prepareStatement(DELETE_EXISING_RECORDS);
+				/*ps_delete_existing_record = conn.prepareStatement(DELETE_EXISING_RECORDS);
 				ps_delete_existing_record.setDate(1, new java.sql.Date(date.getTime()));
 				ps_delete_existing_record.executeQuery();
-				
+				*/
 				ps_query_by_date = conn.prepareStatement(ACQUIRE_PP_LOCK_QUERY);
 				ps_query_by_date.setDate(1, new java.sql.Date(date.getTime()));
 
