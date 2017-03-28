11462716e08664a3cc273509e5dfb2fe78337b2b
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2017-03-28 18:19:31 +0000

    Allow performance monitoring and other forced feature testing (add force disable)
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@66089 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/features/tags/FeatureIsActiveTag.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/features/tags/FeatureIsActiveTag.java
@@ -18,7 +18,8 @@ public class FeatureIsActiveTag extends SimpleTagSupport {
 	private String name;
 	private String featureName;
 	
-	private static String ENABLEFEATURE_CODE = "999";
+	private static String FORCE_DISABLE_FEATURE_CODE = "111";
+	private static String FORCE_ENABLE_FEATURE_CODE = "999";	
 
 	@Override
 	public void doTag() throws JspException, IOException {
@@ -29,10 +30,15 @@ public class FeatureIsActiveTag extends SimpleTagSupport {
 		EnumRolloutFeature feature = FeaturesService.defaultService().parseFeatureName(featureName);
 		boolean result = FeaturesService.defaultService().isFeatureActive(feature, cookies, user);
 		
-		//This is to allow performance monitoring and other forced feature testing (Added for Keynote HomePageRedesign monitoring but can be used for any)
-		if(request.getParameter(featureName) != null && request.getParameter(featureName).equalsIgnoreCase(ENABLEFEATURE_CODE)) {
-			result = true;
+		//This is to allow performance monitoring and other forced feature testing (Added for synthetic HomePageRedesign monitoring but can be used for any)
+		if(request.getParameter(featureName) != null) {
+			if(request.getParameter(featureName).equalsIgnoreCase(FORCE_ENABLE_FEATURE_CODE)) {
+				result = true; //Force enable feature
+			} else if (request.getParameter(featureName).equalsIgnoreCase(FORCE_DISABLE_FEATURE_CODE)) {
+				result = false; //Force disable feature
+			}
 		}
+		
 		context.setAttribute(name, result);
 	}
 	
