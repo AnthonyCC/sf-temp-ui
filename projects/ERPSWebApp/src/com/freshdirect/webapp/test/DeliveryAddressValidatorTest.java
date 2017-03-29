b64b5e9451a243d312d947f83d4e36c3a363908d
aneela <aneela@842219ff-9341-0410-a943-573460bdd4de>
2017-03-29 18:18:09 +0000

    Added Junit Test case APPDEV-5104
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@66167 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/test/DeliveryAddressValidatorTest.java
@@ -0,0 +1,125 @@
+package com.freshdirect.webapp.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.freshdirect.common.address.AddressModel;
+import com.freshdirect.fdlogistics.services.ILogisticsService;
+import com.freshdirect.fdlogistics.services.impl.FDLogisticsService;
+import com.freshdirect.fdlogistics.services.impl.LogisticsServiceLocator;
+import com.freshdirect.fdstore.FDDeliveryManager;
+import com.freshdirect.fdstore.FDResourceException;
+import com.freshdirect.framework.webapp.ActionResult;
+import com.freshdirect.webapp.taglib.fdstore.DeliveryAddressValidator;
+import mockit.Mock;
+import mockit.MockUp;
+
+public class DeliveryAddressValidatorTest {
+	
+	@Test
+	public void testValidateAddressWithoutScrub() throws FDResourceException {		
+		
+		new MockUp<FDDeliveryManager>() {
+			@Mock
+			void $init() {
+			}
+		};
+		
+		new MockUp<LogisticsServiceLocator>() {
+			
+			@Mock
+			void $init() {
+							}
+			
+			@Mock
+			public ILogisticsService getLogisticsService() {
+				return new FDLogisticsService();
+			}
+		};
+		
+		
+		AddressModel addressModel = new AddressModel();
+		addressModel.setAddress1("281 N HIGHLAND AVE");
+		addressModel.setAddress2("");
+		addressModel.setCity("Lansdowne");
+		addressModel.setState("PA");
+		addressModel.setZipCode("19050");
+		addressModel.setCountry("US");
+		
+		DeliveryAddressValidator validator = new DeliveryAddressValidator(addressModel);
+		
+		
+		
+		ActionResult actionResult = new ActionResult();
+		
+		
+		boolean isAddressValid = validator.validateAddressWithoutScrub(actionResult);
+		
+		if (addressModel.getAddress1().equals("EXCEPTION")) {
+			actionResult.setError("Error");
+			throw new FDResourceException("Not a valid address");
+		}
+		
+		assertTrue(isAddressValid);
+		assertTrue(actionResult.isSuccess());
+		assertNotNull(validator.getScrubbedAddress());
+		
+		assertNotNull(validator.getScrubbedAddress().getScrubbedStreet());
+		assertNotNull(validator.getServiceResult().getAvailableServices());
+		assertEquals("281 N HIGHLAND AVE", validator.getScrubbedAddress().getScrubbedStreet());
+		assertEquals(-75.274421, validator.getScrubbedAddress().getLongitude(),validator.getScrubbedAddress().getLongitude());
+		assertEquals(39.948805, validator.getScrubbedAddress().getLatitude(),validator.getScrubbedAddress().getLongitude());
+	}
+	
+	@Test
+	public void testValidateAddressWithScrub() throws FDResourceException {		
+		
+		new MockUp<FDDeliveryManager>() {
+			@Mock
+			void $init() {
+			}
+		};
+		
+		new MockUp<LogisticsServiceLocator>() {
+			
+			@Mock
+			void $init() {
+				
+			}
+			
+			@Mock
+			public ILogisticsService getLogisticsService() {
+				return new FDLogisticsService();
+			}
+		};
+		
+		AddressModel addressModel = new AddressModel();
+		addressModel.setAddress1("281 N HIGHLAND AVE");
+		addressModel.setAddress2("");
+		addressModel.setCity("Lansdowne");
+		addressModel.setState("PA");
+		addressModel.setZipCode("19050");
+		addressModel.setCountry("US");
+		
+		DeliveryAddressValidator validator = new DeliveryAddressValidator(addressModel);
+		
+		ActionResult actionResult = new ActionResult();
+		
+		boolean isAddressValid = validator.validateScrubbedAddress(actionResult, "GEOCODE_OK");
+		
+		assertTrue(isAddressValid);
+		assertTrue(actionResult.isSuccess());
+		assertNotNull(validator.getScrubbedAddress());
+		
+		assertNotNull(validator.getScrubbedAddress().getScrubbedStreet());
+		assertNotNull(validator.getServiceResult().getAvailableServices());
+		assertEquals("281 N HIGHLAND AVE", validator.getScrubbedAddress().getScrubbedStreet());
+		assertEquals(-75.274421, validator.getScrubbedAddress().getLongitude(),validator.getScrubbedAddress().getLongitude());
+		assertEquals(39.948805, validator.getScrubbedAddress().getLatitude(),validator.getScrubbedAddress().getLongitude());
+	}
+
+
+}
