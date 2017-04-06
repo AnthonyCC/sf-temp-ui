5e9d7cb9c37f79449fc21a046d37d6b3527b2fdd
jjoseph <jjoseph@842219ff-9341-0410-a943-573460bdd4de>
2017-04-06 23:57:41 +0000

    SF17-3 : Service client integration and refactoring
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@66412 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/payment/service/FDECommerceService.java
+++ projects/ERPServices/src/com/freshdirect/payment/service/FDECommerceService.java
@@ -4,8 +4,15 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.apache.log4j.Category;
 import org.springframework.http.HttpEntity;
@@ -16,15 +23,22 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.freshdirect.common.customer.EnumCardType;
 import com.freshdirect.common.pricing.ZoneInfo;
+import com.freshdirect.content.attributes.AttributeException;
+import com.freshdirect.content.attributes.FlatAttribute;
+//import com.freshdirect.content.attributes.FlatAttributeCollection;
 import com.freshdirect.customer.ErpProductFamilyModel;
-import com.freshdirect.ecommerce.data.common.Request;
+import com.freshdirect.ecommerce.data.attributes.FlatAttributeCollection;
 import com.freshdirect.ecommerce.data.common.Response;
+import com.freshdirect.ecommerce.data.common.Request;
+import com.freshdirect.ecommerce.data.payment.BINData;
 import com.freshdirect.fdstore.FDPayPalServiceException;
 //import com.freshdirect.fdlogistics.exception.FDLogisticsServiceException;
 import com.freshdirect.fdstore.FDProductPromotionInfo;
 import com.freshdirect.fdstore.FDResourceException;
 import com.freshdirect.framework.util.log.LoggerFactory;
+import com.freshdirect.payment.BINInfo;
 
 
 public class FDECommerceService extends AbstractService implements IECommerceService {
@@ -35,10 +49,16 @@ public class FDECommerceService extends AbstractService implements IECommerceSer
 	private static final String SAP_PRODUCT_FAMILY_LOADER_GET_SKUCODE_BYPRODFLY_API ="dataloader/sap/skucodesbyproductfamily";
 	private static final String SAP_PRODUCT_FAMILY_LOADER_LOAD_API ="dataloader/sap/productfamily";
 	private static final String PRODUCT_BY_PROMO_TYPE = "productPromo/type/";
+	private static final String PROD_MATERIAL_ATTRIBUTES = "attributes/ids";
+	private static final String GET_ACTIVE_BINS = "bin/activebins";
+	private static final String SAVE_ACTIVE_BINS = "bin/storebins";
+	private static final String SAVE_ATTRIBUTES = "attributes/attributecollection";
+	private static final String LOAD_ATTRIBUTES_DATE = "attributes/timestamp/since/";
+	
 	private static FDECommerceService INSTANCE;
 
 
-	public static FDECommerceService getInstance() {
+	public static IECommerceService getInstance() {
 		if (INSTANCE == null)
 			INSTANCE = new FDECommerceService();
 
@@ -202,11 +222,10 @@ protected <T> T postData(String inputJson, String url, Class<T> clazz) throws FD
 			throw new FDResourceException(e, "Unable to process the request.");
 		}
 	}
-
+	@Override
 	public Map<ZoneInfo, List<FDProductPromotionInfo>> getAllProductsByType(
 			String ppType) throws FDResourceException {
 	
-
 			Response<Map<ZoneInfo, List<FDProductPromotionInfo>>> response;
 			try {
 				response = httpGetDataTypeMap(getFdCommerceEndPoint(PRODUCT_BY_PROMO_TYPE+ppType), new TypeReference<Response<Map<ZoneInfo, List<FDProductPromotionInfo>>>>() {});
@@ -218,4 +237,154 @@ protected <T> T postData(String inputJson, String url, Class<T> clazz) throws FD
 				throw new FDResourceException(e, "Unable to process the request.");
 			}
 		}
+	@Override
+	public NavigableMap<Long, BINInfo> getActiveBINs()
+			throws FDResourceException {
+		Response<NavigableMap<Long, BINData>> response;
+	
+			response = httpGetDataTypeMap(getFdCommerceEndPoint(GET_ACTIVE_BINS), new TypeReference<Response<NavigableMap<Long, BINData>>>() {});
+			if(!response.getResponseCode().equals("OK"))
+				throw new FDResourceException(response.getMessage());
+				
+				return buildBinInfoModel(response.getData());
+	
+		}
+	@Override
+	public void saveBINInfo(List<List<BINInfo>> binInfos)
+			throws FDResourceException {
+		
+		Request<List<BINData>> request = new Request<List<BINData>>();
+		List<BINData> binDataList = new ArrayList<BINData>(binInfos.size());
+		for (List<BINInfo> bininfol : binInfos) {
+			for(BINInfo bininfo : bininfol){
+			binDataList.add(buildBinDataModel(bininfo));
+			}
+		}
+		request.setData(binDataList);
+		String inputJson;
+		Response<String> response = null;
+		try {
+			inputJson = buildRequest(request);
+			 response = postData(inputJson, getFdCommerceEndPoint(SAVE_ACTIVE_BINS), Response.class);
+			if(!response.getResponseCode().equals("OK"))
+				throw new FDResourceException(response.getMessage());
+		} catch (FDPayPalServiceException e) {
+			
+			throw new FDResourceException(response.getMessage());
+		}
+	}
+	//Move the below method to util class
+	private NavigableMap<Long, BINInfo> buildBinInfoModel(
+			NavigableMap<Long, BINData> data) {
+		NavigableMap<Long, BINInfo> binInfoData = new TreeMap();
+		for(Long key: data.keySet()){
+			BINData binData = data.get(key);
+			BINInfo bINInfo = new BINInfo(binData.getId(), binData.getLowRange(), binData.getHighRange(), binData.getSequence(), EnumCardType.getByPaymentechCode(binData.getPaymentCode()));
+			binInfoData.put(key, bINInfo);
+		}
+		return binInfoData;
+	}
+	//Move the below method to util class
+	private BINData buildBinDataModel(BINInfo binInfo) {
+
+			BINData binData = new BINData();
+			binData.setId(binInfo.getId());
+			binData.setLowRange(binInfo.getLowRange());
+			binData.setHighRange(binInfo.getLowRange());
+			binData.setSequence(binInfo.getSequence());
+			binData.setPaymentCode(binInfo.getCardType().getPaymentechCode());
+		return binData;
+	}
+	@Override
+	public com.freshdirect.content.attributes.FlatAttributeCollection getAttributes(String[] rootIds) {
+		
+		Response<FlatAttributeCollection> response = null;
+		FlatAttributeCollection result = null;
+		Request<String[]> request = new Request<String[]>();
+			try {
+				request.setData(rootIds);
+				String inputJson;
+				inputJson = buildRequest(request);
+				response = postDataTypeMap(inputJson,getFdCommerceEndPoint(PROD_MATERIAL_ATTRIBUTES),new TypeReference<Response<FlatAttributeCollection>>() {});
+				if(!response.getResponseCode().equals("OK"))
+					throw new FDResourceException(response.getMessage());
+					
+			} catch (FDResourceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (FDPayPalServiceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return buildFlatAttributeModel(response.getData());
+	}
+
+	@Override
+	public void storeAttributes(
+			com.freshdirect.content.attributes.FlatAttributeCollection attrs,
+			String user, String sapId) throws FDResourceException {
+		
+		String inputJson;
+		Response<String> response = null;
+		try {
+			inputJson = buildRequest(buildFlatAttributeModel(attrs));
+			 response = postData(inputJson, getFdCommerceEndPoint(SAVE_ATTRIBUTES+"?sapId="+sapId+"&user="+user), Response.class);
+			if(!response.getResponseCode().equals("OK"))
+				throw new FDResourceException(response.getMessage());
+		} catch (FDPayPalServiceException e) {
+			
+			throw new FDResourceException(response.getMessage());
+		}
+	}
+	private com.freshdirect.content.attributes.FlatAttributeCollection buildFlatAttributeModel(
+			FlatAttributeCollection data) {
+			List<FlatAttribute> lst = new ArrayList();
+			for(com.freshdirect.ecommerce.data.attributes.FlatAttribute flatAtt:data.getFlatAttributes()){
+				FlatAttribute flata = new FlatAttribute(flatAtt.getIdPath(), flatAtt.getName(), flatAtt.getValue());
+				lst.add(flata);
+			}
+			com.freshdirect.content.attributes.FlatAttributeCollection  flatCollection = new com.freshdirect.content.attributes.FlatAttributeCollection(lst);
+			
+		return flatCollection;
+	}
+	private FlatAttributeCollection buildFlatAttributeModel(
+			com.freshdirect.content.attributes.FlatAttributeCollection data) {
+			List<com.freshdirect.ecommerce.data.attributes.FlatAttribute> lst = new ArrayList();
+			for(FlatAttribute flatAtt:data.getFlatAttributes()){
+				com.freshdirect.ecommerce.data.attributes.FlatAttribute flata = new com.freshdirect.ecommerce.data.attributes.FlatAttribute(flatAtt.getIdPath(), flatAtt.getName(), flatAtt.getValue());
+				lst.add(flata);
+			}
+			FlatAttributeCollection  flatCollection = new FlatAttributeCollection(lst);
+			
+		return flatCollection;
+	}
+	@Override
+	public Map loadAttributes(Date since) throws AttributeException {
+		
+		Response<Map<String, List<com.freshdirect.ecommerce.data.attributes.FlatAttribute>>> response = null;
+			try {
+				SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+				String date1 = format1.format(since); 
+				response =httpGetDataTypeMap(getFdCommerceEndPoint(LOAD_ATTRIBUTES_DATE+date1),new TypeReference<Response<Map<String, List<com.freshdirect.ecommerce.data.attributes.FlatAttribute>>>>() {});
+				if(!response.getResponseCode().equals("OK"))
+					throw new FDResourceException(response.getMessage());
+			} catch (FDResourceException e) {
+				throw new AttributeException(e);
+			}
+			Map<String, List<FlatAttribute>> results = new HashMap();
+			Map<String, List<com.freshdirect.ecommerce.data.attributes.FlatAttribute>> responses = response.getData();
+			for(Map.Entry<String, List<com.freshdirect.ecommerce.data.attributes.FlatAttribute>> entry : responses.entrySet()){
+				List<FlatAttribute> valueList = new ArrayList<FlatAttribute>();
+				List<com.freshdirect.ecommerce.data.attributes.FlatAttribute> valueListinternal = entry.getValue();
+				for(com.freshdirect.ecommerce.data.attributes.FlatAttribute flatAtt:valueListinternal){
+					FlatAttribute flata = new FlatAttribute(flatAtt.getIdPath(), flatAtt.getName(), flatAtt.getValue());
+					flata.setLastModified(flatAtt.getLastModifiedDate());
+					valueList.add(flata);
+				}
+				results.put(entry.getKey(), valueList);
+			}
+			return results;
+	}
+	
 }
--- projects/ERPServices/src/com/freshdirect/payment/service/IECommerceService.java
+++ projects/ERPServices/src/com/freshdirect/payment/service/IECommerceService.java
@@ -1,14 +1,33 @@
 package com.freshdirect.payment.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.NavigableMap;
 
+import com.freshdirect.common.pricing.ZoneInfo;
+import com.freshdirect.content.attributes.AttributeException;
+import com.freshdirect.content.attributes.FlatAttributeCollection;
 import com.freshdirect.customer.ErpProductFamilyModel;
+import com.freshdirect.fdstore.FDProductPromotionInfo;
 import com.freshdirect.fdstore.FDResourceException;
+import com.freshdirect.payment.BINInfo;
 
 public interface IECommerceService {
 
 	public Map<String,List<String>> updateCacheWithProdFly(List<String> familyIds)throws FDResourceException;
 	
 	public void loadData(List<ErpProductFamilyModel> productFamilyList) throws FDResourceException;
+	
+	public NavigableMap<Long, BINInfo> getActiveBINs() throws FDResourceException;
+
+	public void saveBINInfo(List<List<BINInfo>> binInfos) throws FDResourceException;
+
+	public Map<ZoneInfo, List<FDProductPromotionInfo>> getAllProductsByType(String ppType) throws FDResourceException;
+
+	public FlatAttributeCollection getAttributes(String[] rootIds);
+
+	public void storeAttributes(FlatAttributeCollection attrs, String user,	String sapId) throws FDResourceException;
+
+	public Map loadAttributes(Date since) throws AttributeException;
 }
