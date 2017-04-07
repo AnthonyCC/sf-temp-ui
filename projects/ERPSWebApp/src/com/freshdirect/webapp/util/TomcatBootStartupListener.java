40155a55298b91ef59a3023c124de34c0643015b
ssubramanian <ssubramanian@842219ff-9341-0410-a943-573460bdd4de>
2017-04-07 19:47:11 +0000

    Changes for including warmup during server startup
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/features/branches/sf2_0_fe@66460 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/util/TomcatBootStartupListener.java
@@ -0,0 +1,30 @@
+package com.freshdirect.webapp.util;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.log4j.Logger;
+
+import com.freshdirect.fdstore.warmup.Warmup;
+import com.freshdirect.framework.util.log.LoggerFactory;
+
+public class TomcatBootStartupListener implements ServletContextListener {
+	
+	private final static Logger LOGGER = LoggerFactory.getInstance(TomcatBootStartupListener.class);
+	
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+    	LOGGER.info("FDTomcat Server Startup Begin.........");
+    	Warmup wup = new Warmup();
+		wup.warmup();
+
+		LOGGER.info("FDTomcat Server Startup Begin.........");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+    	LOGGER.info("FDTomcat Server Shutdown Complete.........");
+    }
+
+
+}
