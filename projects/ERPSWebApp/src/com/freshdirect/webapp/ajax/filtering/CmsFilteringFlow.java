b983d7207ca6d0acd5077a96865252cbeab58c0e
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2017-04-26 23:15:03 +0000

    Fix for NPE on recipes tab in search page.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@67145 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/filtering/CmsFilteringFlow.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/filtering/CmsFilteringFlow.java
@@ -244,13 +244,15 @@ public class CmsFilteringFlow {
              * Here we are iterating through this sorted section data and adding them to the assort products map based upon the product id
              * Thus the assort product map and section data sorted products are always in sync 
              * */
-            if(!browseDataContext.getSectionContexts().isEmpty()){
+            if(!browseDataContext.getSectionContexts().isEmpty() && null !=browseDataContext.getSectionContexts().get(0).getProductItems()){
             	
             	for(FilteringProductItem filteredProduct: browseDataContext.getSectionContexts().get(0).getProductItems()){
-            		for(ProductData productData:browseDataContext.getAssortProducts().getUnfilteredAssortedProducts()){
-            			if(productData.getProductId().equalsIgnoreCase(filteredProduct.getProductModel().getContentKey().getId())){
-                			browseDataContext.getAssortProducts().addProdDataToCat(productData.getErpCategory(), productData);		
-                		}
+            		if(null !=browseDataContext.getAssortProducts() & null !=browseDataContext.getAssortProducts().getUnfilteredAssortedProducts()){
+	            		for(ProductData productData:browseDataContext.getAssortProducts().getUnfilteredAssortedProducts()){
+	            			if(productData.getProductId().equalsIgnoreCase(filteredProduct.getProductModel().getContentKey().getId())){
+	                			browseDataContext.getAssortProducts().addProdDataToCat(productData.getErpCategory(), productData);		
+	                		}
+	            		}
             		}
             		
             	}
