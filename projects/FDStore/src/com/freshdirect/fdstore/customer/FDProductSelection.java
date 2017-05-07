945def2c3e82e5c0d9ce0fb271e083b4575ac96f
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2017-05-07 07:50:04 +0000

    Fix for NPE. Removing the logging not required.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@67515 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDStore/src/com/freshdirect/fdstore/customer/FDProductSelection.java
+++ projects/FDStore/src/com/freshdirect/fdstore/customer/FDProductSelection.java
@@ -832,7 +832,7 @@ public class FDProductSelection implements FDProductSelectionI {
 				pickingPlantId = prodInfo.getPickingPlantId(getUserContext().getPricingContext().getZoneInfo().getSalesOrg(),getUserContext().getPricingContext().getZoneInfo().getDistributionChanel());
 			}			
 			if(null == pickingPlantId && null !=getUserContext().getFulfillmentContext()){
-				LOGGER.info("PickingPlantId is not found for this product: "+prodInfo.getSkuCode());
+//				LOGGER.info("PickingPlantId is not found for this product: "+prodInfo.getSkuCode());
 				pickingPlantId = getUserContext().getFulfillmentContext().getPlantId();				
 			}
 		}
