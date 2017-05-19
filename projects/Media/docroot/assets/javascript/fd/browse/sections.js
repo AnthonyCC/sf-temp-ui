ea1385ee15e919603dedc28ccd32e9af6a006adf
batchley <batchley@842219ff-9341-0410-a943-573460bdd4de>
2017-05-19 20:51:22 +0000

    [APPDEV-6196] Mixed content error seen when hooklogic does not return ads
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@67926 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/fd/browse/sections.js
+++ projects/Media/docroot/assets/javascript/fd/browse/sections.js
@@ -140,17 +140,19 @@ var FreshDirect = FreshDirect || {};
 				
 				//now, page beacon
 				hlSkusStr = hlSkus.join(',');
-				if (hlSkusStr !== '' && $('.browseContent .HLpageBeaconImg.page_'+activePage+'_id_'+cur).length === 0) {
-					if (FreshDirect.browse.data.adProducts.hlSelectionOfProductList[cur].length === FreshDirect.browse.data.adProducts.hlCatProductsCount[cur]) {
-						//all hlprods
-						$(".browseContent").append('<img style="display: none;" class="HLpageBeaconImg page_'+activePage+'_id_'+cur+'" src="' + window.FreshDirect.browse.data.adProducts.hlSelectionsPageBeacons[cur] + 'all&random=' + new Date().getTime() + '" />');
-					} else {
-						//not ALL hlprods
-						$(".browseContent").append('<img style="display: none;" class="HLpageBeaconImg page_'+activePage+'_id_'+cur+'" src="' + window.FreshDirect.browse.data.adProducts.hlSelectionsPageBeacons[cur] + hlSkusStr + '&random=' + new Date().getTime() + '" />');
+				if (window.FreshDirect.browse.data.adProducts.hlSelectionsPageBeacons.hasOwnProperty(cur)) { /* this is required for the src url */
+					if (hlSkusStr !== '' && $('.browseContent .HLpageBeaconImg.page_'+activePage+'_id_'+cur).length === 0) {
+						if (FreshDirect.browse.data.adProducts.hlSelectionOfProductList[cur].length === FreshDirect.browse.data.adProducts.hlCatProductsCount[cur]) {
+							//all hlprods
+							$(".browseContent").append('<img style="display: none;" class="HLpageBeaconImg page_'+activePage+'_id_'+cur+'" src="' + window.FreshDirect.browse.data.adProducts.hlSelectionsPageBeacons[cur] + 'all&random=' + new Date().getTime() + '" />');
+						} else {
+							//not ALL hlprods
+							$(".browseContent").append('<img style="display: none;" class="HLpageBeaconImg page_'+activePage+'_id_'+cur+'" src="' + window.FreshDirect.browse.data.adProducts.hlSelectionsPageBeacons[cur] + hlSkusStr + '&random=' + new Date().getTime() + '" />');
+						}
+					} else if (hlSkusStr === '' && $('.browseContent .HLpageBeaconImg.page_'+activePage+'_id_'+cur).length === 0) {
+						//no hlprods
+						$(".browseContent").append('<img style="display: none;" class="HLpageBeaconImg page_'+activePage+'_id_'+cur+'" src="' + window.FreshDirect.browse.data.adProducts.hlSelectionsPageBeacons[cur] + 'none&random=' + new Date().getTime() + '" />');
 					}
-				} else if (hlSkusStr === '' && $('.browseContent .HLpageBeaconImg.page_'+activePage+'_id_'+cur).length === 0) {
-					//no hlprods
-					$(".browseContent").append('<img style="display: none;" class="HLpageBeaconImg page_'+activePage+'_id_'+cur+'" src="' + window.FreshDirect.browse.data.adProducts.hlSelectionsPageBeacons[cur] + 'none&random=' + new Date().getTime() + '" />');
 				}
 			}
 		}
