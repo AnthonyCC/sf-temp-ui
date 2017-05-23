9e64ca080eeaea7c94b54a052058ec2fc740a1e6
ssubramanian <ssubramanian@842219ff-9341-0410-a943-573460bdd4de>
2017-05-23 14:18:39 +0000

    fixed compilation error
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/features/branches/sf2_0_fe@67984 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/filters/UpdateFDSessionUserFilter.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/filters/UpdateFDSessionUserFilter.java
@@ -37,6 +37,7 @@ public class UpdateFDSessionUserFilter extends AbstractFilter {
 				((HttpServletResponse)response).addCookie(cookie);
 			}
 		}
+		}
 		filterChain.doFilter(request, response);
 	}
 
