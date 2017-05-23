53da6de1917173d22ff4b37783fb7afb6e9936b6
pmaradirangaiah <pmaradirangaiah@842219ff-9341-0410-a943-573460bdd4de>
2017-05-23 14:32:18 +0000

    http://jira.freshdirect.com/browse/SF17-70
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@67989 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/ERPServices/src/com/freshdirect/payment/service/ErpProductInfoModelConvert.java
@@ -0,0 +1,120 @@
+package com.freshdirect.payment.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+
+import com.freshdirect.ecommerce.data.erp.model.EnumATPRuleData;
+import com.freshdirect.ecommerce.data.erp.model.ErpMaterialPriceData;
+import com.freshdirect.ecommerce.data.erp.model.ErpMaterialSalesAreaInfoData;
+import com.freshdirect.ecommerce.data.erp.model.ErpPlantMaterialInfoData;
+import com.freshdirect.ecommerce.data.erp.model.ErpProductInfoModelData;
+import com.freshdirect.ecommerce.data.fdstore.SalesAreaInfoData;
+import com.freshdirect.ecommerce.data.utill.DayOfWeekSetData;
+import com.freshdirect.erp.EnumATPRule;
+import com.freshdirect.erp.model.ErpProductInfoModel;
+import com.freshdirect.erp.model.ErpProductInfoModel.ErpMaterialPrice;
+import com.freshdirect.erp.model.ErpProductInfoModel.ErpMaterialSalesAreaInfo;
+import com.freshdirect.erp.model.ErpProductInfoModel.ErpPlantMaterialInfo;
+import com.freshdirect.fdstore.SalesAreaInfo;
+import com.freshdirect.framework.util.DayOfWeekSet;
+
+public class ErpProductInfoModelConvert {
+
+	public static ErpProductInfoModel convertModelToData(
+			ErpProductInfoModelData data) {
+		ErpProductInfoModel model = new ErpProductInfoModel(		
+				data.getSkuCode(),
+				data.getVersion(),
+				data.getMaterialNumbers(),
+				data.getDescription(),
+				getErpMaterialPriceModel(data.getMaterialPrices()),
+				data.getUpc(),
+				getErpMaterialPriceModel(data.getMaterialPlants()),
+				getErpMaterialSalesAreaInfoModel(data.getMaterialSalesAreas()),
+				data.isAlcohol());
+		return model;
+	}
+
+	private static ErpMaterialSalesAreaInfo[] getErpMaterialSalesAreaInfoModel(
+			ErpMaterialSalesAreaInfoData[] materialSalesAreas) {
+		if(null == materialSalesAreas){
+			return null;
+		}
+		ErpMaterialSalesAreaInfo[] data = new  ErpMaterialSalesAreaInfo[materialSalesAreas.length];
+		for (int i = 0; i < materialSalesAreas.length; i++) {
+			ErpMaterialSalesAreaInfoData erpMaterialSalesAreaInfoData = materialSalesAreas[i];
+			data[i] = new ErpMaterialSalesAreaInfo(createSalesAreaInfo(erpMaterialSalesAreaInfoData.getSalesAreaInfo()), erpMaterialSalesAreaInfoData.getUnavailabilityStatus(),erpMaterialSalesAreaInfoData.getUnavailabilityDate(), erpMaterialSalesAreaInfoData.getUnavailabilityReason(), erpMaterialSalesAreaInfoData.getDayPartType(), erpMaterialSalesAreaInfoData.getPickingPlantId());
+		}
+		return data;
+	}
+
+	private static SalesAreaInfo createSalesAreaInfo(
+			SalesAreaInfoData salesAreaInfoData) {
+		SalesAreaInfo salesAreaInfo = new SalesAreaInfo(salesAreaInfoData.getSalesOrg(), salesAreaInfoData.getDistChannel());
+		return salesAreaInfo;
+	}
+
+	private static ErpPlantMaterialInfo[] getErpMaterialPriceModel(
+			ErpPlantMaterialInfoData[] materialPlants) {
+		if(null == materialPlants){
+			return null;
+		}
+		ErpPlantMaterialInfo[] data = new  ErpPlantMaterialInfo[materialPlants.length];
+		for (int i = 0; i < materialPlants.length; i++) {
+			ErpPlantMaterialInfoData erpPlantMaterialData = materialPlants[i];
+			ErpPlantMaterialInfo erpPlantMaterialInfo = new ErpPlantMaterialInfo(erpPlantMaterialData.isKosherProduction(), erpPlantMaterialData.isPlatter(), getDayOfWeekSet(erpPlantMaterialData.getBlockedDays()), getAtpRule(erpPlantMaterialData.getAtpRule()), erpPlantMaterialData.getRating(), erpPlantMaterialData.getFreshness(), erpPlantMaterialData.getSustainabilityRating(), erpPlantMaterialData.getPlantId(), erpPlantMaterialData.isLimitedQuantity());
+//			erpPlantMaterialData.getLeadTime();
+			data[i]= erpPlantMaterialInfo;
+		}
+		return data;
+	}
+
+	private static EnumATPRule getAtpRule(EnumATPRuleData atpRule) {
+		return EnumATPRule.getEnum(atpRule.getName());
+	}
+
+	private static DayOfWeekSet getDayOfWeekSet(DayOfWeekSetData blockedDays) {
+		DayOfWeekSet dayOfWeekSet = null;
+		
+		Iterator iterator = blockedDays.getDaysOfWeek().iterator();
+		while(iterator.hasNext()){
+			String dayNumber = (String) iterator.next();
+			dayOfWeekSet = DayOfWeekSet.decode(dayNumber);
+		}
+		return dayOfWeekSet;
+	}
+	
+	private static ErpMaterialPrice[] getErpMaterialPriceModel(
+			ErpMaterialPriceData[] materialPrices) {
+		if(null == materialPrices){
+			return null;
+		}
+		ErpMaterialPrice[] material = new ErpMaterialPrice[materialPrices.length]; 
+		for (int i = 0; i < materialPrices.length; i++) {
+			ErpMaterialPriceData erpMaterialPrice = materialPrices[i];
+			material[i]= new ErpMaterialPrice(erpMaterialPrice.getPrice(), erpMaterialPrice.getUnit(), erpMaterialPrice.getPromoPrice(), erpMaterialPrice.getScaleUnit(), erpMaterialPrice.getScaleQuantity(), erpMaterialPrice.getSapZoneId(), erpMaterialPrice.getSalesOrg(), erpMaterialPrice.getDistChannel());
+		}
+		return material;
+	}
+
+
+	public static Collection<ErpProductInfoModel> convertListModelToListData(
+			Collection<ErpProductInfoModelData> datas) {
+		Collection<ErpProductInfoModel> erpProductInfoModels = new ArrayList<ErpProductInfoModel>();
+		for (ErpProductInfoModelData erpProductInfoModelData : datas) {
+			erpProductInfoModels.add(convertModelToData(erpProductInfoModelData));
+		}
+		return erpProductInfoModels;
+	}
+
+	
+
+	
+	
+	
+	
+	
+	
+}
