40eb6258d22b86d41a7302a45f50ff48fc533661
pmaradirangaiah <pmaradirangaiah@842219ff-9341-0410-a943-573460bdd4de>
2017-06-07 16:54:42 +0000

    SF17-57
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@68401 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/payment/service/ErpProductInfoModelConvert.java
+++ projects/ERPServices/src/com/freshdirect/payment/service/ErpProductInfoModelConvert.java
@@ -2,10 +2,8 @@ package com.freshdirect.payment.service;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 import java.util.Iterator;
 
-import com.freshdirect.ecommerce.data.erp.model.EnumATPRuleData;
 import com.freshdirect.ecommerce.data.erp.model.ErpMaterialPriceData;
 import com.freshdirect.ecommerce.data.erp.model.ErpMaterialSalesAreaInfoData;
 import com.freshdirect.ecommerce.data.erp.model.ErpPlantMaterialInfoData;
@@ -71,9 +69,9 @@ public class ErpProductInfoModelConvert {
 		return data;
 	}
 
-	private static EnumATPRule getAtpRule(EnumATPRuleData atpRule) {
+	private static EnumATPRule getAtpRule(String atpRule) {
 		if(atpRule!=null)
-		return EnumATPRule.getEnum(atpRule.getName());
+		return EnumATPRule.getEnum(atpRule);
 		return null;
 	}
 
@@ -97,7 +95,7 @@ public class ErpProductInfoModelConvert {
 		ErpMaterialPrice[] material = new ErpMaterialPrice[materialPrices.length]; 
 		for (int i = 0; i < materialPrices.length; i++) {
 			ErpMaterialPriceData erpMaterialPrice = materialPrices[i];
-			material[i]= new ErpMaterialPrice(erpMaterialPrice.getPrice(), erpMaterialPrice.getUnit(), erpMaterialPrice.getPromoPrice(), erpMaterialPrice.getScaleUnit(), erpMaterialPrice.getScaleQuantity(), erpMaterialPrice.getSapZoneId(), erpMaterialPrice.getSalesOrg(), erpMaterialPrice.getDistChannel());
+			material[i]= new ErpMaterialPrice(erpMaterialPrice.getPrice(), erpMaterialPrice.getPricingUnit(), erpMaterialPrice.getPromoPrice(), erpMaterialPrice.getScaleUnit(), erpMaterialPrice.getScaleQuantity(), erpMaterialPrice.getSapZoneId(), erpMaterialPrice.getSalesOrg(), erpMaterialPrice.getDistChannel());
 		}
 		return material;
 	}
