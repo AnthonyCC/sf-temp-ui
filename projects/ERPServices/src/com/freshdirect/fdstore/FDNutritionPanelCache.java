5dde139b2160fb655531bbc73d9449e6365f6b4d
rgupta <rgupta@842219ff-9341-0410-a943-573460bdd4de>
2017-06-12 14:50:23 +0000

    AppBug-5031 : temporary unavailable product is giving null pointer error
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@68580 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/fdstore/FDNutritionPanelCache.java
+++ projects/ERPServices/src/com/freshdirect/fdstore/FDNutritionPanelCache.java
@@ -74,7 +74,10 @@ public class FDNutritionPanelCache {
 	}
 
 	public NutritionPanel getNutritionPanel(String skuCode) {		
-		return cache.get( skuCode );
+		if(cache!=null)
+			return cache.get( skuCode );
+		else
+			return null;
 	}
 	
 	private ErpNutritionHome lookupNutritionHome() {
