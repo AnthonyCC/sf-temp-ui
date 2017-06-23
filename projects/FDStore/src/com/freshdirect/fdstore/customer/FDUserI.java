41811d9f27cfc516c4b22666853bee7955b80f55
amidha <amidha@842219ff-9341-0410-a943-573460bdd4de>
2017-06-23 05:37:45 +0000

    SF17-75 : created a setter for isReferrerEligible in FDUserI.java for jackson parsing in Storefront 2.0
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@69031 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDStore/src/com/freshdirect/fdstore/customer/FDUserI.java
+++ projects/FDStore/src/com/freshdirect/fdstore/customer/FDUserI.java
@@ -730,6 +730,9 @@ public interface FDUserI extends java.io.Serializable {
 
 	public void setRefreshNewSoFeature(boolean isRefreshNewSoFeature);
 	
+	// Only created for jackson parsing in Storefront 2.0
+	public void setReferrerEligible(Boolean referrerEligible) ;
+	
 	
 
 }
