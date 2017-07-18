9611af282467ddbd09715b78d8a54b3ab94e0fb9
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2017-07-18 20:05:04 +0000

    Reverting revision(s) 69701, 69668 from FreshDirect/trunk: [OPT-49] Extend expiration time of cached entry, if it already exists INSTEAD OF always adding new entry during PUT operation.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@69844 842219ff-9341-0410-a943-573460bdd4de

--- projects/Framework/src/com/freshdirect/framework/util/TimedLruCache.java
+++ projects/Framework/src/com/freshdirect/framework/util/TimedLruCache.java
@@ -31,12 +31,7 @@ public class TimedLruCache<K,V> extends LruCache<K,V> {
 	}
 
 	public synchronized void put(K key, V value) {
-		TimedEntry<K,V> entry = (TimedEntry<K,V>)this.getEntry(key);
-		if (entry!=null) {
-			entry.renewLease(this.expire);
-			
-		} else 
-			super.putEntry( new TimedEntry<K,V>(key, value, this.expire) );
+		super.putEntry( new TimedEntry<K,V>(key, value, this.expire) );
 	}
 
 	public synchronized V get(K key) {
@@ -47,9 +42,6 @@ public class TimedLruCache<K,V> extends LruCache<K,V> {
 		TimedEntry<K,V> timedEntry = (TimedEntry<K,V>)entry;
 		if ( timedEntry.isExpired() ) {
 			return this.getExpired(timedEntry);
-		}
-		else {
-			timedEntry.renewLease(this.expire);
 		}
 		return entry.value;
 	}
