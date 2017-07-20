1b328d220942a05e68c2cb9f3ec8039ddbb77947
batchley <batchley@842219ff-9341-0410-a943-573460bdd4de>
2017-07-20 20:23:48 +0000

    [APPDEV-6379] [Cannot Checkout Safari Browser] Escalation: Website issues LinkID: 227854
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@69930 842219ff-9341-0410-a943-573460bdd4de

--- projects/Media/docroot/assets/javascript/fd/components/dfp.js
+++ projects/Media/docroot/assets/javascript/fd/components/dfp.js
@@ -3,7 +3,7 @@ var googletag = window.googletag || {};
 
 // load DFP
 (function(fd) {
-  "use strict"
+  "use strict";
   if (fd.properties.isDFPEnabled) {
     var gads = document.createElement('script'),
         useSSL = 'https:' == document.location.protocol,
@@ -23,15 +23,16 @@ var googletag = window.googletag || {};
 (function (fd) {
   "use strict";
 
+  /* using strict means this function declaration needs to not be nested */
+  function getDfpSlotNameFromOasName(slot) {
+    return slot.id.substring(4,slot.id.length);
+  }
+  
   if (fd.properties.isDFPEnabled) {
     var $=fd.libs.$,
         slots=document.querySelectorAll("[id^=oas_]"),
         attributes=fd.utils.getParameters(DFP_query),
         dfpId= fd.properties.dfpId || '1072054678';
-
-    function getDfpSlotNameFromOasName(slot) {
-      return slot.id.substring(4,slot.id.length);
-    }
 
     googletag.cmd = googletag.cmd || [];
     googletag.cmd.push(function() {
@@ -48,7 +49,7 @@ var googletag = window.googletag || {};
           size = [+slot.getAttribute('ad-size-width'), +slot.getAttribute('ad-size-height')];
         }
         googletag.defineSlot('/'+ dfpId +'/'+ getDfpSlotNameFromOasName(slot), size, slot.id).addService(googletag.pubads())
-        .setCollapseEmptyDiv(true)
+        .setCollapseEmptyDiv(true);
         googletag.display(slot.id);
       });
     });
