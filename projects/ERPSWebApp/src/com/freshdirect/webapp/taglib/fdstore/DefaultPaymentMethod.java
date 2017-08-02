c267b4c215a0f2bbbcf5757cde489047d0f1eeb8
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2017-08-02 15:38:27 +0000

    Merged revision(s) 70062-70264 from pdev/FreshDirect/features/branches/2017_debitCardProject:
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@70272 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/DefaultPaymentMethod.java
@@ -0,0 +1,39 @@
+package com.freshdirect.webapp.taglib.fdstore;
+
+import java.util.Collection;
+
+import org.apache.log4j.Category;
+
+import com.freshdirect.customer.EnumPaymentMethodDefaultType;
+import com.freshdirect.customer.ErpPaymentMethodI;
+import com.freshdirect.fdstore.FDResourceException;
+import com.freshdirect.fdstore.customer.FDActionInfo;
+import com.freshdirect.fdstore.customer.FDUser;
+import com.freshdirect.framework.util.log.LoggerFactory;
+
+public class DefaultPaymentMethod implements Runnable {
+	
+	private static Category LOGGER = LoggerFactory.getInstance( DefaultPaymentMethod.class );
+	
+	private FDActionInfo info;
+	private EnumPaymentMethodDefaultType defaultPaymentType;
+	Collection<ErpPaymentMethodI> pMethods;
+
+	
+	public DefaultPaymentMethod(FDActionInfo info, EnumPaymentMethodDefaultType defaultSys, Collection<ErpPaymentMethodI> pMethods) {
+		this.info = info;
+		this.defaultPaymentType = defaultSys;
+		this.pMethods = pMethods;
+	}
+
+	@Override
+	public void run() {		
+		   
+			ErpPaymentMethodI defaultPaymentMethod = com.freshdirect.fdstore.payments.util.PaymentMethodUtil.getSystemDefaultPaymentMethod(info, pMethods);
+			if(null != defaultPaymentMethod){
+			com.freshdirect.fdstore.payments.util.PaymentMethodUtil.updateDefaultPaymentMethod(info, pMethods,
+					defaultPaymentMethod.getPK().getId(), defaultPaymentType);			
+		}
+	}
+
+}
