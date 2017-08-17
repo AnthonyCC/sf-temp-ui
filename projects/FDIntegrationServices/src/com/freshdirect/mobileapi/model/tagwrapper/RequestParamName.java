41afa2d189cd8ae1addfd3aec857e92ebd5a4194
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2017-08-17 19:04:12 +0000

    DCS-13 - Debitcard project.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@70798 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDIntegrationServices/src/com/freshdirect/mobileapi/model/tagwrapper/RequestParamName.java
+++ projects/FDIntegrationServices/src/com/freshdirect/mobileapi/model/tagwrapper/RequestParamName.java
@@ -353,4 +353,6 @@ public interface RequestParamName {
     public static final String REQ_PARAM_CARTLINE = "cartLine";
     
     public static final String REQ_PARAM_GO_GREEN = "go_green";
+    
+    public static final String REQ_PARAM_DATA = "data";
 }
