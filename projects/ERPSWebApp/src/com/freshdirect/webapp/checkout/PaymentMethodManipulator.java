10f12968dad235e821f06506c59acdb6b7d4bdf9
psingh <psingh@842219ff-9341-0410-a943-573460bdd4de>
2017-09-07 20:43:30 +0000

    APPBUG-5231: For echeck alert account: Manual selection of Echeck should not show error message
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@71330 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/checkout/PaymentMethodManipulator.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/checkout/PaymentMethodManipulator.java
@@ -260,11 +260,11 @@ public class PaymentMethodManipulator extends CheckoutManipulator {
 				if(paymentMethod.getPaymentMethodType().equals(EnumPaymentMethodType.ECHECK) || paymentMethod.getPaymentMethodType().equals(EnumPaymentMethodType.CREDITCARD) 
 						|| paymentMethod.getPaymentMethodType().equals(EnumPaymentMethodType.DEBITCARD)){
 				if(paymentMethod.getPaymentMethodType().equals(EnumPaymentMethodType.ECHECK)){
-					if(FDCustomerManager.isECheckRestricted(info.getIdentity()) || FDCustomerManager.isOnAlert(info.getIdentity().getErpCustomerPK(), EnumAlertType.ECHECK.getName())){
+			/*		if(FDCustomerManager.isECheckRestricted(info.getIdentity()) || FDCustomerManager.isOnAlert(info.getIdentity().getErpCustomerPK(), EnumAlertType.ECHECK.getName())){
 						result.addError(new ActionError("verificationFailed",SystemMessageList.MSG_DEFAULT_PAYMENT_VERIVICATION_FAILURE));
-					}else{
+					}else{*/
 					auth = FDCustomerManager.verifyCard(info, paymentMethod, true);
-					}
+					//}
 				}else if(paymentMethod.getPaymentMethodType().equals(EnumPaymentMethodType.CREDITCARD) || paymentMethod.getPaymentMethodType().equals(EnumPaymentMethodType.DEBITCARD)){
 					try {
 						if(paymentMethod.isAvsCkeckFailed() && !paymentMethod.isBypassAVSCheck()){
@@ -276,15 +276,19 @@ public class PaymentMethodManipulator extends CheckoutManipulator {
 						}
 					} catch (ErpTransactionException e) {
 						LOGGER.error(e);
+						throw new FDResourceException(e);
 					} catch (ErpAuthorizationException e) {
 						LOGGER.error(e);
+						throw new FDResourceException(e);
 					}
 				}
 				if(null == auth || !auth.isApproved()){
 					result.addError(new ActionError("verificationFailed",SystemMessageList.MSG_DEFAULT_PAYMENT_VERIVICATION_FAILURE));
 				}
 				}
+				if(result.isSuccess()){
 				FDCustomerManager.setDefaultPaymentMethod( info, pmPK, EnumPaymentMethodDefaultType.DEFAULT_CUST, true );
+				}
 			user.refreshFdCustomer();
 			}
 			
