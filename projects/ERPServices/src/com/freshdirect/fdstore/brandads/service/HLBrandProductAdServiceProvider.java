da47bde6de55bcc3f16891ca4e712310fbd93bbb
smerugu <smerugu@842219ff-9341-0410-a943-573460bdd4de>
2017-09-20 02:07:11 +0000

    Fixing the Hooklogic parsing exception.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@71747 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/fdstore/brandads/service/HLBrandProductAdServiceProvider.java
+++ projects/ERPServices/src/com/freshdirect/fdstore/brandads/service/HLBrandProductAdServiceProvider.java
@@ -252,9 +252,10 @@ public class HLBrandProductAdServiceProvider implements BrandProductAdService {
 			throws IOException, UnsupportedEncodingException {
 		String returnString="";
 		InputStream response = null;
+		BufferedReader reader=null;
 		try {
 			response = connection.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(response,charSet));
+			 reader = new BufferedReader(new InputStreamReader(response,charSet));
 			StringBuilder responseString = new StringBuilder();
 			for ( String oneLine; (oneLine = reader.readLine()) != null; ) {
 				responseString.append(oneLine);
@@ -264,6 +265,10 @@ public class HLBrandProductAdServiceProvider implements BrandProductAdService {
 			// handle an exception
 		} finally { //  finally blocks are guaranteed to be executed
 			try {
+				if (reader != null) {
+					reader.close();
+				}
+
 				if (response != null) {
 					response.close();
 				}
