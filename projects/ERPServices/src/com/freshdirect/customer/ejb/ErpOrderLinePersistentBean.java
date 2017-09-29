9a4d1519959f64d83ee81e7ea3aa31ad7d31ecc5
nsubramanian <nsubramanian@842219ff-9341-0410-a943-573460bdd4de>
2017-09-29 12:58:09 +0000

    DP17-35- Backend Implementation DeliveryPass Free TRAIL
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@71949 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/customer/ejb/ErpOrderLinePersistentBean.java
+++ projects/ERPServices/src/com/freshdirect/customer/ejb/ErpOrderLinePersistentBean.java
@@ -41,10 +41,10 @@ import com.freshdirect.framework.util.StringUtil;
  * @stereotype fd-persistent
  */
 public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
-	
+
 	private ErpOrderLineModel model;
 	private ErpCouponDiscountLineModel couponDiscount;
-	
+
 	public ErpOrderLinePersistentBean() {
 		super();
 		this.model = new ErpOrderLineModel();
@@ -76,19 +76,23 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 	 * Copy into model.
 	 * @return ErpOrderLineModel object.
 	 */
+	@Override
 	public ModelI getModel() {
 		return this.model.deepCopy();
 	}
 
 	/** Copy from model. */
+	@Override
 	public void setFromModel(ModelI model) {
 		this.model = (ErpOrderLineModel)model;
 	}
-	
+
+	@Override
 	public PrimaryKey getPK() {
 		return this.model.getPK();
 	}
-	
+
+	@Override
 	public void setPK(PrimaryKey pk) {
 		this.model.setPK(pk);
 	}
@@ -107,14 +111,14 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 		ResultSet rs = ps.executeQuery();
 		while (rs.next()) {
 			ErpOrderLinePersistentBean bean = new ErpOrderLinePersistentBean( new PrimaryKey(rs.getString("ID")), rs,conn );
-			bean.setParentPK(parentPK);			
+			bean.setParentPK(parentPK);
 			lst.add(bean);
 		}
 		rs.close();
 		ps.close();
 		return lst;
 	}
-	
+
 	private final static String INSERT_QUERY =
 		"INSERT INTO CUST.ORDERLINE (ID, SALESACTION_ID, ORDERLINE_NUMBER, SKU_CODE, VERSION,"
 		+ " QUANTITY, SALES_UNIT, CONFIGURATION, PROMOTION_TYPE, PROMOTION_AMT, PROMOTION_CAMPAIGN,"
@@ -122,6 +126,7 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 		+ " TAX_RATE, DEPOSIT_VALUE, ALCOHOL, AFFILIATE, CARTLINE_ID, RECIPE_SOURCE_ID, REQUEST_NOTIFICATION,RATING,BASE_PRICE,BASE_PRICE_UNIT,DISCOUNT_AMT,VARIANT_ID,SAVINGS_ID,PRICING_ZONE_ID,GROUP_ID,GROUP_VERSION,GROUP_QTY,SUSTAINABILITY_RATING, SKU_LIMIT,UPC,TAXATION_TYPE, EXTERNAL_AGENCY, EXTERNAL_SOURCE, EXTERNAL_GROUP,SALES_ORG,DISTRIBUTION_CHANNEL,SCALE_QTY,SOURCE,TAX_CODE, MATERIAL_GROUP,PICKING_PLANT_ID)"
 		+ " values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
+	@Override
 	public PrimaryKey create(Connection conn) throws SQLException {
 		String id = this.getNextId(conn, "CUST");
 		PreparedStatement ps = conn.prepareStatement(INSERT_QUERY);
@@ -135,11 +140,11 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 		ps.setString(8, ErpOrderLineUtil.convertHashMapToString(this.model.getConfiguration().getOptions()));
 
 		Discount d = this.model.getDiscount();
-		if (d!=null) {		
+		if (d!=null) {
 			ps.setInt(9, d.getDiscountType().getId());
 			ps.setBigDecimal(10, new BigDecimal(String.valueOf(d.getAmount())));
 			ps.setString(11, d.getPromotionCode());
-			ps.setInt(36, d.getSkuLimit());	
+			ps.setInt(36, d.getSkuLimit());
 		} else {
 			ps.setNull(9, Types.INTEGER);
 			ps.setNull(10, Types.DOUBLE);
@@ -161,27 +166,28 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 		ps.setString(23, this.model.getRecipeSourceId());
 		ps.setString(24, this.model.isRequestNotification() ? "X" : "");
 		if(this.model.getProduceRating()!=null)
-		   ps.setString(25, this.model.getProduceRating().getStatusCode());	
+		   ps.setString(25, this.model.getProduceRating().getStatusCode());
 		else
 			ps.setNull(25, Types.NULL);
-		if(this.model.getBasePrice()!=0) {
+		ps.setBigDecimal(26, new BigDecimal(String.valueOf(this.model.getBasePrice())));
+		/*if(this.model.getBasePrice()!=0) {
 			ps.setBigDecimal(26, new BigDecimal(String.valueOf(this.model.getBasePrice())));
 		} else {
 			ps.setNull(26, Types.NULL);
-		}
+		}*/
 		if(this.model.getBasePriceUnit()!=null) {
 			ps.setString(27,this.model.getBasePriceUnit());
 		} else {
 			ps.setNull(27, Types.VARCHAR);
 		}
-		
+
 		ps.setBigDecimal(28, new BigDecimal(String.valueOf(this.model.getDiscountAmount())));
-		
+
 		if(this.model.getVariantId()!=null) {
 			ps.setString(29,this.model.getVariantId());
 		} else {
 			ps.setNull(29, Types.VARCHAR);
-		}				
+		}
 		if(this.model.getSavingsId()!=null) {
 			ps.setString(30,this.model.getSavingsId());
 		} else {
@@ -212,25 +218,25 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 			ps.setNull(34, Types.DOUBLE);
 		}
 		if(this.model.getSustainabilityRating()!=null)
-			   ps.setString(35, this.model.getSustainabilityRating().getStatusCode());	
+			   ps.setString(35, this.model.getSustainabilityRating().getStatusCode());
 			else
-				ps.setNull(35, Types.NULL);	
+				ps.setNull(35, Types.NULL);
 		ps.setString(37, this.model.getUpc());
 		ps.setString(38, null !=this.model.getTaxationType()?this.model.getTaxationType().getName():null);
-		
+
 		ps.setString(39, StringUtil.crop(model.getExternalAgency(), 30));
 		ps.setString(40, StringUtil.crop(model.getExternalSource(), 30));
 		ps.setString(41, StringUtil.crop(model.getExternalGroup(), 256));
-		
+
 		ps.setString(42, model.getSalesOrg());
 		ps.setString(43, model.getDistChannel());
-		
+
 		if(null!=this.model.getScaleQuantity()){
 			ps.setBigDecimal(44, new BigDecimal(String.valueOf(this.model.getScaleQuantity())));
 		}else{
 			ps.setNull(44, Types.DOUBLE);
 		}
-		
+
 		if(null!=this.model.getSource()){
 			ps.setString(45, model.getSource().toString());
 		}else{
@@ -244,7 +250,7 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 		}else{
 			ps.setNull(48, Types.VARCHAR);
 		}
-		
+
 		try {
 			if (ps.executeUpdate() != 1) {
 				throw new SQLException("Row not created");
@@ -255,17 +261,18 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 		} finally {
 			ps.close();
 		}
-		
+
 		// create children
 		CouponLineList cList = this.getCouponLinePBList();
 		cList.create(conn);
-		
+
 		this.unsetModified();
 		return this.getPK();
 	}
 
+	@Override
 	public void load(Connection conn) throws SQLException {
-				
+
 		PreparedStatement ps = conn.prepareStatement("SELECT ORDERLINE_NUMBER, SKU_CODE, VERSION, QUANTITY, SALES_UNIT, CONFIGURATION, PROMOTION_TYPE, PROMOTION_AMT, PROMOTION_CAMPAIGN, DESCRIPTION, CONFIGURATION_DESC, DEPARTMENT_DESC, MATERIAL_NUMBER, PRICE, PERISHABLE, TAX_RATE, DEPOSIT_VALUE, ALCOHOL, AFFILIATE, CARTLINE_ID, RECIPE_SOURCE_ID, REQUEST_NOTIFICATION, RATING,BASE_PRICE,BASE_PRICE_UNIT,DISCOUNT_AMT,VARIANT_ID,SAVINGS_ID,PRICING_ZONE_ID,GROUP_ID,GROUP_VERSION,GROUP_QTY,SUSTAINABILITY_RATING,SKU_LIMIT,UPC,TAXATION_TYPE, EXTERNAL_AGENCY, EXTERNAL_SOURCE, EXTERNAL_GROUP,SALES_ORG,DISTRIBUTION_CHANNEL,SCALE_QTY,SOURCE,TAX_CODE, MATERIAL_GROUP,PICKING_PLANT_ID FROM CUST.ORDERLINE WHERE ID=?");
 		ps.setString(1, this.getPK().getId());
 		ResultSet rs = ps.executeQuery();
@@ -278,7 +285,7 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 				this.model.setPrice(this.model.getPrice()-this.model.getCouponDiscount().getDiscountAmt());
 			}*/
 		} else {
-			throw new SQLException("No such ErpOrderLine PK: " + this.getPK()); 
+			throw new SQLException("No such ErpOrderLine PK: " + this.getPK());
 		}
 		rs.close();
 		ps.close();
@@ -312,7 +319,7 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
         this.model.setCartlineId(rs.getString("CARTLINE_ID"));
         this.model.setRecipeSourceId(rs.getString("RECIPE_SOURCE_ID"));
         this.model.setRequestNotification(NVL.apply(rs.getString("REQUEST_NOTIFICATION"), "").equals("X"));
-        this.model.setProduceRating(EnumOrderLineRating.getEnumByStatusCode(NVL.apply(rs.getString("RATING"), "X")));  
+        this.model.setProduceRating(EnumOrderLineRating.getEnumByStatusCode(NVL.apply(rs.getString("RATING"), "X")));
         this.model.setBasePrice(rs.getDouble("BASE_PRICE"));
         this.model.setBasePriceUnit(rs.getString("BASE_PRICE_UNIT"));
         this.model.setDiscountAmount(rs.getDouble("DISCOUNT_AMT"));
@@ -322,7 +329,7 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
         this.model.setPricingZoneId(pricingZoneId);
         this.model.setTaxCode(rs.getString("TAX_CODE"));
         this.model.setMaterialGroup(rs.getString("MATERIAL_GROUP"));
-        
+
         String grpId = rs.getString("GROUP_ID");
         int version = rs.getInt("GROUP_VERSION");
         if(grpId!=null) {
@@ -333,9 +340,9 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
         	this.model.setFDGroup(group);
         }
         this.model.setGroupQuantity(rs.getDouble("GROUP_QTY"));
-         
+
         this.model.setSustainabilityRating(EnumSustainabilityRating.getEnumByStatusCode(NVL.apply(rs.getString("SUSTAINABILITY_RATING"), "X")));
-        
+
 		// deal with no promotion (PROMOTION_TYPE is NULL)
 		int type = rs.getInt("PROMOTION_TYPE");
 		if(!rs.wasNull()){
@@ -343,9 +350,9 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 			if(promotionType == null){
 				throw new EJBException("Promotion code is wrong or does not exist");
 			}
-			
+
 			Discount discount = new Discount(rs.getString("PROMOTION_CAMPAIGN"), promotionType, rs.getDouble("PROMOTION_AMT"));
-			discount.setSkuLimit(rs.getInt("SKU_LIMIT"));		
+			discount.setSkuLimit(rs.getInt("SKU_LIMIT"));
 			this.model.setDiscount(discount);
 		}
 		this.model.setUpc(rs.getString("UPC"));
@@ -359,9 +366,9 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 		this.model.setExternalAgency(ExternalAgency.safeValueOf(rs.getString("EXTERNAL_AGENCY")));
         this.model.setExternalSource(rs.getString("EXTERNAL_SOURCE"));
         this.model.setExternalGroup(rs.getString("EXTERNAL_GROUP"));
-        
+
         String salesOrg=rs.getString("SALES_ORG");
-        salesOrg = "1000".equals(salesOrg)?"0001":salesOrg;		
+        salesOrg = "1000".equals(salesOrg)?"0001":salesOrg;
         this.model.setSalesOrg(salesOrg);
         String distrChannel=rs.getString("DISTRIBUTION_CHANNEL");
         distrChannel = "1000".equals(salesOrg)?"01":distrChannel;
@@ -379,10 +386,10 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
         }
        UserContext userCtx=new UserContext();
         userCtx.setPricingContext(new PricingContext( pricingZone));//::FDX:: UserContext is not setup fully here.
-        
+
         this.model.setUserContext(userCtx);
         this.model.setPlantID(rs.getString("PICKING_PLANT_ID"));
-        
+
 
 		this.unsetModified();
 	}
@@ -390,17 +397,18 @@ public class ErpOrderLinePersistentBean extends ErpReadOnlyPersistentBean {
 		CouponLineList lst = new CouponLineList();
 		lst.setParentPK(this.getPK());
 		if(null !=this.model.getCouponDiscount()){
-			
+
 				lst.add(new ErpCouponDiscountLinePersistentBean(this.model.getCouponDiscount()));
-			
+
 		}
 		return lst;
 	}
-	
-	
+
+
 	private static class CouponLineList extends DependentPersistentBeanList {
-	    public void load(Connection conn) throws SQLException {
-			this.set(ErpCouponDiscountLinePersistentBean.findByParent(conn, (PrimaryKey) CouponLineList.this.getParentPK()));
+	    @Override
+		public void load(Connection conn) throws SQLException {
+			this.set(ErpCouponDiscountLinePersistentBean.findByParent(conn, CouponLineList.this.getParentPK()));
 	    }
 	}
 }
