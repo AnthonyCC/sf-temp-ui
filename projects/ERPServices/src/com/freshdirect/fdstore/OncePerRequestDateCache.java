014249aa05aa9b6413b1782400b17554000cfe24
dheller <dheller@842219ff-9341-0410-a943-573460bdd4de>
2017-10-23 21:18:26 +0000

    appdev-6184 assuring that the date init is aware of whether or not we are in fdx of FK to initialize today and tomorrow.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@72596 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/fdstore/OncePerRequestDateCache.java
+++ projects/ERPServices/src/com/freshdirect/fdstore/OncePerRequestDateCache.java
@@ -11,18 +11,22 @@ public class OncePerRequestDateCache {
 	
 	private static ThreadLocal availabilityHorizon = new ThreadLocal();
 	private static ThreadLocal today = new ThreadLocal();
-	
+	private  EnumEStoreId storeId =null;
 	private OncePerRequestDateCache(){
 		
 	}
 	
-	public static void init(){
+	public static void init(  EnumEStoreId storeId){
+		  EnumEStoreId enumStoreId =storeId;
 		Calendar startCal = DateUtil.truncate(Calendar.getInstance());
 		today.set(startCal.getTime());
 		
 		Calendar endCal = (Calendar) startCal.clone();
-		
-		startCal.add(Calendar.DATE, 1);
+		//story appdev 6841 commented out this line temporarily!
+		if(!EnumEStoreId.FDX.equals(enumStoreId)) {
+			startCal.add(Calendar.DATE, 1);
+			
+		}
 		endCal.add(Calendar.DATE, ErpServicesProperties.getHorizonDays());
 		
 		availabilityHorizon.set(new DateRange(startCal.getTime(), endCal.getTime()));
