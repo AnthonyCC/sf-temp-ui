c701026d2b22730b92754a2e04e9d7ccdfda8d41
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2017-12-04 15:25:57 +0000

    Merged revision(s) 73220 (Anthony Cheung's) from pdev/FreshDirect/features/branches/2017_web_perf_opt_phase2: [OPT-98]-Truncate '.00' from pricing display of a product on  PLP, PDP, but not in view cart, checkout etc.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@73513 842219ff-9341-0410-a943-573460bdd4de

--- projects/Framework/src/com/freshdirect/framework/util/FormatterUtil.java
+++ projects/Framework/src/com/freshdirect/framework/util/FormatterUtil.java
@@ -1,11 +1,14 @@
 package com.freshdirect.framework.util;
 
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class FormatterUtil {
 	
 	private static final DecimalFormat DECIMALFORMAT_TWODECIMAL = new DecimalFormat("0.00");
 	private static final DecimalFormat DECIMALFORMAT = new DecimalFormat("#,##,###,####");
+	private static final NumberFormat CURRENCYFORMAT = NumberFormat.getCurrencyInstance(Locale.US);
 	
 	public static String formatToTwoDecimal(double number) {
 		return DECIMALFORMAT_TWODECIMAL.format(number);
@@ -28,5 +31,13 @@ public class FormatterUtil {
 		strNumber = strNumber.replaceAll(",", ".");
 		Double numberDouble = new Double(strNumber);
 		return numberDouble.doubleValue();
+	}
+	
+	public static String formatCurrency(double number) {
+		String value = CURRENCYFORMAT.format(number);
+		if (value.endsWith(".00")) {
+			value = value.substring(0, value.lastIndexOf(".00"));
+		}
+		return value;
 	}
 }
