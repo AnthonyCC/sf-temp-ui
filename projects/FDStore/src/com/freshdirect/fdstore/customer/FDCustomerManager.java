14d97d661a576a982e0ea687ae74fb07faf7f999
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2017-12-04 01:19:16 +0000

    APPDEV-6683
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@73502 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDStore/src/com/freshdirect/fdstore/customer/FDCustomerManager.java
+++ projects/FDStore/src/com/freshdirect/fdstore/customer/FDCustomerManager.java
@@ -294,7 +294,7 @@ public class FDCustomerManager {
 			FDCustomerManagerSB sb = managerHome.create();
 			FDUser user = sb.recognize(cookie,eStoreId);
 			
-			populateShoppingCart(user, true, false);
+			populateShoppingCart(user, true, true);
 			
 			return user;
 
@@ -371,7 +371,7 @@ public class FDCustomerManager {
 			if(user.isVoucherHolder() && EnumEStoreId.FDX.equals( user.getUserContext().getStoreContext().getEStoreId() )){
 				throw new FDAuthenticationException("voucherredemption");
 			}
-			populateShoppingCart(user, updateUserState, false);
+			populateShoppingCart(user, updateUserState, true);
 
 			return user;
 
