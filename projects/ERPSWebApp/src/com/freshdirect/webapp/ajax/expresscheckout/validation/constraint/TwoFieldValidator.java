712c1ab8453f404c0478af47e783dee25ceadbd7
dheller <dheller@842219ff-9341-0410-a943-573460bdd4de>
2018-01-24 20:03:04 +0000

    appdev 6789 and appdev 6822, this is the validator
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@74679 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/expresscheckout/validation/constraint/TwoFieldValidator.java
@@ -0,0 +1,21 @@
+package com.freshdirect.webapp.ajax.expresscheckout.validation.constraint;
+
+/**
+ * Generally used to validate, not a single field, but two fields together
+ * @author dheller
+ *
+ */
+public class TwoFieldValidator {
+	
+	
+	
+	public boolean isValid(String f1, String f2){
+	 if (f2==null || f2.isEmpty()) return true;
+	 if (f1==null || f1.isEmpty()) return true;
+	 return f1.equalsIgnoreCase(f2);
+		
+	}
+ public String getErrorMessage(){
+	 return "Account Numbers should match.";
+ }
+}
