080dc4dffac8c0a6fa5a816a29db978941556a15
dheller <dheller@842219ff-9341-0410-a943-573460bdd4de>
2018-01-24 20:04:04 +0000

    appdev 6789 and appdev 6822, change dthe error message for clarity
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@74680 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/expresscheckout/validation/constraint/AccountNumberConstraint.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/expresscheckout/validation/constraint/AccountNumberConstraint.java
@@ -6,7 +6,7 @@ public class AccountNumberConstraint extends RegexConstraint {
 		super(optional);
 	}
 
-	private static final String ERROR_MESSAGE = "Account number is invalid.";
+	private static final String ERROR_MESSAGE = "Account number should be 5 to 17 digits, no alpha chars.";
     private static final String REGEX="^\\d{5,17}$";
 	@Override
 	public String getErrorMessage() {
