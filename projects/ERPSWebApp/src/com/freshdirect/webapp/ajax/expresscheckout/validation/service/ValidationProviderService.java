2514d1ffa955283e59a5998361f45e8b00515fa9
dheller <dheller@842219ff-9341-0410-a943-573460bdd4de>
2018-01-24 20:15:51 +0000

    appdev 6789 and appdev 6822, this is where the new  validator is used
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@74681 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/expresscheckout/validation/service/ValidationProviderService.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/expresscheckout/validation/service/ValidationProviderService.java
@@ -3,7 +3,9 @@ package com.freshdirect.webapp.ajax.expresscheckout.validation.service;
 import java.util.List;
 import java.util.Map;
 
+import com.freshdirect.fdstore.ewallet.PaymentMethodName;
 import com.freshdirect.webapp.ajax.expresscheckout.validation.ConstraintValidator;
+import com.freshdirect.webapp.ajax.expresscheckout.validation.constraint.TwoFieldValidator;
 import com.freshdirect.webapp.ajax.expresscheckout.validation.data.ValidationError;
 
 public class ValidationProviderService {
@@ -35,7 +37,12 @@ public class ValidationProviderService {
 	}
 
 	public List<ValidationError> validateBankPayment(final Map<String, String> data) {
-		return validator.validateByDatas(data, constraintService.getBankPaymentConstraints());
+		List<ValidationError> errors = validator.validateByDatas(data, constraintService.getBankPaymentConstraints());
+		ValidationError verifyAccountNumberError = validateBankPaymentAccountNumberVerify(data);
+		if (null != verifyAccountNumberError) {
+			errors.add(verifyAccountNumberError);
+		}
+		return errors;
 	}
 
 	public List<ValidationError> validateEbtPayment(final Map<String, String> data) {
@@ -45,9 +52,29 @@ public class ValidationProviderService {
 	public List<ValidationError> validateSmsAlert(final Map<String, String> data) {
 		return validator.validateByDatas(data, constraintService.getSmsAlertConstraints(data));
 	}
-	
+
 	public List<ValidationError> validateUnattendedDelivery(final Map<String, String> data) {
 		return validator.validateByConstraints(data, constraintService.getUnattendedDeliveryAddressConstraints());
+	}
+
+	/*THIS IS AN ODD DUCK THAT iM ADDING IN; appdev 6822 and 6789,
+	 *  account number verify and only digits in account number.
+	 *  Im checking for the presence of both fields in the map before trying to verify it.
+	 */
+
+	public ValidationError validateBankPaymentAccountNumberVerify(final Map<String, String> data) {
+		TwoFieldValidator validator = new TwoFieldValidator();
+		ValidationError error = null;
+		if (data.containsKey(PaymentMethodName.ACCOUNT_NUMBER)
+				&& data.containsKey(PaymentMethodName.ACCOUNT_NUMBER_VERIFY)) {
+
+			if (!validator.isValid(data.get(PaymentMethodName.ACCOUNT_NUMBER),
+					data.get(PaymentMethodName.ACCOUNT_NUMBER_VERIFY))) {
+				error = new ValidationError(PaymentMethodName.ACCOUNT_NUMBER_VERIFY, validator.getErrorMessage());
+			}
+		}
+
+		return error;
 	}
 
 }
