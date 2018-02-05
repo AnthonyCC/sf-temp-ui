e23ca41a44f99fe24a37beb491dbbae368aa52e6
tsoltesz <tsoltesz@842219ff-9341-0410-a943-573460bdd4de>
2018-02-05 15:30:32 +0000

    [APPDEV-6877] fixing incorrect rollout script
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@75017 842219ff-9341-0410-a943-573460bdd4de

--- release/db/APPDEV-6877_cust_address-rollout.sql
+++ release/db/APPDEV-6877_cust_address-rollout.sql
@@ -1,8 +1,8 @@
 update cust.address
 	set UNATTENDED_FLAG = 0
-	where UNATTENDED_FLAG = null; 
+	where UNATTENDED_FLAG is null; 
 
 update cust.address
 	set UNATTENDED_INSTR = 'OK'
-	where UNATTENDED_INSTR = null;
+	where UNATTENDED_INSTR is null;
 
