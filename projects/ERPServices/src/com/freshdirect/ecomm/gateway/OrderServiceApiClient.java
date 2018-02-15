efa1097940b9e4db4fc7d6d85c10cb812b7da318
tbalumuri <tbalumuri@842219ff-9341-0410-a943-573460bdd4de>
2018-02-15 18:42:34 +0000

    http://jira.freshdirect.com/browse/SF17-85
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@75451 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/ecomm/gateway/OrderServiceApiClient.java
+++ projects/ERPServices/src/com/freshdirect/ecomm/gateway/OrderServiceApiClient.java
@@ -1,19 +1,24 @@
+/*
+ * @author tbalumuri
+ */
 package com.freshdirect.ecomm.gateway;
 
 import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Category;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.freshdirect.common.CustomMapper;
 import com.freshdirect.customer.DlvSaleInfo;
 import com.freshdirect.customer.ErpSaleInfo;
 import com.freshdirect.deliverypass.DlvPassUsageInfo;
 import com.freshdirect.deliverypass.DlvPassUsageLine;
 import com.freshdirect.ecommerce.data.common.Response;
+import com.freshdirect.ecommerce.data.dlvpass.DlvPassUsageLineData;
+import com.freshdirect.ecommerce.data.order.DlvSaleInfoData;
+import com.freshdirect.ecommerce.data.order.ErpSaleInfoData;
 import com.freshdirect.fdstore.EnumEStoreId;
 import com.freshdirect.fdstore.FDResourceException;
 import com.freshdirect.framework.util.log.LoggerFactory;
@@ -27,7 +32,7 @@ public class OrderServiceApiClient extends AbstractEcommService implements Order
 	private static final String ORDER_HISTORY_API = "customers/{customerId}/orders";
 	private static final String LAST_ORDER_ID_API = "customers/{customerId}/orders/last";
 	private static final String LAST_ORDER_ID_ESTORE_API = "customers/{customerId}/estore/{eStore}/orders/last";
-	private static final String DELIVERY_ORDER_API = "orders/{orderId}";
+	private static final String DELIVERY_ORDER_API = "delivery/orders/{orderId}";
 	private static final String ORDERS_BYDLVPASS_API = "customers/{customerId}/orders/deliverypass/{dlvPassId}";
 	private static final String ORDERS_BYDLVPASS_RECENT_API = "customers/{customerId}/orders/deliverypass/{dlvPassId}/{noOfDaysOld}";
 	private static final String DLVPASS_USAGE_API = "customers/{customerId}/deliverypassUsage";
@@ -46,36 +51,44 @@ public class OrderServiceApiClient extends AbstractEcommService implements Order
 	public List<ErpSaleInfo> getOrderHistory(String customerId)throws FDResourceException, RemoteException {
 		try{
 			String response =  httpGetData(getFdCommerceEndPoint(ORDER_HISTORY_API), String.class, new Object[]{customerId});
-			List<ErpSaleInfo> info = new ArrayList<ErpSaleInfo>();
+			
 			try{
-				info = getMapper().readValue(response, new TypeReference<Response<List<ErpSaleInfo>>>() { });
+				Response<List<ErpSaleInfoData>> info = getMapper().readValue(response, new TypeReference<Response<List<ErpSaleInfoData>>>() { });
+				return CustomMapper.map(parseResponse(info));	
 			}catch(Exception e){
+				LOGGER.info(e);
 				LOGGER.info("Exception converting {} to ListOfObjects "+response);
+				throw new FDResourceException(e);
+				
 			}
-			return info;	
-		
 		}  catch (FDResourceException e) {
 			throw new RemoteException(e.getMessage());
 		}
+		
 		
 	}
 
 	@Override
 	public String getLastOrderId(String customerId) throws RemoteException {
 		try{
-			return httpGetData(getFdCommerceEndPoint(LAST_ORDER_ID_API), String.class, new Object[]{customerId});
+			Response<String> info = httpGetData(getFdCommerceEndPoint(LAST_ORDER_ID_API), Response.class, new Object[]{customerId});
+			return parseResponse(info);	
 		}  catch (FDResourceException e) {
+			LOGGER.info(e);
 			throw new RemoteException(e.getMessage());
 		}
 		
+		
 	}
-
+	
 	@Override
 	public String getLastOrderId(String customerId, EnumEStoreId eStoreId)
 			throws RemoteException {
 		try{
-			return httpGetData(getFdCommerceEndPoint(LAST_ORDER_ID_ESTORE_API), String.class, new Object[]{customerId, (eStoreId!=null)? eStoreId.getContentId():EnumEStoreId.FD.getContentId() });
+			Response<String> info = httpGetData(getFdCommerceEndPoint(LAST_ORDER_ID_ESTORE_API), Response.class, new Object[]{customerId, (eStoreId!=null)? eStoreId.getContentId():EnumEStoreId.FD.getContentId() });
+			return parseResponse(info);	
 		}  catch (FDResourceException e) {
+			LOGGER.info(e);
 			throw new RemoteException(e.getMessage());
 		}
 		
@@ -85,11 +98,19 @@ public class OrderServiceApiClient extends AbstractEcommService implements Order
 	public DlvSaleInfo getDlvSaleInfo(String orderNumber)
 			throws RemoteException {
 		try{
-			return httpGetData(getFdCommerceEndPoint(DELIVERY_ORDER_API), DlvSaleInfo.class, new Object[]{orderNumber});
+			String response = httpGetData(getFdCommerceEndPoint(DELIVERY_ORDER_API), String.class, new Object[]{orderNumber});
+			try{
+				Response<DlvSaleInfoData> info  = getMapper().readValue(response, new TypeReference<Response<DlvSaleInfoData>>() { });
+				return CustomMapper.map(parseResponse(info));
+			}catch(Exception e){
+				LOGGER.info("Exception converting {} to ListOfObjects "+response);
+				LOGGER.info(e);
+				throw new FDResourceException(e);
+			}
+		
 		}  catch (FDResourceException e) {
 			throw new RemoteException(e.getMessage());
 		}
-		
 	}
 
 	@Override
@@ -97,13 +118,14 @@ public class OrderServiceApiClient extends AbstractEcommService implements Order
 			String dlvPassId) throws RemoteException {
 		try{
 			String response =  httpGetData(getFdCommerceEndPoint(ORDERS_BYDLVPASS_API), String.class, new Object[]{customerId, dlvPassId});
-			List<ErpSaleInfo> info = new ArrayList<ErpSaleInfo>();
 			try{
-				info = getMapper().readValue(response, new TypeReference<Response<List<ErpSaleInfo>>>() { });
+				Response<List<ErpSaleInfoData>> info  = getMapper().readValue(response, new TypeReference<Response<List<ErpSaleInfoData>>>() { });
+				return CustomMapper.map(parseResponse(info));	
 			}catch(Exception e){
+				LOGGER.info(e);
 				LOGGER.info("Exception converting {} to ListOfObjects "+response);
+				throw new FDResourceException(e);
 			}
-			return info;	
 		
 		}  catch (FDResourceException e) {
 			throw new RemoteException(e.getMessage());
@@ -111,20 +133,23 @@ public class OrderServiceApiClient extends AbstractEcommService implements Order
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see com.freshdirect.ecomm.gateway.OrderServiceApiClientI#getRecentOrdersByDlvPassId(java.lang.String, java.lang.String, int)
+	 */
 	@Override
 	public List<DlvPassUsageLine> getRecentOrdersByDlvPassId(
 			String customerId, String dlvPassId, int noOfDaysOld)
 			throws RemoteException {
 		try{
 			String response =  httpGetData(getFdCommerceEndPoint(ORDERS_BYDLVPASS_RECENT_API), String.class, new Object[]{customerId, dlvPassId, noOfDaysOld});
-			List<DlvPassUsageLine> info = new ArrayList<DlvPassUsageLine>();
 			try{
-				info = getMapper().readValue(response, new TypeReference<Response<List<DlvPassUsageLine>>>() { });
+				Response<List<DlvPassUsageLineData>> info = getMapper().readValue(response, new TypeReference<Response<List<DlvPassUsageLineData>>>() { });
+				return CustomMapper.mapDeliverypass(parseResponse(info));	
 			}catch(Exception e){
+				LOGGER.info(e);
 				LOGGER.info("Exception converting {} to ListOfObjects "+response);
+				throw new FDResourceException(e);
 			}
-			return info;	
-		
 		}  catch (FDResourceException e) {
 			throw new RemoteException(e.getMessage());
 		}
@@ -136,13 +161,14 @@ public class OrderServiceApiClient extends AbstractEcommService implements Order
 			throws RemoteException {
 		try{
 			String response =  httpGetData(getFdCommerceEndPoint(DLVPASS_USAGE_API), String.class, new Object[]{customerId});
-			Map<String, DlvPassUsageInfo>  info = new HashMap<String, DlvPassUsageInfo>();
 			try{
-				info = getMapper().readValue(response, new TypeReference<Response<Map<String, DlvPassUsageInfo> >>() { });
+				Response<Map<String, DlvPassUsageInfo>> info = getMapper().readValue(response, new TypeReference<Response<Map<String, DlvPassUsageInfo> >>() { });
+				return parseResponse(info);	
 			}catch(Exception e){
-				LOGGER.info("Exception converting {} to ListOfObjects "+response);
+				LOGGER.info(e);
+				LOGGER.info("Exception converting {} to getDlvPassesUsageInfo "+response);
+				throw new FDResourceException(e);
 			}
-			return info;	
 		
 		}  catch (FDResourceException e) {
 			throw new RemoteException(e.getMessage());
@@ -153,7 +179,15 @@ public class OrderServiceApiClient extends AbstractEcommService implements Order
 	@Override
 	public int getValidOrderCount(String customerId) throws RemoteException {
 		try{
-			return httpGetData(getFdCommerceEndPoint(VALID_ORDERCOUNT_API), Integer.class, new Object[]{customerId});
+			String response = httpGetData(getFdCommerceEndPoint(VALID_ORDERCOUNT_API), String.class, new Object[]{customerId});
+			try{
+				Response<Integer> info = getMapper().readValue(response, new TypeReference<Response<Integer>>() { });
+				return parseResponse(info);	
+			}catch(Exception e){
+				LOGGER.info(e);
+				LOGGER.info("Exception converting {} to getValidOrderCount "+response);
+				throw new FDResourceException(e);
+			}
 		}  catch (FDResourceException e) {
 			throw new RemoteException(e.getMessage());
 		}
@@ -163,30 +197,47 @@ public class OrderServiceApiClient extends AbstractEcommService implements Order
 	@Override
 	public boolean isOrderBelongsToUser(String customerId, String orderId) throws RemoteException {
 		try{
-			return httpGetData(getFdCommerceEndPoint(ORDER_BELONGS_TOUSER_API), Boolean.class, new Object[]{customerId, orderId});
+			String response = httpGetData(getFdCommerceEndPoint(ORDER_BELONGS_TOUSER_API), String.class, new Object[]{customerId, orderId});
+			try{
+				Response<Boolean> info = getMapper().readValue(response, new TypeReference<Response<Boolean>>() { });
+				return parseResponse(info);	
+			}catch(Exception e){
+				LOGGER.info(e);
+				LOGGER.info("Exception converting {} to isOrderBelongsToUser "+response);
+				throw new FDResourceException(e);
+			}
 		}  catch (FDResourceException e) {
 			throw new RemoteException(e.getMessage());
 		}
-		
 	}
 	
 	@Override
 	public List<ErpSaleInfo> getWebOrderHistory(String customerId)throws FDResourceException, RemoteException {
 		try{
 			String response =  httpGetData(getFdCommerceEndPoint(WEB_ORDER_HISTORY_API), String.class, new Object[]{customerId});
-			List<ErpSaleInfo> info = new ArrayList<ErpSaleInfo>();
+			Response<List<ErpSaleInfoData>> info = null;
 			try{
-				info = getMapper().readValue(response, new TypeReference<Response<List<ErpSaleInfo>>>() { });
+				info = getMapper().readValue(response, new TypeReference<Response<List<ErpSaleInfoData>>>() { });
+				return CustomMapper.map(parseResponse(info));	
 			}catch(Exception e){
-				LOGGER.info("Exception converting {} to ListOfObjects "+response);
+				LOGGER.info(e);
+				LOGGER.info("Exception converting {} to getWebOrderHistory "+response);
+				throw new FDResourceException(e);
 			}
-			return info;	
-		
 		}  catch (FDResourceException e) {
 			throw new RemoteException(e.getMessage());
 		}
 		
 	}
+
+	private <T> T parseResponse(Response<T> info) throws FDResourceException {
+		if(info!=null && info.getResponseCode().equalsIgnoreCase("OK")){
+			return info.getData();
+		}else{
+			throw new FDResourceException("API error");
+		}
+	}
+
 	
 	
 }
