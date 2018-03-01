b027352da07dd066628385b191cb76aeb5b30966
tsoltesz <tsoltesz@842219ff-9341-0410-a943-573460bdd4de>
2018-03-01 15:37:35 +0000

    [APPDEV-7045] thirdparty integration marker interface
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@76005 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/Framework/src/com/freshdirect/framework/marker/ThirdPartyIntegration.java
@@ -0,0 +1,11 @@
+package com.freshdirect.framework.marker;
+
+/**
+ * This is just a marker interface to make easier the search of third party integration services
+ * 
+ * @author tamassoltesz
+ *
+ */
+public interface ThirdPartyIntegration {
+
+}
