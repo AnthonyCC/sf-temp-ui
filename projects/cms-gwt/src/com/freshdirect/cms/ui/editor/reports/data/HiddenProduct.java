1e0db94fd29324eff575735ef0a38d72b774c839
gsebestyen <gsebestyen@842219ff-9341-0410-a943-573460bdd4de>
2018-03-19 14:10:55 +0000

    [APPDEV-6974] minor fix; remove extra property name from toString method
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@76644 842219ff-9341-0410-a943-573460bdd4de

--- projects/cms-gwt/src/com/freshdirect/cms/ui/editor/reports/data/HiddenProduct.java
+++ projects/cms-gwt/src/com/freshdirect/cms/ui/editor/reports/data/HiddenProduct.java
@@ -41,6 +41,6 @@ public class HiddenProduct {
 
     @Override
     public String toString() {
-        return "{store:'"+storeKey.id+"'; product:'"+productKey.id+"'; primaryHome:'"+primaryHomeKey.id+"'; reason: '"+reason.name()+"'; at:'\"+changeDate.toString()+\"'}";
+        return "{store:'"+storeKey.id+"'; product:'"+productKey.id+"'; primaryHome:'"+primaryHomeKey.id+"'; reason: '"+reason.name()+"'}";
     }
 }
