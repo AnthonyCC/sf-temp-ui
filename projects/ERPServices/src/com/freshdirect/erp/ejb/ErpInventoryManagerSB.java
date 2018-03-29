c61f186a0c327ef3c74d7aba9c6c623f17aad7b4
acheung <acheung@842219ff-9341-0410-a943-573460bdd4de>
2018-03-29 19:39:30 +0000

    Marked ErpInventoryManagerSB and erp/ejb/ErpInventoryEB depreciated
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@77009 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/erp/ejb/ErpInventoryManagerSB.java
+++ projects/ERPServices/src/com/freshdirect/erp/ejb/ErpInventoryManagerSB.java
@@ -8,9 +8,15 @@ import javax.ejb.EJBObject;
 
 import com.freshdirect.customer.ErpRestrictedAvailabilityModel;
 
+/**
+ * @deprecated Please use the ErpInventoryServiceI in Storefront2.0 project.
+ * SVN location :: https://appdevsvn.nj01/appdev/ecommerce
+ *
+ *
+ */
 public interface ErpInventoryManagerSB extends EJBObject {
-
+	@Deprecated
 	public void updateInventories(List inventories) throws RemoteException;
-
+	@Deprecated
 	public void updateRestrictedInfos(Set<ErpRestrictedAvailabilityModel> restrictedInfos, Set<String> deletedMaterials) throws RemoteException;
 }
