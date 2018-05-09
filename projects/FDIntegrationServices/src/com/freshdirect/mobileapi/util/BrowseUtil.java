e05599263ed5519fcba2667e0a78df8a011c750a
oprymak <oprymak@842219ff-9341-0410-a943-573460bdd4de>
2018-05-09 18:53:48 +0000

    bugfix for FOOD-391 jira.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@78437 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDIntegrationServices/src/com/freshdirect/mobileapi/util/BrowseUtil.java
+++ projects/FDIntegrationServices/src/com/freshdirect/mobileapi/util/BrowseUtil.java
@@ -790,7 +790,7 @@ public class BrowseUtil {
 
     private static Category buildCategoryData(FDUserI user, CategoryModel model, boolean isExtraResponse) {
         Category category = null;
-        if (model != null && (!NavigationUtil.isCategoryHiddenInContext(user, model) || !isExtraResponse)) {
+        if (model != null && !NavigationUtil.isCategoryHiddenInContext(user, model)) {
             category = Category.wrap(model);
             if (model.getSubcategories() != null && !model.getSubcategories().isEmpty()) {
                 for (CategoryModel subcat : model.getSubcategories()) {
