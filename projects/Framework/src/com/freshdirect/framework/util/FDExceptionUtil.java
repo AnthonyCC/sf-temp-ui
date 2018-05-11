be89b2e4a06e9a565203f12b4da4a6d1e8a635f3
vduka <vduka@842219ff-9341-0410-a943-573460bdd4de>
2018-05-11 11:16:37 +0000

    [FIX500-51] Added card declined and timeslot expired errors a new logging id
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@78500 842219ff-9341-0410-a943-573460bdd4de

--- projects/Framework/src/com/freshdirect/framework/util/FDExceptionUtil.java
+++ projects/Framework/src/com/freshdirect/framework/util/FDExceptionUtil.java
@@ -30,4 +30,19 @@ public class FDExceptionUtil {
         return relatedException;
 
     }
+
+    public static boolean isCheckoutPaymentError(String exceptionText) {
+        boolean relatedException = false;
+
+        if (exceptionText != null) {
+            String upperExceptionText = exceptionText.toUpperCase();
+            relatedException = upperExceptionText.contains(
+                    "Your payment method was declined. Please select a different payment method for this order and contact your card issuer for clarification.".toUpperCase())
+                    || upperExceptionText.contains("Your timeslot reservation has expired, please go back and choose another timeslot.".toUpperCase());
+        }
+
+        return relatedException;
+
+    }
+
 }
