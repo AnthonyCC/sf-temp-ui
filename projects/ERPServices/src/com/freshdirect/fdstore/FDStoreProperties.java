f16bd5a08b01c1e05f166586b8fd267881921fa1
ksriram <ksriram@842219ff-9341-0410-a943-573460bdd4de>
2018-06-08 17:01:51 +0000

    Disabling SF2.0 by default.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@79514 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/fdstore/FDStoreProperties.java
+++ projects/ERPServices/src/com/freshdirect/fdstore/FDStoreProperties.java
@@ -1930,7 +1930,7 @@ public class FDStoreProperties {
         defaults.put(PROP_PLANT1310_PRICE_INDICATOR, "BASE");
         defaults.put(PROP_PLANTWDC_PRICE_INDICATOR, "BASE");
 
-        defaults.put(PROP_SF_2_0_ENABLED, "true");
+        defaults.put(PROP_SF_2_0_ENABLED, "false");
         defaults.put(PROP_MAT_SALESORG__EXPORT_PICKPLANT_VALIDATION_ENABLED, "true");
         defaults.put(PROP_EXTRA_LOG_FOR_LOGIN_FAILS_ENABLED, "false");
         defaults.put(PROP_MEALBUNDLE_CARTONVIEW_ENABLED, "true");
@@ -1944,7 +1944,7 @@ public class FDStoreProperties {
         defaults.put(PROP_DFP_ENABLED, "false");
         defaults.put(PROP_DFP_ID, "1072054678");
 
-        defaults.put(PROP_CLUSTER_NAME, "locallhost");
+        defaults.put(PROP_CLUSTER_NAME, "localhost");
         defaults.put(PROP_NODE_NAME, "localhost");
 
         defaults.put(PROP_PRODUCT_CACHE_OPTIMIZATION_ENABLED, "true");
