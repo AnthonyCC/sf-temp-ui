bab345de0da7c2e659e616b3e1ce8bdb058ccb48
sbagavathiappan <sbagavathiappan@842219ff-9341-0410-a943-573460bdd4de>
2018-06-21 16:27:16 +0000

    FDLAB-15 Standalone FoodKick Web Feed Page for Marketing Campaigns (Fix Backend Bug to return feed by ID)
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@79903 842219ff-9341-0410-a943-573460bdd4de

--- projects/StoreAPI/src/main/java/com/freshdirect/storeapi/content/CMSWebPageModel.java
+++ projects/StoreAPI/src/main/java/com/freshdirect/storeapi/content/CMSWebPageModel.java
@@ -9,6 +9,8 @@ public class CMSWebPageModel{
 	private String type;
 	private List<CMSSectionModel> sections;
 	private List<CMSScheduleModel> schedule;
+	
+	private String id;
 
 	public List<CMSSectionModel> getSections() {
 		return sections;
@@ -49,8 +51,16 @@ public class CMSWebPageModel{
 	public void setType(String type) {
 		this.type = type;
 	}
+		
+    public String getId() {
+		return id;
+	}
 
-    @Override
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
@@ -59,6 +69,7 @@ public class CMSWebPageModel{
         result = prime * result + ((seoMetaDescription == null) ? 0 : seoMetaDescription.hashCode());
         result = prime * result + ((title == null) ? 0 : title.hashCode());
         result = prime * result + ((type == null) ? 0 : type.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
 
@@ -95,6 +106,11 @@ public class CMSWebPageModel{
             if (other.type != null)
                 return false;
         } else if (!type.equals(other.type))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
             return false;
         return true;
     }
