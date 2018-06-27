f8916f47f9cf068879e098a933df5db6efedcd31
ssubramanian <ssubramanian@842219ff-9341-0410-a943-573460bdd4de>
2018-06-27 18:08:29 +0000

    Merged 2018_Jun_R2 upto revision # 80130
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/features/branches/sf2_0_fe@80132 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/PaymentMethodControllerTag.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/taglib/fdstore/PaymentMethodControllerTag.java
@@ -209,6 +209,18 @@ public class PaymentMethodControllerTag extends com.freshdirect.framework.webapp
                     throw new JspException("Error redirecting "+ioe.getMessage());
                 }
             }
+            if("deactivated_Account".equals( actionResult.getError("deactivated_Account").getType()))  {
+            	HttpServletResponse response = (HttpServletResponse) pageContext.getResponse();
+                try {
+                    response.sendRedirect(response.encodeRedirectURL("/logout.jsp"));
+                    JspWriter writer = pageContext.getOut();
+                    writer.close();
+                    return SKIP_BODY;
+                } catch (IOException ioe) {
+                    // if there was a problem redirecting, well.. it can't get any worse :)
+                    throw new JspException("Error redirecting "+ioe.getMessage());
+                }
+            }
         }
 
         // place the result as a scripting variable in the page
