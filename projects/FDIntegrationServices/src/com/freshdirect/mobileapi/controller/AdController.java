f908f451e4cc47391f4b4f6ea317080b718eac18
gsebestyen <gsebestyen@842219ff-9341-0410-a943-573460bdd4de>
2018-07-05 16:16:00 +0000

    [APPDEV-7348] add controller class
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@80307 842219ff-9341-0410-a943-573460bdd4de

---
+++ projects/FDIntegrationServices/src/com/freshdirect/mobileapi/controller/AdController.java
@@ -0,0 +1,61 @@
+package com.freshdirect.mobileapi.controller;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Category;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.freshdirect.fdstore.FDException;
+import com.freshdirect.fdstore.ads.AdQueryStringFactory;
+import com.freshdirect.fdstore.customer.FDUserI;
+import com.freshdirect.fdstore.rollout.EnumRolloutFeature;
+import com.freshdirect.fdstore.rollout.FeatureRolloutArbiter;
+import com.freshdirect.framework.template.TemplateException;
+import com.freshdirect.framework.util.log.LoggerFactory;
+import com.freshdirect.mobileapi.controller.data.Message;
+import com.freshdirect.mobileapi.controller.data.response.AdQueryStringResponse;
+import com.freshdirect.mobileapi.exception.JsonException;
+import com.freshdirect.mobileapi.exception.NoSessionException;
+import com.freshdirect.mobileapi.model.MessageCodes;
+import com.freshdirect.mobileapi.model.SessionUser;
+import com.freshdirect.mobileapi.service.ServiceException;
+import com.freshdirect.webapp.util.JspMethods;
+
+
+public class AdController extends BaseController {
+
+    private static Category LOGGER = LoggerFactory.getInstance(AdController.class);
+
+    private static String ACTION_GET_AD_QUERYSTRING = "queryparams";
+
+    @Override
+    protected ModelAndView processRequest(HttpServletRequest request, HttpServletResponse response, ModelAndView model, String action, SessionUser user)
+            throws JsonException, FDException, ServiceException, NoSessionException, IOException, TemplateException {
+        if (ACTION_GET_AD_QUERYSTRING.equals(action)) {
+            FDUserI fdUser = user != null ? user.getFDSessionUser() : null;
+
+            final boolean isMobileWebCapable = FeatureRolloutArbiter.isFeatureRolledOut(EnumRolloutFeature.mobweb, fdUser) && JspMethods.isMobile(request.getHeader("User-Agent"));
+
+            final Map<String, String> queryParams = AdQueryStringFactory.composeAdQueryParams(fdUser, request, isMobileWebCapable);
+
+            Message responseMessage = new AdQueryStringResponse(queryParams);
+            responseMessage.setStatus(Message.STATUS_SUCCESS);
+            setResponseMessage(model, responseMessage, user);
+        } else {
+            createErrorResponseMessage(model, user, MessageCodes.ERR_SYSTEM, "Invalid or missing action.");
+        }
+
+        return model;
+    }
+
+    private void createErrorResponseMessage(ModelAndView modelAndView, SessionUser user, String code, String message) throws JsonException {
+        Message responseMessage = new Message();
+        responseMessage.setStatus(Message.STATUS_FAILED);
+        responseMessage =  getErrorMessage(code, message);
+        setResponseMessage(modelAndView, responseMessage, user);
+    }
+}
