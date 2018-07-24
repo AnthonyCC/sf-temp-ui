6cce163cfde0cf59485090cd40e8221662b4bee5
smerugu <smerugu@842219ff-9341-0410-a943-573460bdd4de>
2018-07-24 02:50:30 +0000

    APPBUG-5705:Criteo homepage- When a keyword returns less products than others, the iteration for other keyword does not continue.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@80900 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/product/CriteoProductsHomePageCache.java
+++ projects/ERPSWebApp/src/com/freshdirect/webapp/ajax/product/CriteoProductsHomePageCache.java
@@ -1,8 +1,10 @@
 package com.freshdirect.webapp.ajax.product;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -22,40 +24,44 @@ import com.freshdirect.storeapi.content.ProductModel;
 public class CriteoProductsHomePageCache {
 
 	private static final Category LOGGER = LoggerFactory.getInstance(CriteoProductsHomePageCache.class);	
-	private ConcurrentHashMap<String, List<HLBrandProductAdInfo>> cacheCriteoMap = new ConcurrentHashMap<String, List<HLBrandProductAdInfo>>();
+	private ConcurrentHashMap<String, HLBrandProductAdResponse> cacheCriteoMap = new ConcurrentHashMap<String, HLBrandProductAdResponse>();
 //	private ConcurrentHashMap<String, String> pageBeaconMap = new ConcurrentHashMap<String, String>();
-	private  ArrayList<HLBrandProductAdInfo> hlba = new ArrayList<HLBrandProductAdInfo>();
+	//private  ArrayList<HLBrandProductAdInfo> hlba = new ArrayList<HLBrandProductAdInfo>();
+	
+	private Map<String, HLBrandProductAdResponse> CriteoProductnMap = new  HashMap<String, HLBrandProductAdResponse>();
 	
 	private final static Object lock = new Object();
 	private static CriteoProductsHomePageCache sharedInstance = null;
 	
 
 	
-	public ArrayList<HLBrandProductAdInfo> getProducts() {
+	public Map<String,HLBrandProductAdResponse> getProducts() {
 		return this.getPrductsMap();
 	}
 	
-	protected ArrayList<HLBrandProductAdInfo> getPrductsMap() {
-		ArrayList<HLBrandProductAdInfo> productsMap = this.criteoProd.get();
+	protected Map<String, HLBrandProductAdResponse> getPrductsMap() {
+		Map<String, HLBrandProductAdResponse> productsMap = this.criteoProd.get();
 		if(null !=productsMap && !productsMap.isEmpty()){
 		//	this.hlba.clear();	/* if in case CretioProducts does'nt reflect Updated list, this should be uncommented. */
-			this.hlba= productsMap;
+			this.CriteoProductnMap= productsMap;
 		}
-		return this.hlba;
+		return this.CriteoProductnMap;
 	}
 									/*  30 min interval , 60,000 milliseconds/Min	*/
-	private ExpiringReference< ArrayList<HLBrandProductAdInfo>> criteoProd = new ExpiringReference<ArrayList<HLBrandProductAdInfo>>(FDStoreProperties.getFDHomeCriteoServerCacheRefresh() * 60 * 1000) {
+	private ExpiringReference<Map<String, HLBrandProductAdResponse>> criteoProd = new ExpiringReference<Map<String, HLBrandProductAdResponse>>(FDStoreProperties.getFDHomeCriteoServerCacheRefresh() * 60 * 1000) {
 		@Override
-		protected ArrayList<HLBrandProductAdInfo> load() {
+		protected Map<String, HLBrandProductAdResponse> load() {
 			try {
 					LOGGER.info("REFRESHING start");
-					hlba.clear();		/* clearing previous cached products and updating with new list */
-					hlba =	loadProducts();
+					CriteoProductnMap.clear();		/* clearing previous cached products and updating with new list */
+					//hlba =	loadProducts();
+					CriteoProductnMap =	loadProducts();
 					LOGGER.info("REFRESH completed.");
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
-			return hlba;
+			//return hlba;
+			return CriteoProductnMap;
 		}
 	};
 	
@@ -78,72 +84,22 @@ public class CriteoProductsHomePageCache {
 		loadProducts();
 	}
 	
-	private  ArrayList<HLBrandProductAdInfo> loadProducts(){
+	private  Map<String, HLBrandProductAdResponse> loadProducts(){
 			HLBrandProductAdRequest hLBrandProductAdRequest = new HLBrandProductAdRequest();
 			HLBrandProductAdResponse response = null;
 			List<String> keys = CriteoProductsUtil.getFDSearchPriorityKeyWords();
-			int counter=0;
-			Set<String> skuList = new HashSet<String>();
 			if (!keys.isEmpty() && keys.size() != 0) {
 				for (String key : keys) {
 					try {
 						hLBrandProductAdRequest.setSearchKeyWord(key);
-						/*
-						 * we can fetch products without PUserId, but Not with  'empty string'.	 */
 						if (hLBrandProductAdRequest.getUserId() == null) {
 							hLBrandProductAdRequest.setUserId("null");
 						}
+						hLBrandProductAdRequest.setSelectedServiceType("RESIDENTIAL");
 						response = FDBrandProductsAdManager.getHLadproductToHomeByFDPriority(hLBrandProductAdRequest);
 
 					if (null != response && null != response.getSearchProductAd()) {
-						List<HLBrandProductAdInfo> respList = response.getSearchProductAd();
-						List<HLBrandProductAdInfo> avaiProductFDList= new ArrayList<HLBrandProductAdInfo>();
-						for (HLBrandProductAdInfo resp : respList) {
-							ProductModel productModel = null;
-							try {
-								productModel = ContentFactory.getInstance().getProduct(resp.getProductSKU());
-							} catch (Exception e) {
-								LOGGER.info("SKu not found for Hooklogic product: " + resp.getProductSKU());
-							}
-							if (null != productModel && !productModel.isUnavailable()) {
-								avaiProductFDList.add(resp);
-							}
-						}
-						/*  page beacon of Api call, we are setting to product level here */
-						 final String A_SHOWN="&ashown=";
-						 final StringBuffer updatedPageBeacon= new StringBuffer(A_SHOWN);
-						 //String pageBeaconString = null;
-						if (!avaiProductFDList.isEmpty()) {
-							for (HLBrandProductAdInfo pdata : avaiProductFDList) {
-								updatedPageBeacon.append(((A_SHOWN.equals(updatedPageBeacon.toString()))
-										? pdata.getProductSKU() : "," + pdata.getProductSKU()));
-							}
-							avaiProductFDList.get(0).setPageBeacon(response.getPageBeacon() + updatedPageBeacon.toString());
-						}
-						
-				
-					/*	if(!avaiProductFDList.isEmpty() && avaiProductFDList.size() < 5){
-											int i= avaiProductFDList.size();
-											int j=0;
-											while ( i < 5) {
-												avaiProductFDList.add(avaiProductFDList.get(j));
-												i++;	
-												j++;
-											}
-										}*/
-							if (!avaiProductFDList.isEmpty()) {
-								for (HLBrandProductAdInfo productData : avaiProductFDList) {
-									if (!skuList.contains(productData
-											.getProductSKU()))
-										skuList.add(productData.getProductSKU());
-									else {
-										avaiProductFDList.remove(productData);
-									}
-								}
-							}
-						cacheCriteoMap.put(key, avaiProductFDList);
-						//pageBeaconMap.put(key, response.getPageBeacon());
-						counter = counter + avaiProductFDList.size();
+						cacheCriteoMap.put(key, response);
 					}
 					} catch (FDResourceException e) {
 						LOGGER.debug("Exception occured while making call to Criteo search-Api: " + e);
@@ -152,31 +108,8 @@ public class CriteoProductsHomePageCache {
 					}
 				}
 			}
-			//setting HasMap to ArrayList here
-			int i=0;
-			int j=0;
-		if (!keys.isEmpty() && keys.size() != 0 && counter != 0) {
-			boolean found = false;
-			while (counter >=  i++) {
-				for (String key : keys) {
-					if (null != cacheCriteoMap.get(key) && !cacheCriteoMap.get(key).isEmpty()
-							&& cacheCriteoMap.get(key).size() >= (j + 1) ) {
-						HLBrandProductAdInfo hlBrandProduct = cacheCriteoMap.get(key).get(j);
-						//String pageBeacon = pageBeaconMap.get(key);
-					//	hlBrandProduct.setPageBeacon(pageBeacon);
-						hlba.add(hlBrandProduct);
-						found = true;
-						i++;
-					}
-				}
-				if (!found) {
-					break;
-				}
-				j++;
-			}
-		}
-
-		return hlba;	/* this is the final list which we are saving in server cache*/
+			
+	return CriteoProductnMap=cacheCriteoMap;
 	
 	}
 }
