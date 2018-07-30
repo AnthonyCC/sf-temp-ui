4fb4887d8888eef713c016f35451c276a5965b02
acheung <acheung@842219ff-9341-0410-a943-573460bdd4de>
2018-07-30 20:35:47 +0000

    Add ErpFraudException check
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@81132 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDStore/src/com/freshdirect/fdstore/ecomm/gateway/CustomerPaymentService.java
+++ projects/FDStore/src/com/freshdirect/fdstore/ecomm/gateway/CustomerPaymentService.java
@@ -8,7 +8,9 @@ import org.apache.log4j.Category;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.freshdirect.customer.EnumFraudReason;
 import com.freshdirect.customer.EnumPaymentMethodDefaultType;
+import com.freshdirect.customer.ErpFraudException;
 import com.freshdirect.customer.ErpPaymentMethodException;
 import com.freshdirect.customer.ErpPaymentMethodI;
 import com.freshdirect.ecomm.gateway.AbstractEcommService;
@@ -56,7 +58,7 @@ public class CustomerPaymentService extends AbstractEcommService implements Cust
 
 	@Override
 	public void addPaymentMethod(FDActionInfo info, ErpPaymentMethodI paymentMethod, boolean paymentechEnabled,
-			boolean isDebitCardSwitch) throws FDResourceException, RemoteException, ErpPaymentMethodException {
+			boolean isDebitCardSwitch) throws FDResourceException, RemoteException, ErpPaymentMethodException, ErpFraudException {
 		Response<Void> response = null;
 		try {
 			Request<ObjectNode> request = new Request<ObjectNode>();
@@ -81,6 +83,11 @@ public class CustomerPaymentService extends AbstractEcommService implements Cust
 					} else {
 						throw new ErpPaymentMethodException();
 					}
+				}
+				if ("ErpFraudException".equals(response.getMessage())) {
+					
+					throw new ErpFraudException(EnumFraudReason.DEACTIVATED_ACCOUNT);
+					
 				}
 				throw new FDResourceException(response.getMessage());
 			}
