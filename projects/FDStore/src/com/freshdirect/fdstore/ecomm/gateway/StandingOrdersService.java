99c59d2d2cb2f9206a5107724010dc0481498231
jjoseph <jjoseph@842219ff-9341-0410-a943-573460bdd4de>
2018-08-20 18:01:29 +0000

    SF17-329 : When we ran the Standing order cron in console.stdev07.nj01 an email was sent with exception to applicationdevelopment@freshdirect.com.
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@81661 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDStore/src/com/freshdirect/fdstore/ecomm/gateway/StandingOrdersService.java
+++ projects/FDStore/src/com/freshdirect/fdstore/ecomm/gateway/StandingOrdersService.java
@@ -14,18 +14,24 @@ import org.apache.log4j.Category;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.freshdirect.customer.ErpOrderLineModel;
 import com.freshdirect.customer.ErpSaleModel;
 import com.freshdirect.ecomm.gateway.AbstractEcommService;
 import com.freshdirect.ecommerce.data.common.Request;
 import com.freshdirect.ecommerce.data.common.Response;
+import com.freshdirect.ecommerce.data.ecoupon.FDConfigurationData;
+import com.freshdirect.ecommerce.data.ecoupon.FDSkuData;
 import com.freshdirect.ecommerce.data.standingorders.PlaceStandingOrderData;
 import com.freshdirect.ecommerce.data.standingorders.ResultData;
 import com.freshdirect.ecommerce.data.standingorders.ResultListData;
 import com.freshdirect.ecommerce.data.standingorders.StandingOrdersJobConfigData;
+import com.freshdirect.ecommerce.data.standingorders.UnAvailabilityDetailsData;
 import com.freshdirect.ecommerce.data.standingorders.UnavailabilityDetailsWrapper;
+import com.freshdirect.fdstore.FDConfiguration;
 import com.freshdirect.fdstore.FDEcommServiceException;
 import com.freshdirect.fdstore.FDResourceException;
 import com.freshdirect.fdstore.FDRuntimeException;
+import com.freshdirect.fdstore.FDSku;
 import com.freshdirect.fdstore.customer.FDCartLineI;
 import com.freshdirect.fdstore.customer.FDCartLineModel;
 import com.freshdirect.fdstore.ecomm.converter.StandingOrderConverter;
@@ -38,6 +44,7 @@ import com.freshdirect.framework.util.log.LoggerFactory;
 import com.freshdirect.payment.service.ModelConverter;
 import com.freshdirect.fdstore.standingorders.FDStandingOrder.ErrorCode;
 import com.freshdirect.fdstore.standingorders.SOResult.Status;
+import com.freshdirect.fdstore.standingorders.UnavailabilityReason;
 
 public class StandingOrdersService extends AbstractEcommService implements StandingOrdersServiceI {
 
@@ -124,13 +131,39 @@ private final static Category LOGGER = LoggerFactory.getInstance(StandingOrdersS
 		data.setStatus(Status.valueOf(resultData.getStatus().toString()));
 		if(resultData.getRequestedDate()!=null)
 		data.setRequestedDate(new Date(resultData.getRequestedDate()));
-//		data.setUnavailabilityDetails(buildUnavailabilityDetails(resultData.getUnavailabilityDetails()));
+		data.setUnavailabilityDetails(buildUnavailabilityDetails(resultData.getUnavailabilityDetails()));
 		return data;
 		
 	}
 	
+	private Map<FDCartLineI, UnAvailabilityDetails> buildUnavailabilityDetails(
+			List<UnavailabilityDetailsWrapper> unavailabilityDetails) {
+		Map<FDCartLineI, UnAvailabilityDetails> unavailabilityMap = new HashMap<FDCartLineI, UnAvailabilityDetails>();
+		for (UnavailabilityDetailsWrapper wrapper : unavailabilityDetails) {
+			ErpOrderLineModel model = new ErpOrderLineModel();
+			model.setSku(buildFDSku(wrapper.getCartLine().getSkuData()));
+			model.setMaterialNumber(wrapper.getCartLine().getMaterialNumber());
+			model.setConfiguration(buildConfiguration(wrapper.getCartLine().getConfigurationData()));
+			FDCartLineI cartline =  new FDCartLineModel(model);
+			cartline.setSku(buildFDSku(wrapper.getCartLine().getSkuData()));
+			cartline.setSalesUnit(wrapper.getCartLine().getSalesUnit());
+			cartline.setDescription(wrapper.getCartLine().getDescription());
+		
+			unavailabilityMap.put(cartline, buildUnavailabilityDeatils(wrapper.getUnavData()));
+		}
+		return unavailabilityMap;
+	}
 
-
+	private FDSku buildFDSku(FDSkuData skuData) {
+		return new FDSku(skuData.getSkuCode(), skuData.getVersion());
+	}
+	private FDConfiguration buildConfiguration(FDConfigurationData configurationData) {
+		FDConfiguration configuration = new FDConfiguration(configurationData.getQuantity(), configurationData.getSalesUnit());
+		return configuration;
+	}
+	private UnAvailabilityDetails buildUnavailabilityDeatils(UnAvailabilityDetailsData unAvailabilityDetailsData) {
+		return new UnAvailabilityDetails(unAvailabilityDetailsData.getUnavailQty(), UnavailabilityReason.valueOf(unAvailabilityDetailsData.getReason()), unAvailabilityDetailsData.getAltSkucode());
+	}
 
 	@Override
 	public void persistUnavDetailsToDB(ResultList resultCounter) throws FDResourceException, RemoteException {
