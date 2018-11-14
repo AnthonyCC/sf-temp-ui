e6f8cc3acaad7a74e76543901c0f7dcc496c80d0
ssubramanian <ssubramanian@842219ff-9341-0410-a943-573460bdd4de>
2018-11-14 15:02:15 +0000

    Merged revision(s) 83768-83770 from FreshDirect/releases/branches/2018_Nov_R1: Merged revision(s) 83767 from FreshDirect/trunk: Adding extra log for sporadic Restrictions issue in PROD. ........
    ........
    Merged revision(s) 83769 from FreshDirect/trunk:
    Adding extra log to debug sporadic Restrictions issue in PROD.
    ........
    
    ........
    
    
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/features/branches/sf2_0_fe@83783 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDStore/src/com/freshdirect/fdstore/customer/FDAvailabilityMapper.java
+++ projects/FDStore/src/com/freshdirect/fdstore/customer/FDAvailabilityMapper.java
@@ -80,14 +80,19 @@ class FDAvailabilityMapper {
 			
 
 
-			Set<EnumDlvRestrictionReason> applicableRestrictions = cartline.getApplicableRestrictions();
+			Set<EnumDlvRestrictionReason> applicableRestrictions = cartline.getApplicableRestrictions();			
 			
-			LOGGER.debug(" applicable restrictions :"+applicableRestrictions);
+			if(null !=cartline.getUserContext() && null !=cartline.getUserContext().getFdIdentity() && null !=cartline.getUserContext().getFdIdentity().getErpCustomerPK()){
+				LOGGER.info("RESTRICTIONS_LOG: Applicable restrictions for customer: "+ (cartline.getUserContext().getFdIdentity().getErpCustomerPK())+" , for sku: "+ cartline.getSkuCode()+" , for delivery date:"+ order.getRequestedDate()+" are: "+applicableRestrictions);
+			}
 
 			if (!applicableRestrictions.isEmpty()) {
 
 				// apply delivery restrictions
 				List<RestrictionI> r = allRestrictions.getRestrictions(EnumDlvRestrictionCriterion.DELIVERY, applicableRestrictions);
+				if(null !=cartline.getUserContext() && null !=cartline.getUserContext().getFdIdentity() && null !=cartline.getUserContext().getFdIdentity().getErpCustomerPK()){
+					LOGGER.info("RESTRICTIONS_LOG: Filtered DELIVERY restrictions for customer: "+ (cartline.getUserContext().getFdIdentity().getErpCustomerPK())+" , for sku: "+ cartline.getSkuCode()+" , for delivery date:"+ order.getRequestedDate()+" are: "+r);
+				}
 				LOGGER.debug(" filtered applicable restrictions :"+applicableRestrictions);
 
 				if (!r.isEmpty()) {
