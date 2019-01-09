5bfe9ad7bdc4206777d024350d90ce8da94112e2
ketki <ketki@842219ff-9341-0410-a943-573460bdd4de>
2019-01-09 16:16:58 +0000

    https://jira.freshdirect.com/browse/APPDEV-7773
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@84841 842219ff-9341-0410-a943-573460bdd4de

--- projects/FDIntegrationServices/src/com/freshdirect/mobileapi/controller/ExternalInterfaceController.java
+++ projects/FDIntegrationServices/src/com/freshdirect/mobileapi/controller/ExternalInterfaceController.java
@@ -26,6 +26,7 @@ import com.freshdirect.fdstore.customer.FDIdentity;
 import com.freshdirect.fdstore.customer.FDOrderI;
 import com.freshdirect.fdstore.mail.FDEmailFactory;
 import com.freshdirect.framework.util.log.LoggerFactory;
+import com.freshdirect.logistics.controller.data.Result;
 import com.freshdirect.logistics.delivery.model.EnumDeliveryMenuOption;
 import com.freshdirect.mobileapi.controller.data.Message;
 import com.freshdirect.mobileapi.exception.JsonException;
@@ -213,7 +214,13 @@ public class ExternalInterfaceController extends BaseController {
         						}
         					}
         				}
-        				fDDeliveryManager.captureDeliveryEventNotification(carrier, sb.toString().trim());
+        				Result	result=fDDeliveryManager.captureDeliveryEventNotification(carrier, sb.toString().trim());
+        				if(result.getStatus()!=null && result.getStatus().equals("FAILED")) {
+	    					responseMessage=Message.createFailureMessage(result.getErrors()!=null?result.getErrors().toString():"Error Occurred");
+	    					responseMessage.setStatus("FAILED");
+	    					response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+	    				}
+	    				else
         				responseMessage = Message.createSuccessMessage("T005 Successful.");
         				
     				}else{
@@ -232,7 +239,8 @@ public class ExternalInterfaceController extends BaseController {
 	       			  			 estDeliveryTime=temp[3]; 
 	       			  		 }
 	       			  	FDDeliveryManager fDDeliveryManager = FDDeliveryManager.getInstance();
-	    				fDDeliveryManager.captureFdxDeliveryInfo(erpOrderId,deliveryTime,nexStopErpOrderId,estDeliveryTime);
+	    				Result result= fDDeliveryManager.captureFdxDeliveryInfo(erpOrderId,deliveryTime,nexStopErpOrderId,estDeliveryTime);
+	    				
 	    				responseMessage = Message.createSuccessMessage("T005 Successful.");
 	    				}
 	       			  	 
