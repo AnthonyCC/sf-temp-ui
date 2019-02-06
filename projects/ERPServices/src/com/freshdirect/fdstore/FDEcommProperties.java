cdc2cefc503975231980c3aa667d3e4b309c3935
acheung <acheung@842219ff-9341-0410-a943-573460bdd4de>
2019-02-06 16:58:54 +0000

    Removed ecom property giftcard.ejb.GiftCardManagerSB
    git-svn-id: https://appdevsvn.nj01/appdev/FreshDirect/trunk@85707 842219ff-9341-0410-a943-573460bdd4de

--- projects/ERPServices/src/com/freshdirect/fdstore/FDEcommProperties.java
+++ projects/ERPServices/src/com/freshdirect/fdstore/FDEcommProperties.java
@@ -63,6 +63,7 @@ public class FDEcommProperties {
 	public static final String FDCustomerComplaint = "com.freshdirect.customer.complaint";
 	public static final String FDCustomerInfo = "com.freshdirect.customer.info";
 	public static final String FDCustomer = "com.freshdirect.customer";
+    public static final String FDCustomerReport = "com.freshdirect.customer.report";
 	public static final String FDCustomerMisc1 = "com.freshdirect.customer.misc1";
 	public static final String FDCustomerFactory = "com.freshdirect.fdstore.customer.FDCustomerFactory";
 	public static final String SaleCronSB = "payment.ejb.SaleCronSB";
